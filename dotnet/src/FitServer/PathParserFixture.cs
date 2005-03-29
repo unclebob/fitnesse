f63ffef4b9a323f46a310bebbd3d66281458b38d
micah <micah>
2005-03-29 16:47:47 +0000

    no message

---
+++ dotnet/src/FitServer/PathParserFixture.cs
@@ -0,0 +1,20 @@
+namespace fit
+{
+	public class PathParserFixture : ColumnFixture
+	{
+		public string PathString;
+		public string[] AssemblyPaths;
+		public string ConfigFilePath;
+
+		public override void Execute()
+		{
+			PathParser parser = new PathParser(PathString);
+			AssemblyPaths = new string[parser.AssemblyPaths.Count];
+			int index = 0;
+			foreach(string assemblyPath in parser.AssemblyPaths)
+				AssemblyPaths[index++] = assemblyPath;
+			ConfigFilePath = parser.ConfigFilePath;
+		}
+
+	}
+}
