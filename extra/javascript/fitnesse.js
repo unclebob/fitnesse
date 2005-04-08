b40ddd48df6f24f343e9dd9fad083efdefabc35a
david <david>
2005-04-08 01:51:35 +0000

    added collapseAll() and expandAll() javascript functions

--- extra/javascript/fitnesse.js
+++ extra/javascript/fitnesse.js
@@ -19,3 +19,26 @@ function toggleCollapsable(id)
 		img.src = collapsableClosedImg;
 	}
 }
+
+function expandOrCollapseAll(cssClass)
+{
+	divs = document.getElementsByTagName("div");
+	for (i = 0; i < divs.length; i++)
+	{
+	 	div = divs[i];
+	 	if (div.className == cssClass)
+	 	{
+	 	  toggleCollapsable(div.id);
+		}
+	}
+}
+
+function collapseAll()
+{
+	expandOrCollapseAll(collapsableOpenCss);
+}
+
+function expandAll()
+{
+	expandOrCollapseAll(collapsableClosedCss);
+}
