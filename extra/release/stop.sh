4a3be1aecfa6117d945761e4ceda5ee8103dd89b
deanwampler <deanwampler>
2008-06-18 23:25:06 +0000

    bug fix.

--- extra/release/stop.sh
+++ extra/release/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-# stop.sh - Start a running FitNesse server. You must specify the same port used with "run.sh".
+# stop.sh - Stop a running FitNesse server. You must specify the same port used with "run.sh".
 
 java -cp fitnesse.jar fitnesse.Shutdown "$@"
