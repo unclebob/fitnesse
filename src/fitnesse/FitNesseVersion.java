6b50dacd921b596dbd75b9dff06e1e4aa1e6d2a9
unclebob <unclebob@objectmentor.com>
2009-01-22 17:52:04 -0600

    FitNesseRoot/properties

--- src/fitnesse/FitNesseVersion.java
+++ src/fitnesse/FitNesseVersion.java
@@ -1,9 +1,9 @@
-// Copyright (C) 2003-2009 by Object Mentor, Inc. All rights reserved.
-// Released under the terms of the CPL Common Public License version 1.0.
+// Copyright (C) 2003,2004,2005 by Object Mentor, Inc. All rights reserved.
+// Released under the terms of the GNU General Public License version 2 or later.
 package fitnesse;
 
 class FitNesseVersion {
 	public String toString() {
-		return "v20090121";
+		return "v20090122";
 	}
 }
