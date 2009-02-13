d93277dfebedd91e331805e2fec0ffa147f543c0
unclebob <unclebob@objectmentor.com>
2009-02-13 11:49:52 -0600

    PacketResponder works

---
+++ src/fitnesse/responders/PacketResponder.java
@@ -0,0 +1,95 @@
+package fitnesse.responders;
+
+import fitnesse.FitNesseContext;
+import fitnesse.Responder;
+import fitnesse.http.Request;
+import fitnesse.http.Response;
+import fitnesse.http.SimpleResponse;
+import fitnesse.slimTables.HtmlTableScanner;
+import fitnesse.slimTables.Table;
+import fitnesse.slimTables.TableScanner;
+import fitnesse.wiki.PageCrawler;
+import fitnesse.wiki.PathParser;
+import fitnesse.wiki.WikiPage;
+import fitnesse.wiki.WikiPagePath;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PacketResponder implements Responder {
+  private SimpleResponse response;
+  private WikiPage page;
+  private JSONObject packet;
+  List<JSONObject> tables = new ArrayList<JSONObject>();
+
+  public Response makeResponse(FitNesseContext context, Request request) throws Exception {
+    response = new SimpleResponse();
+    String pageName = request.getResource();
+    PageCrawler pageCrawler = context.root.getPageCrawler();
+    WikiPagePath resourcePath = PathParser.parse(pageName);
+    page = pageCrawler.getPage(context.root, resourcePath);
+
+    if (page == null)
+      response.setStatus(404);
+    else {
+      buildPacket();
+      response.setStatus(200);
+    }
+    return response;
+  }
+
+  private void buildPacket() throws Exception {
+    packet = new JSONObject();
+    String html = page.getData().getHtml();
+    TableScanner scanner = new HtmlTableScanner(html);
+    addTablesToPacket(scanner);
+    response.setContent(packet.toString(1));
+  }
+
+  private void addTablesToPacket(TableScanner scanner) throws JSONException {
+    for (int i = 0; i < scanner.getTableCount(); i++) {
+      Table t = scanner.getTable(i);
+      addTableToPacket(t);
+    }
+    packet.put("tables", tables);
+  }
+
+  private void addTableToPacket(Table t) throws JSONException {
+    JSONObject table = new JSONObject();
+    JSONObject parents[] = new JSONObject[10];
+    parents[0] = table;
+    for (int row = 0; row < t.getRowCount(); row++) {
+      List<String> rowList = getRowFromTable(t, row);
+      int indent = getIndent(rowList);
+      if (indent >= 0) {
+        String name = rowList.get(indent);
+        String value = rowList.size() > (indent + 1) ? rowList.get(indent + 1) : "";
+        if (null == value || "".equals(value)) {
+          JSONObject parent = new JSONObject();
+          parents[indent].put(name, parent);
+          parents[indent + 1] = parent;
+        } else {
+          parents[indent].put(name, value);
+        }
+      }
+    }
+    tables.add(table);
+  }
+
+  private int getIndent(List<String> rowList) {
+    for (int indent = 0; indent < rowList.size(); indent++) {
+      if ("".equals(rowList.get(indent)) == false)
+        return indent;
+    }
+    return -1;
+  }
+
+  private List<String> getRowFromTable(Table t, int row) {
+    List<String> rowList = new ArrayList<String>();
+    for (int col = 0; col < t.getColumnCountInRow(row); col++)
+      rowList.add(t.getCellContents(col, row));
+    return rowList;
+  }
+}
---
+++ src/fitnesse/responders/PacketResponderTest.java
@@ -0,0 +1,125 @@
+package fitnesse.responders;
+
+import fitnesse.FitNesseContext;
+import fitnesse.Responder;
+import fitnesse.http.MockRequest;
+import fitnesse.http.SimpleResponse;
+import fitnesse.wiki.InMemoryPage;
+import fitnesse.wiki.PageCrawler;
+import fitnesse.wiki.PathParser;
+import fitnesse.wiki.WikiPage;
+import org.json.JSONObject;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PacketResponderTest {
+  protected WikiPage root;
+  protected MockRequest request;
+  protected Responder responder;
+  protected PageCrawler crawler;
+  protected FitNesseContext context;
+  private SimpleResponse response;
+
+  @Before
+  public void setUp() throws Exception {
+    root = InMemoryPage.makeRoot("RooT");
+    crawler = root.getPageCrawler();
+    request = new MockRequest();
+    responder = new PacketResponder();
+    context = new FitNesseContext(root);
+  }
+
+  private SimpleResponse makeResponse() throws Exception {
+    return (SimpleResponse) responder.makeResponse(context, request);
+  }
+
+  private void assertPageWithTableResponseWith(String table, String expected) throws Exception {
+    crawler.addPage(root, PathParser.parse("TablePage"), table);
+    request.setResource("TablePage");
+    SimpleResponse response = makeResponse();
+    assertEquals(expected, response.getContent());
+  }
+
+  @Test
+  public void noSuchPage() throws Exception {
+    request.setResource("NoSuchPage");
+    response = makeResponse();
+    assertEquals(404, response.getStatus());
+  }
+
+  @Test
+  public void pageWithNoTables() throws Exception {
+    crawler.addPage(root, PathParser.parse("SimplePage"), "simple content");
+    request.setResource("SimplePage");
+    SimpleResponse response = makeResponse();
+    assertEquals(200, response.getStatus());
+    assertEquals("{\"tables\": []}", response.getContent());
+  }
+
+  @Test
+  public void pageWithOneSimpleTable() throws Exception {
+    assertPageWithTableResponseWith("|cell|\n", "{\"tables\": [{\"cell\": {}}]}");
+  }
+
+  @Test
+  public void pageWithOneSimplePair() throws Exception {
+    assertPageWithTableResponseWith("|name|value|\n", "{\"tables\": [{\"name\": \"value\"}]}");
+  }
+
+  @Test
+  public void pageWithTwoPairs() throws Exception {
+    assertPageWithTableResponseWith("|name1|value1|\n|name2|value2|\n",
+      "{\"tables\": [{\n" +
+        " \"name1\": \"value1\",\n" +
+        " \"name2\": \"value2\"\n" +
+        "}]}");
+  }
+
+  @Test
+  public void twoTablesWithSimplePairs() throws Exception {
+    assertPageWithTableResponseWith("|n|v|\n\n|n2|v2|\n",
+      "{\"tables\": [\n" +
+        " {\"n\": \"v\"},\n" +
+        " {\"n2\": \"v2\"}\n" +
+        "]}");
+  }
+
+  @Test
+  public void oneTableWithNestedPair() throws Exception {
+    JSONObject expected = new JSONObject("{\"tables\": [{\"n\": {\"m\": \"v\"}}]}");
+    assertPageWithTableResponseWith("|n|\n||m|v|\n", expected.toString(1));
+  }
+
+  @Test
+  public void tableWithBlankLinesIgnored() throws Exception {
+    JSONObject expected = new JSONObject("{\"tables\": [{\"n\": {\"m\": \"v\"}}]}");
+    assertPageWithTableResponseWith("|n|\n||||\n||m|v|\n", expected.toString(1));
+  }
+
+  @Test
+  public void deeplyNestedTable() throws Exception {
+    String table =
+      "|bob|\n" +
+        "||Angela|\n" +
+        "|||Lexy|6|\n" +
+        "|||Sami|4|\n" +
+        "|||Mandy|2|\n" +
+        "||Micah|\n" +
+        "|||Luka|5|\n" +
+        "||Gina|\n" +
+        "||Justin|\n";
+    String expectedString = "{\"tables\": [{\"bob\": {\n" +
+      " \"Angela\": {\n" +
+      "  \"Lexy\": \"6\",\n" +
+      "  \"Mandy\": \"2\",\n" +
+      "  \"Sami\": \"4\"\n" +
+      " },\n" +
+      " \"Gina\": {},\n" +
+      " \"Justin\": {},\n" +
+      " \"Micah\": {\"Luka\": \"5\"}\n" +
+      "}}]}";
+    JSONObject expected = new JSONObject(expectedString);
+    assertPageWithTableResponseWith(table, expected.toString(1));
+  }
+}
