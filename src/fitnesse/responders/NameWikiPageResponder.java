46af854317179f81ccfcae87f3f658901d55397a
LupusDei <lupusdei108@gmail.com>
2009-06-01 15:07:10 -0500

    Add Child Page fix. And child count in name responder

--- src/fitnesse/responders/NameWikiPageResponder.java
+++ src/fitnesse/responders/NameWikiPageResponder.java
@@ -16,17 +16,18 @@ public class NameWikiPageResponder extends BasicWikiPageResponder {
     List<String> pages = new ArrayList<String>();
     for (Iterator<?> iterator = requestedPage.getChildren().iterator(); iterator.hasNext();) {
       WikiPage child = (WikiPage) iterator.next();
-      if (request.hasInput("ShowChildCount")){
-        String name = child.getName() +" " + Integer.toString(child.getChildren().size());
+      if (request.hasInput("ShowChildCount")) {
+        String name = child.getName() + " " + Integer.toString(child.getChildren().size());
         pages.add(name);
-      }
+      } else
+        pages.add(child.getName());
 
     }
 
     String format = (String) request.getInput("format");
     if ("json".equalsIgnoreCase(format)) {
       JSONArray jsonPages = new JSONArray(pages);
-      return jsonPages.toString(); 
+      return jsonPages.toString();
     }
     return StringUtil.join(pages, System.getProperty("line.separator"));
   }
