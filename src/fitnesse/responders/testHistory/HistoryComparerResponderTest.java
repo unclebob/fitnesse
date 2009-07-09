8c5c4f4b814fb231cfd3506fee41a724bd076bf1
LupusDei <lupusdei108@gmail.com>
2009-07-09 14:16:21 -0500

    Compare History feature added

---
+++ src/fitnesse/responders/testHistory/HistoryComparerResponderTest.java
@@ -0,0 +1,12 @@
+package fitnesse.responders.testHistory;
+
+import org.junit.Test;
+
+public class HistoryComparerResponderTest {
+
+  @Test
+  public void shouldBeAbleToMakeASimpleResponse() throws Exception {
+    HistoryComparerResponder responder = new HistoryComparerResponder();
+    responder.makeResponse()
+  }
+}
