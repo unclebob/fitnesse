aa1420926686d0f25c2c1d2db24a08069f2256b2
Gregor Gramlich <gramlich@eosc.de>
2009-08-02 16:31:30 +0200

    added unit test for BooleanConverter

--- src/fitnesse/slim/ConverterTest.java
+++ src/fitnesse/slim/ConverterTest.java
@@ -6,13 +6,48 @@ import java.util.Date;
 
 import org.junit.Test;
 
+import fitnesse.slim.converters.BooleanConverter;
 import fitnesse.slim.converters.DateConverter;
 
 public class ConverterTest {
   @Test
   public void convertDate() throws Exception {
-    DateConverter converter = new DateConverter();
-    Date date = (Date) converter.fromString("5-May-2009");
-    assertEquals("05-May-2009", converter.toString(date));
+    assertConverts(new DateConverter(), "05-May-2009");
+  }
+  
+  @Test
+  public void convertDateWithoutLeadingZero() throws Exception {
+    assertConverts("05-May-2009", new DateConverter(), "5-May-2009");
+  }
+
+  @Test
+  public void convertBooleanTrue() throws Exception {
+    BooleanConverter converter = new BooleanConverter();
+    assertConverts(converter, "true");
+    assertConverts("true", converter, "True");
+    assertConverts("true", converter, "TRUE");
+    assertConverts("true", converter, "YES");
+    assertConverts("true", converter, "yes");
+  }
+
+  @Test
+  public void convertBooleanFalse() throws Exception {
+    BooleanConverter converter = new BooleanConverter();
+    assertConverts(converter, "false");
+    assertConverts("false", converter, "FALSE");
+    assertConverts("false", converter, "False");
+    assertConverts("false", converter, "no");
+    assertConverts("false", converter, "NO");
+    assertConverts("false", converter, "0");
+    assertConverts("false", converter, "1");
+    assertConverts("false", converter, "x");
+  }
+  
+  private void assertConverts(Converter converter, String value) {
+    assertConverts(value, converter, value);
+  }
+  
+  private void assertConverts(String expected, Converter converter, String value) {
+    assertEquals(expected, converter.toString(converter.fromString(value)));
   }
 }
