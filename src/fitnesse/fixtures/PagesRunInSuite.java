4d8e13273bada334754c55319976e6679cef2013
unclebob <unclebob@objectmentor.com>
2009-08-07 10:24:43 -0500

    decrap fixture method

--- src/fitnesse/fixtures/PagesRunInSuite.java
+++ src/fitnesse/fixtures/PagesRunInSuite.java
@@ -14,23 +14,39 @@ public class PagesRunInSuite {
   }
 
   public List<Object> query() throws Exception {
+    return buildQueryResponse(getPagesRunInSuite());
+
+  }
+
+  private List<String> getPagesRunInSuite() throws Exception {
     List<String> testPages = new ArrayList<String>();
 
     String content = FitnesseFixtureContext.sender.sentData();
     String testSystems[] = content.split("slim:");
     for (String testSystem : testSystems) {
-      if (testSystem.startsWith(testSystemID)) {
-        String fragments[] = testSystem.split("test_name\">");
-        for (int i = 1; i < fragments.length; i++) {
-          int fragmentEnd = fragments[i].indexOf("<");
-          String testPage = fragments[i].substring(0, fragmentEnd);
-          if (testPage.startsWith(prefix)) {
-            testPages.add(testPage);
-          }
-        }
+      addPagesForThisTestSystem(testPages, testSystem);
+    }
+    return testPages;
+  }
+
+  private void addPagesForThisTestSystem(List<String> testPages, String testSystem) {
+    if (testSystem.startsWith(testSystemID)) {
+      String pageString[] = testSystem.split("test_name\">");
+      addPages(testPages, pageString);
+    }
+  }
+
+  private void addPages(List<String> testPages, String[] pageString) {
+    for (int i = 1; i < pageString.length; i++) {
+      int fragmentEnd = pageString[i].indexOf("<");
+      String testPage = pageString[i].substring(0, fragmentEnd);
+      if (testPage.startsWith(prefix)) {
+        testPages.add(testPage);
       }
     }
+  }
 
+  private List<Object> buildQueryResponse(List<String> testPages) {
     List<Object> rows = new ArrayList<Object>();
     for (int i = 0; i < testPages.size(); i++) {
       List<Object> columns = new ArrayList<Object>();
@@ -38,8 +54,6 @@ public class PagesRunInSuite {
       columns.add(pageNameCell);
       rows.add(columns);
     }
-
     return rows;
-
   }
 }
