d42015aea9760e4365de47c0d2bd984b290f0c69
unclebob <unclebob@objectmentor.com>
2009-08-12 15:59:43 -0500

    Fixed #153, Duplicate Rows in OrderedQuery sometimes fails

--- src/fitnesse/fixtures/DuplicateRows.java
+++ src/fitnesse/fixtures/DuplicateRows.java
@@ -4,30 +4,63 @@ import java.util.Arrays;
 import java.util.List;
 
 public class DuplicateRows {
+  private String phase;
+
+  public DuplicateRows(String phase) {
+    this.phase = phase;
+  }
+
   public List<Object> query() {
-    List results = Arrays.asList(
-      Arrays.asList(
-        Arrays.asList("x", "SuiteChildOne.SuiteSetUp")
-      ),
-      Arrays.asList(
-        Arrays.asList("x", "SuiteChildOne.TestOneOne")
-      ),
-      Arrays.asList(
-        Arrays.asList("x", "SuiteChildOne.TestOneTwo")
-      ),
-      Arrays.asList(
-        Arrays.asList("x", "SuiteChildOne.SuiteTearDown")
-      ),
-      Arrays.asList(
-        Arrays.asList("x", "SuiteChildOne.SuiteSetUp")
-      ),
-      Arrays.asList(
-        Arrays.asList("x", "SuiteChildOne.TestOneThree")
-      ),
-      Arrays.asList(
-        Arrays.asList("x", "SuiteChildOne.SuiteTearDown")
-      )
-    );
+    List results;
+    if ("A".equalsIgnoreCase(phase)) {
+      results = Arrays.asList(
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.SuiteSetUp")
+        ),
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.TestOneOne")
+        ),
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.TestOneTwo")
+        ),
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.SuiteTearDown")
+        ),
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.SuiteSetUp")
+        ),
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.TestOneThree")
+        ),
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.SuiteTearDown")
+        )
+      );
+    } else {
+      results = Arrays.asList(
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.SuiteSetUp")
+        ),
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.TestOneThree")
+        ),
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.SuiteTearDown")
+        ),
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.SuiteSetUp")
+        ),
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.TestOneOne")
+        ),
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.TestOneTwo")
+        ),
+        Arrays.asList(
+          Arrays.asList("x", "SuiteChildOne.SuiteTearDown")
+        )
+      );
+    }
     return results;
 
   }
