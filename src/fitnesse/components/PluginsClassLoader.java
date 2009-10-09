5fd8dca3003aa7b973206e3e0d1da86dfdcc6f15
Tim Andersen <timander@gmail.com>
2009-10-09 18:15:13 -0500

    applied the code style using fitnesse.xml and wrote a test for plugins directory not existing

--- src/fitnesse/components/PluginsClassLoader.java
+++ src/fitnesse/components/PluginsClassLoader.java
@@ -1,7 +1,6 @@
 package fitnesse.components;
 
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -10,44 +9,37 @@ import java.net.URLClassLoader;
 
 public class PluginsClassLoader {
 
-    protected String pluginsDirectory = "plugins";
+  protected String pluginsDirectory = "plugins";
 
-    public void addPluginsToClassLoader() {
-        File pluginsDir = new File(pluginsDirectory);
-        if (pluginsDir.exists()) {
-            for (File plugin : pluginsDir.listFiles()) {
-                if (plugin.getName().endsWith("jar")) {
-                    addFile(plugin);
-                }
-            }
+  public void addPluginsToClassLoader() {
+    File pluginsDir = new File(pluginsDirectory);
+    if (pluginsDir.exists()) {
+      for (File plugin : pluginsDir.listFiles()) {
+        if (plugin.getName().endsWith("jar")) {
+          addFile(plugin);
         }
+      }
     }
+  }
 
-
-    public void addFile(File plugin) {
-        try {
-            addURL(plugin.toURI().toURL());
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
+  public void addFile(File plugin) {
+    try {
+      addURL(plugin.toURI().toURL());
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
     }
+  }
 
-
-    public void addURL(URL url) {
-        URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
-        Class sysclass = URLClassLoader.class;
-        try {
-            Method method = sysclass.getDeclaredMethod("addURL", new Class[]{URL.class});
-            method.setAccessible(true);
-            method.invoke(sysloader, url);
-        } catch (NoSuchMethodException nsme) {
-            throw new RuntimeException("Error, could not add URL to system classloader", nsme);
-        } catch (InvocationTargetException ite) {
-            throw new RuntimeException("Error, could not add URL to system classloader", ite);
-        } catch (IllegalAccessException iae) {
-            throw new RuntimeException("Error, could not add URL to system classloader", iae);
-        }
+  public void addURL(URL url) {
+    URLClassLoader sysloader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+    Class sysclass = URLClassLoader.class;
+    try {
+      Method method = sysclass.getDeclaredMethod("addURL", new Class[]{URL.class});
+      method.setAccessible(true);
+      method.invoke(sysloader, url);
+    } catch (Exception e) {
+      throw new RuntimeException("Error, could not add URL to system classloader", e);
     }
-
+  }
 
 }
--- src/fitnesse/components/PluginsClassLoaderTest.java
+++ src/fitnesse/components/PluginsClassLoaderTest.java
@@ -3,49 +3,54 @@ package fitnesse.components;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.io.File;
+
 
 public class PluginsClassLoaderTest {
 
-    @Test
-    public void whenPluginsDirectoryDoesNotExist() {
-        try {
-            PluginsClassLoader pluginsClassLoader = new PluginsClassLoader();
-            pluginsClassLoader.pluginsDirectory = "nonExistingPluginsDirectory";
-            pluginsClassLoader.addPluginsToClassLoader();
-            assertTrue("didn't cause exception", true);
-        } catch (Exception e) {
-            fail("if exception occurs when plugins directory does not exist");
-        }
+  @Test
+  public void whenPluginsDirectoryDoesNotExist() {
+    try {
+      PluginsClassLoader pluginsClassLoader = new PluginsClassLoader();
+      pluginsClassLoader.pluginsDirectory = "nonExistingPluginsDirectory";
+      assertFalse(new File(pluginsClassLoader.pluginsDirectory).exists());
+      pluginsClassLoader.addPluginsToClassLoader();
+      assertTrue("didn't cause exception", true);
+    } catch (Exception e) {
+      fail("if exception occurs when plugins directory does not exist");
     }
+  }
 
-    @Test
-    public void addPluginsToClassLoader() {
-        String[] dynamicClasses = new String[]{"fitnesse.testing.PluginX", "fitnesse.testing.PluginY"};
-        assertLoadingClassCausesException(dynamicClasses);
-        new PluginsClassLoader().addPluginsToClassLoader();
-        assertLoadingClassWorksNow(dynamicClasses);
-    }
+  @Test
+  public void addPluginsToClassLoader() {
+    String[] dynamicClasses = new String[]{"fitnesse.testing.PluginX", "fitnesse.testing.PluginY"};
+    assertLoadingClassCausesException(dynamicClasses);
+    PluginsClassLoader pluginsClassLoader = new PluginsClassLoader();
+    assertTrue(new File(pluginsClassLoader.pluginsDirectory).exists());
+    pluginsClassLoader.addPluginsToClassLoader();
+    assertLoadingClassWorksNow(dynamicClasses);
+  }
 
-    private void assertLoadingClassWorksNow(String... dynamicClasses) {
-        for (String dynamicClass : dynamicClasses) {
-            try {
-                Class<?> dynamicallyLoadedClass = Class.forName(dynamicClass);
-                assertEquals(dynamicClass, dynamicallyLoadedClass.getName());
-            } catch (ClassNotFoundException e) {
-                fail(e.getMessage());
-            }
-        }
+  private void assertLoadingClassWorksNow(String... dynamicClasses) {
+    for (String dynamicClass : dynamicClasses) {
+      try {
+        Class<?> dynamicallyLoadedClass = Class.forName(dynamicClass);
+        assertEquals(dynamicClass, dynamicallyLoadedClass.getName());
+      } catch (ClassNotFoundException e) {
+        fail(e.getMessage());
+      }
     }
+  }
 
-    private void assertLoadingClassCausesException(String... dynamicClasses) {
-        for (String dynamicClass : dynamicClasses) {
-            try {
-                Class.forName(dynamicClass);
-                fail("plugins are not yet added to the classloader");
-            } catch (ClassNotFoundException e) {
-                assertEquals(dynamicClass, e.getMessage());
-            }
-        }
+  private void assertLoadingClassCausesException(String... dynamicClasses) {
+    for (String dynamicClass : dynamicClasses) {
+      try {
+        Class.forName(dynamicClass);
+        fail("plugins are not yet added to the classloader");
+      } catch (ClassNotFoundException e) {
+        assertEquals(dynamicClass, e.getMessage());
+      }
     }
+  }
 
 }
