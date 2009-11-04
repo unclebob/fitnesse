a7242991039970d769db681afa2336285700d421
unclebob <unclebob@objectmentor.com>
2009-11-03 18:11:59 -0600

    Fix bug in variable replacement.

--- src/fitnesse/wikitext/widgets/VariableWidget.java
+++ src/fitnesse/wikitext/widgets/VariableWidget.java
@@ -2,10 +2,10 @@
 // Released under the terms of the CPL Common Public License version 1.0.
 package fitnesse.wikitext.widgets;
 
+import fitnesse.html.HtmlUtil;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import fitnesse.html.HtmlUtil;
 
 public class VariableWidget extends ParentWidget {
   public static final String REGEXP = "\\$\\{[\\w\\.]+\\}";
@@ -30,11 +30,21 @@ public class VariableWidget extends ParentWidget {
   }
 
   private void doRender() throws Exception {
+    //Todo: The nonlocalities in this function are truly horrendous.  There is a loop in the widget builder that
+    //reapplies the variable widgets until all the variables have been replaced.  This function is part of the
+    //body of that loop.  The parent.addVariable statement ensures that the value of a variable is updated
+    //each time through the loop which makes sure that nested variables are expressed properly.
+    //The if (matcher.find()) statement ensures that if there are no more nested variables to express, we don't
+    //continue to change the value of the variable.  If you don't understand that, well, I'm not sure I do either.
+    //The whole variable mechanism needs to be completely redone...
+
     String value = parent.getVariable(name);
     if (value != null) {
+      Matcher matcher = pattern.matcher(value);
       addChildWidgets(value);
       renderedText = childHtml();
-      parent.addVariable(name, renderedText);
+      if (matcher.find())
+        parent.addVariable(name, renderedText);
     } else
       renderedText = makeUndefinedVariableExpression(name);
     rendered = true;
