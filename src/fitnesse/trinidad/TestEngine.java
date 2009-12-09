b6a06e33a2ef50f80d7c681483bd8654388b03d8
unclebob <unclebob@objectmentor.com>
2009-12-09 11:10:12 -0600

    Unbundled Trinidad

--- src/fitnesse/trinidad/TestEngine.java
+++
@@ -1,7 +0,0 @@
-/** this class is adapted from the trinidad project (http://fitnesse.info/trinidad) */
-
-package fitnesse.trinidad;
-
-public interface TestEngine {
-  public TestResult runTest(TestDescriptor test);
-}
