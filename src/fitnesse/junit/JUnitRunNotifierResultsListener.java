50908d016c4daae060f3cf81263320baebb6a0da
Gregor Gramlich <gramlich@gramlich-develop.local.gra>
2010-01-17 10:22:25 +0100

    Fixed superfluous @Override annotations, applied autoformat

--- src/fitnesse/junit/JUnitRunNotifierResultsListener.java
+++ src/fitnesse/junit/JUnitRunNotifierResultsListener.java
@@ -11,51 +11,53 @@ import fitnesse.responders.run.TestSystem;
 import fitnesse.wiki.WikiPage;
 import fitnesse.wiki.WikiPagePath;
 
-public class JUnitRunNotifierResultsListener implements ResultsListener{
-    private final Class<?> mainClass;
-    private final RunNotifier notifier;
-    public JUnitRunNotifierResultsListener(RunNotifier notifier, Class<?> mainClass){
-      this.notifier = notifier;
-      this.mainClass = mainClass;
-    }
-    @Override
-    public void allTestingComplete() throws Exception {
+public class JUnitRunNotifierResultsListener implements ResultsListener {
+  private final Class<?> mainClass;
+  private final RunNotifier notifier;
 
-    }
-    @Override
-    public void announceNumberTestsToRun(int testsToRun) {
-      
-    }
-    @Override
-    public void errorOccured() {
-      
-    }
-    @Override
-    public void newTestStarted(WikiPage test, long time) throws Exception {
-      notifier.fireTestStarted(descriptionFor(test));
-    }
-    private Description descriptionFor(WikiPage test) throws Exception {
-      return Description.createTestDescription(mainClass, new WikiPagePath(test).toString());
-    }
-    @Override
-    public void setExecutionLogAndTrackingId(String stopResponderId, CompositeExecutionLog log)
-        throws Exception {
-    }
-    @Override
-    public void testComplete(WikiPage test, TestSummary testSummary) throws Exception {
-      if (testSummary.wrong == 0 && testSummary.exceptions == 0) {
-        notifier.fireTestFinished(descriptionFor(test));
-      } else {
-        notifier.fireTestFailure(new Failure(descriptionFor(test), new AssertionError(
-            "wrong: " + testSummary.wrong + " exceptions: " + testSummary.exceptions)));
-      }
-    }
-    @Override
-    public void testOutputChunk(String output) throws Exception {
-
-    }
-    public void testSystemStarted(TestSystem testSystem, String testSystemName, String testRunner)
-        throws Exception {
-    }    
+  public JUnitRunNotifierResultsListener(RunNotifier notifier, Class<?> mainClass) {
+    this.notifier = notifier;
+    this.mainClass = mainClass;
   }
 
+  public void allTestingComplete() throws Exception {
+
+  }
+
+  public void announceNumberTestsToRun(int testsToRun) {
+
+  }
+
+  public void errorOccured() {
+
+  }
+
+  public void newTestStarted(WikiPage test, long time) throws Exception {
+    notifier.fireTestStarted(descriptionFor(test));
+  }
+
+  private Description descriptionFor(WikiPage test) throws Exception {
+    return Description.createTestDescription(mainClass, new WikiPagePath(test).toString());
+  }
+
+  public void setExecutionLogAndTrackingId(String stopResponderId, CompositeExecutionLog log)
+      throws Exception {
+  }
+
+  public void testComplete(WikiPage test, TestSummary testSummary) throws Exception {
+    if (testSummary.wrong == 0 && testSummary.exceptions == 0) {
+      notifier.fireTestFinished(descriptionFor(test));
+    } else {
+      notifier.fireTestFailure(new Failure(descriptionFor(test), new AssertionError("wrong: "
+          + testSummary.wrong + " exceptions: " + testSummary.exceptions)));
+    }
+  }
+
+  public void testOutputChunk(String output) throws Exception {
+
+  }
+
+  public void testSystemStarted(TestSystem testSystem, String testSystemName, String testRunner)
+      throws Exception {
+  }
+}
--- src/fitnesse/junit/PrintTestListener.java
+++ src/fitnesse/junit/PrintTestListener.java
@@ -7,36 +7,37 @@ import fitnesse.responders.run.TestSystem;
 import fitnesse.wiki.WikiPage;
 import fitnesse.wiki.WikiPagePath;
 
-public class PrintTestListener implements ResultsListener{
-    @Override
-    public void allTestingComplete() throws Exception {
-      System.out.println("--complete--");
-    }
-    @Override
-    public void announceNumberTestsToRun(int testsToRun) {
-      
-    }
-    @Override
-    public void errorOccured() {
-    }
-    @Override
-    public void newTestStarted(WikiPage test, long time) throws Exception {
-      
-    }
-    @Override
-    public void setExecutionLogAndTrackingId(String stopResponderId, CompositeExecutionLog log)
-        throws Exception {
-      
-    }
-    @Override
-    public void testComplete(WikiPage test, TestSummary testSummary) throws Exception {
-      System.out.println(new WikiPagePath(test).toString() + " r " + testSummary.right + " w "+ testSummary.wrong + " "+  testSummary.exceptions );
-    }
-    @Override
-    public void testOutputChunk(String output) throws Exception {
+public class PrintTestListener implements ResultsListener {
+  public void allTestingComplete() throws Exception {
+    System.out.println("--complete--");
+  }
 
-    }
-    public void testSystemStarted(TestSystem testSystem, String testSystemName, String testRunner)
-        throws Exception {
-    }    
+  public void announceNumberTestsToRun(int testsToRun) {
+
+  }
+
+  public void errorOccured() {
+  }
+
+  public void newTestStarted(WikiPage test, long time) throws Exception {
+
+  }
+
+  public void setExecutionLogAndTrackingId(String stopResponderId, CompositeExecutionLog log)
+      throws Exception {
+
+  }
+
+  public void testComplete(WikiPage test, TestSummary testSummary) throws Exception {
+    System.out.println(new WikiPagePath(test).toString() + " r " + testSummary.right + " w "
+        + testSummary.wrong + " " + testSummary.exceptions);
+  }
+
+  public void testOutputChunk(String output) throws Exception {
+
+  }
+
+  public void testSystemStarted(TestSystem testSystem, String testSystemName, String testRunner)
+      throws Exception {
+  }
 }
