531f90e9af946893aa68c9660a81b316120ed563
Gregor Gramlich <gramlich@gramlich-develop.local.gra>
2010-01-24 22:28:58 +0100

    Extracted StatementExecutorTestBase

--- src/fitnesse/slim/StatementExecutorTest.java
+++ src/fitnesse/slim/StatementExecutorTest.java
@@ -1,222 +1,147 @@
 package fitnesse.slim;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Before;
-import org.junit.Test;
 
-public class StatementExecutorTest {
-  private static final String INSTANCE_NAME = "myInstance";
-  private static final String MESSAGE_NO_METHOD_IN_CLASS = "message:<<NO_METHOD_IN_CLASS %s[%d] %s.>>";
-  private int library = 0;
-  private StatementExecutor statementExecutor;
+public class StatementExecutorTest extends StatementExecutorTestBase {
 
-  public static class MySystemUnderTest {
-    public boolean speakCalled = false;
-    public boolean echoCalled = false;
-    
+  protected String voidMessage() {
+    return "/__VOID__/";
+  }
+
+  protected String echoMethodName() {
+    return "echo";
+  }
+
+  public static class MySystemUnderTestJava extends MySystemUnderTestBase {
+    private boolean echoCalled = false;
+    private boolean speakCalled;
+
     public void speak() {
       speakCalled = true;
     }
-    
+
+    public boolean speakCalled() {
+      return speakCalled;
+    }
+
     public void echo() {
       echoCalled = true;
     }
+
+    public boolean echoCalled() {
+      return echoCalled;
+    }
   }
 
-  public static class MyAnnotatedSystemUnderTestFixture {
-    public boolean called = false;
-
+  public static class MyAnnotatedSystemUnderTestFixtureJava extends
+      MyAnnotatedSystemUnderTestFixture {
     @SystemUnderTest
-    public MySystemUnderTest sut = new MySystemUnderTest();
+    public MySystemUnderTestBase sut = new MySystemUnderTestJava();
+    private boolean echoCalled = false;
 
-    public void echo() {
-      called = true;
-    }
-  }
-
-  public static class FixtureWithNamedSystemUnderTest {
-    public boolean called = false;
-
-    public MySystemUnderTest systemUnderTest = new MySystemUnderTest();
-
-    public void echo() {
-      called = true;
-    }
-  }
-  
-  public static class SimpleFixture {
-    public boolean echoCalled = false;
     public void echo() {
       echoCalled = true;
     }
+
+    public boolean echoCalled() {
+      return echoCalled;
+    }
+
+    public MySystemUnderTestBase getSystemUnderTest() {
+      return sut;
+    }
   }
-  public static class EchoSupport {
-    public boolean echoCalled = false;
-    public boolean speakCalled = false;
+
+  public static class FixtureWithNamedSystemUnderTestJava extends FixtureWithNamedSystemUnderTestBase {
+    public MySystemUnderTestBase systemUnderTest = new MySystemUnderTestJava();
+    private boolean echoCalled;
+
     public void echo() {
       echoCalled = true;
     }
+
+    public boolean echoCalled() {
+      return echoCalled;
+    }
+
+    public MySystemUnderTestBase getSystemUnderTest() {
+      return systemUnderTest;
+    }
+  }
+
+  public static class SimpleFixtureJava extends SimpleFixture {
+    private boolean echoCalled;
+
+    public void echo() {
+      echoCalled = true;
+    }
+
+    public boolean echoCalled() {
+      return echoCalled;
+    }
+  }
+
+  public static class EchoSupportJava extends EchoSupport {
+    private boolean echoCalled;
+    private boolean speakCalled;
+
+    public void echo() {
+      echoCalled = true;
+    }
+
     public void speak() {
       speakCalled = true;
     }
-  }
-  
-  public static class FileSupport {
-    public boolean called;
-    public void delete(String fileName) {
-      called = true;
+
+    public boolean speakCalled() {
+      return speakCalled;
+    }
+
+    public boolean echoCalled() {
+      return echoCalled;
     }
   }
 
+  public static class FileSupportJava extends FileSupport {
+    private boolean deleteCalled;
+
+    public void delete(String fileName) {
+      deleteCalled = true;
+    }
+
+    public boolean deleteCalled() {
+      return deleteCalled;
+    }
+  }
+
+  @Override
   @Before
   public final void init() {
     statementExecutor = new StatementExecutor();
   }
 
-  @Test
-  public void shouldCallMethodOnGivenInstanceBeforeTryingToInvokeOnSystemUnderTest() {
-    MyAnnotatedSystemUnderTestFixture myInstance = createAnnotatedFixture();
-
-    Object result = statementExecutor.call(INSTANCE_NAME, "echo");
-    assertEquals("/__VOID__/", result);
-    assertTrue(myInstance.called);
-    assertFalse(myInstance.sut.speakCalled);
+  @Override
+  protected String annotatedFixtureName() {
+    return MyAnnotatedSystemUnderTestFixtureJava.class.getName();
   }
 
-  private MyAnnotatedSystemUnderTestFixture createAnnotatedFixture() {
-    createFixtureInstance(MyAnnotatedSystemUnderTestFixture.class);
-
-    MyAnnotatedSystemUnderTestFixture myInstance = (MyAnnotatedSystemUnderTestFixture) statementExecutor.getInstance(INSTANCE_NAME);
-    assertFalse(myInstance.called);
-    return myInstance;
+  @Override
+  protected String namedFixtureName() {
+    return FixtureWithNamedSystemUnderTestJava.class.getName();
   }
 
-  private void createFixtureInstance(Class<?> fixtureClass) {
-    Object created = statementExecutor.create(INSTANCE_NAME, ((Class<?>) fixtureClass).getName(),
-        new Object[] {});
-    assertEquals("OK", created);
+  @Override
+  protected String simpleFixtureName() {
+    return SimpleFixtureJava.class.getName();
   }
 
-  @Test
-  public void shouldCallMethodOnFieldAnnotatedWithSystemUnderTestWhenFixtureDoesNotHaveMethod() {
-    MyAnnotatedSystemUnderTestFixture myFixture = createAnnotatedFixture();
-
-    executeStatementAndVerifyResultIsVoid();
-    assertFalse(myFixture.called);
-    assertTrue(myFixture.sut.speakCalled);
-  }
-  
-  @Test
-  public void shouldCallMethodOnFieldNamed_systemUnderTest_WhenFixtureDoesNotHaveMethod() {
-    FixtureWithNamedSystemUnderTest myFixture = createNamedFixture();
-    
-    executeStatementAndVerifyResultIsVoid();
-    assertFalse(myFixture.called);
-    assertTrue(myFixture.systemUnderTest.speakCalled);
+  @Override
+  protected String echoLibraryName() {
+    return EchoSupportJava.class.getName();
   }
 
-  private void executeStatementAndVerifyResultIsVoid() {
-    Object result = statementExecutor.call(INSTANCE_NAME, "speak");
-    assertEquals("/__VOID__/", result);
-  }
-  
-  private FixtureWithNamedSystemUnderTest createNamedFixture() {
-    createFixtureInstance(FixtureWithNamedSystemUnderTest.class);
-    FixtureWithNamedSystemUnderTest myInstance = (FixtureWithNamedSystemUnderTest) statementExecutor.getInstance(INSTANCE_NAME);
-    assertFalse(myInstance.called);
-    return myInstance;
-  }
-  private SimpleFixture createSimpleFixture() {
-    createFixtureInstance(SimpleFixture.class);
-    SimpleFixture myInstance = (SimpleFixture) statementExecutor.getInstance(INSTANCE_NAME);
-    return myInstance;
-  }
-
-  @Test
-  public void shouldReportMissingMethodOnFixtureClassWhenMethodCanNotBeFoundOnBothFixtureAndSystemUnderTest() {
-    createAnnotatedFixture();
-    String result = (String) statementExecutor.call(INSTANCE_NAME, "noSuchMethod");
-    String expectedErrorMessage = String.format(MESSAGE_NO_METHOD_IN_CLASS, "noSuchMethod", 0,
-        MyAnnotatedSystemUnderTestFixture.class.getName());
-    assertTrue(result.contains(expectedErrorMessage));
-  }
-  
-  @Test
-  public void shouldPreferMethodOnFixtureOverMethodOnSystemUnderTest() {
-    FixtureWithNamedSystemUnderTest instance = createNamedFixture();
-    statementExecutor.call(INSTANCE_NAME, "echo");
-    assertFalse(instance.systemUnderTest.echoCalled);
-    assertTrue(instance.called);
-  }
-
-  @Test
-  public void shouldPreferMethodOnFixtureOverMethodOnLibrary() {
-    SimpleFixture instance = createSimpleFixture();
-    EchoSupport echoLibrary = createEchoLibrary();
-    statementExecutor.call(INSTANCE_NAME, "echo");
-    assertFalse(echoLibrary.echoCalled);
-    assertTrue(instance.echoCalled);
-  }
-  
-  @Test
-  public void shouldPreferMethodOnSystemUnderTestOverMethodOnLibrary() {
-    FixtureWithNamedSystemUnderTest instance = createNamedFixture();
-    EchoSupport echoLibrary = createEchoLibrary();
-    
-    statementExecutor.call(INSTANCE_NAME, "speak");
-    assertFalse(echoLibrary.speakCalled);
-    assertTrue(instance.systemUnderTest.speakCalled);
-  }
-  
-  @Test
-  public void shouldPreferMethodsOnLibrariesCreatedLaterOverMethodsOnLibrariesCreatedEarlier() {
-    createSimpleFixture();
-    EchoSupport echoLibrary1 = createEchoLibrary();
-    EchoSupport echoLibrary2 = createEchoLibrary();
-    EchoSupport echoLibrary3 = createEchoLibrary();
-    statementExecutor.call(INSTANCE_NAME, "speak");
-    assertFalse(echoLibrary1.speakCalled);
-    assertFalse(echoLibrary2.speakCalled);
-    assertTrue(echoLibrary3.speakCalled);
-  }
-  
-  private EchoSupport createEchoLibrary() {
-    String instanceName = "library" + library++;
-    statementExecutor.create(instanceName, EchoSupport.class.getName(), new Object[]{});
-    return (EchoSupport) statementExecutor.getInstance(instanceName);
-    
-  }
-  
-  @Test
-  public void shouldCallMethodOnInstallLibraryWhenMethodIsNotFoundInAFixture_WithSystemUnderTestInFixture() {
-    createNamedFixture();
-    FileSupport library1 = createFileSupportLibrary();
-    assertNotNull(library1);
-    Object result = statementExecutor.call(INSTANCE_NAME, "delete", "filename.txt");
-    assertEquals("/__VOID__/", result);
-    assertTrue(library1.called);
-    
-  }
-
-  private FileSupport createFileSupportLibrary() {
-    String instanceName = "library" + library++;
-    statementExecutor.create(instanceName, FileSupport.class.getName(), new Object[]{});
-    FileSupport library1 = (FileSupport) statementExecutor.getInstance(instanceName);
-    return library1;
-  }
-  
-  @Test
-  public void shouldCallMethodOnInstallLibraryWhenMethodIsNotFoundInAFixture() {
-    createFixtureInstance(EchoSupport.class);
-    FileSupport library1 = createFileSupportLibrary();
-    assertNotNull(library1);
-    Object result = statementExecutor.call(INSTANCE_NAME, "delete", "filename.txt");
-    assertEquals("/__VOID__/", result);
-    assertTrue(library1.called);
-    
+  @Override
+  protected String fileSupportName() {
+    return FileSupportJava.class.getName();
   }
 }
---
+++ src/fitnesse/slim/StatementExecutorTestBase.java
@@ -0,0 +1,215 @@
+package fitnesse.slim;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+// Extracted Test class to be implemented by all Java based Slim ports
+// The tests for PhpSlim and JsSlim implement this class
+
+public abstract class StatementExecutorTestBase {
+
+  protected static final String MESSAGE_NO_METHOD_IN_CLASS = "message:<<NO_METHOD_IN_CLASS %s[%d] %s.>>";
+  protected static final String INSTANCE_NAME = "myInstance";
+  protected StatementExecutorInterface statementExecutor;
+
+  // some untyped languages do not explicitely define void return values
+  protected abstract String voidMessage();
+
+  // echo is a keyword in some languages, allow for other methodNames
+  protected abstract String echoMethodName();
+
+  protected int library = 0;
+
+  public interface Echo {
+    public void echo();
+
+    public boolean echoCalled();
+  }
+
+  public interface Speak {
+    public void speak();
+
+    public boolean speakCalled();
+  }
+
+  public interface Delete {
+    public void delete(String fileName);
+
+    public boolean deleteCalled();
+  }
+
+  public interface SystemUnderTestFixture {
+    public MySystemUnderTestBase getSystemUnderTest();
+  }
+
+  public abstract static class MySystemUnderTestBase implements Speak, Echo {
+  }
+
+  public static abstract class MyAnnotatedSystemUnderTestFixture implements Echo,
+      SystemUnderTestFixture {
+  }
+
+  public static abstract class FixtureWithNamedSystemUnderTestBase implements Echo,
+      SystemUnderTestFixture {
+  }
+
+  public static abstract class SimpleFixture implements Echo {
+  }
+
+  public static abstract class EchoSupport implements Echo, Speak {
+  }
+
+  public static abstract class FileSupport implements Delete {
+  }
+
+  public abstract void init() throws Exception;
+
+  @Test
+  public void shouldCallMethodOnGivenInstanceBeforeTryingToInvokeOnSystemUnderTest() {
+    MyAnnotatedSystemUnderTestFixture myInstance = createAnnotatedFixture();
+    Object result = statementExecutor.call(INSTANCE_NAME, echoMethodName());
+    assertEquals(voidMessage(), result);
+    assertTrue(myInstance.echoCalled());
+    assertFalse(myInstance.getSystemUnderTest().speakCalled());
+  }
+
+  @Test
+  public void shouldCallMethodOnFieldAnnotatedWithSystemUnderTestWhenFixtureDoesNotHaveMethod() {
+    MyAnnotatedSystemUnderTestFixture myFixture = createAnnotatedFixture();
+    executeStatementAndVerifyResultIsVoid();
+    assertFalse(myFixture.echoCalled());
+    assertTrue(myFixture.getSystemUnderTest().speakCalled());
+  }
+
+  @Test
+  public void shouldCallMethodOnFieldNamed_systemUnderTest_WhenFixtureDoesNotHaveMethod() {
+    FixtureWithNamedSystemUnderTestBase myFixture = createNamedFixture();
+    executeStatementAndVerifyResultIsVoid();
+    assertFalse(myFixture.echoCalled());
+    assertTrue(myFixture.getSystemUnderTest().speakCalled());
+  }
+
+  @Test
+  public void shouldReportMissingMethodOnFixtureClassWhenMethodCanNotBeFoundOnBothFixtureAndSystemUnderTest() {
+    createAnnotatedFixture();
+    String result = (String) statementExecutor.call(INSTANCE_NAME, "noSuchMethod");
+    String expectedErrorMessage = String.format(MESSAGE_NO_METHOD_IN_CLASS, "noSuchMethod", 0,
+        annotatedFixtureName());
+    assertTrue(result.contains(expectedErrorMessage));
+  }
+
+  @Test
+  public void shouldPreferMethodOnFixtureOverMethodOnSystemUnderTest() {
+    FixtureWithNamedSystemUnderTestBase instance = createNamedFixture();
+    statementExecutor.call(INSTANCE_NAME, echoMethodName());
+    assertFalse(instance.getSystemUnderTest().echoCalled());
+    assertTrue(instance.echoCalled());
+  }
+
+  @Test
+  public void shouldPreferMethodOnFixtureOverMethodOnLibrary() {
+    SimpleFixture instance = createSimpleFixture();
+    EchoSupport echoLibrary = createEchoLibrary();
+    statementExecutor.call(INSTANCE_NAME, echoMethodName());
+    assertFalse(echoLibrary.echoCalled());
+    assertTrue(instance.echoCalled());
+  }
+
+  @Test
+  public void shouldPreferMethodOnSystemUnderTestOverMethodOnLibrary() {
+    FixtureWithNamedSystemUnderTestBase instance = createNamedFixture();
+    EchoSupport echoLibrary = createEchoLibrary();
+    statementExecutor.call(INSTANCE_NAME, "speak");
+    assertFalse(echoLibrary.speakCalled());
+    assertTrue(instance.getSystemUnderTest().speakCalled());
+  }
+
+  @Test
+  public void shouldPreferMethodsOnLibrariesCreatedLaterOverMethodsOnLibrariesCreatedEarlier() {
+    createSimpleFixture();
+    EchoSupport echoLibrary1 = createEchoLibrary();
+    EchoSupport echoLibrary2 = createEchoLibrary();
+    EchoSupport echoLibrary3 = createEchoLibrary();
+    statementExecutor.call(INSTANCE_NAME, "speak");
+    assertFalse(echoLibrary1.speakCalled());
+    assertFalse(echoLibrary2.speakCalled());
+    assertTrue(echoLibrary3.speakCalled());
+  }
+
+  @Test
+  public void shouldCallMethodOnInstallLibraryWhenMethodIsNotFoundInAFixture_WithSystemUnderTestInFixture() {
+    createNamedFixture();
+    FileSupport library = createFileSupportLibrary();
+    assertNotNull(library);
+    Object result = statementExecutor.call(INSTANCE_NAME, "delete", "filename.txt");
+    assertEquals(voidMessage(), result);
+    assertTrue(library.deleteCalled());
+  }
+
+  @Test
+  public void shouldCallMethodOnInstallLibraryWhenMethodIsNotFoundInAFixture() {
+    createFixtureInstance(echoLibraryName());
+    FileSupport library = createFileSupportLibrary();
+    assertNotNull(library);
+    Object result = statementExecutor.call(INSTANCE_NAME, "delete", "filename.txt");
+    assertEquals(voidMessage(), result);
+    assertTrue(library.deleteCalled());
+  }
+
+  protected MyAnnotatedSystemUnderTestFixture createAnnotatedFixture() {
+    createFixtureInstance(annotatedFixtureName());
+    return (MyAnnotatedSystemUnderTestFixture) getVerifiedInstance();
+  }
+
+  protected abstract String annotatedFixtureName();
+
+  protected FixtureWithNamedSystemUnderTestBase createNamedFixture() {
+    createFixtureInstance(namedFixtureName());
+    return (FixtureWithNamedSystemUnderTestBase) getVerifiedInstance();
+  }
+
+  protected abstract String namedFixtureName();
+
+  protected SimpleFixture createSimpleFixture() {
+    createFixtureInstance(simpleFixtureName());
+    return (SimpleFixture) getVerifiedInstance();
+  }
+
+  protected abstract String simpleFixtureName();
+
+  protected EchoSupport createEchoLibrary() {
+    String instanceName = "library" + library++;
+    statementExecutor.create(instanceName, echoLibraryName(), new Object[] {});
+    return (EchoSupport) statementExecutor.getInstance(instanceName);
+  }
+
+  protected abstract String echoLibraryName();
+
+  protected FileSupport createFileSupportLibrary() {
+    String instanceName = "library" + library++;
+    statementExecutor.create(instanceName, fileSupportName(), new Object[] {});
+    return (FileSupport) statementExecutor.getInstance(instanceName);
+  }
+
+  protected abstract String fileSupportName();
+
+  protected void createFixtureInstance(String fixtureClass) {
+    Object created = statementExecutor.create(INSTANCE_NAME, fixtureClass, new Object[] {});
+    assertEquals("OK", created);
+  }
+
+  protected Echo getVerifiedInstance() {
+    Echo myInstance = (Echo) statementExecutor.getInstance(INSTANCE_NAME);
+    assertFalse(myInstance.echoCalled());
+    return myInstance;
+  }
+
+  protected void executeStatementAndVerifyResultIsVoid() {
+    Object result = statementExecutor.call(INSTANCE_NAME, "speak");
+    assertEquals(voidMessage(), result);
+  }
+}
