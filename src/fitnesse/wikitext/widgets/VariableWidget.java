2d05f768ca704bfdf2b3a32cf31d35d37d4f89d1
Andy Dassing <andy@dassing.us>
2010-01-24 02:21:55 -0500

    Altered variable explansion logic to accommodate nested tables and embedded newlines

--- src/fitnesse/wikitext/widgets/VariableWidget.java
+++ src/fitnesse/wikitext/widgets/VariableWidget.java
@@ -8,9 +8,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class VariableWidget extends ParentWidget {
-  public static final String REGEXP = "\\$\\{[\\w\\.]+\\}";
-  public static final Pattern pattern = Pattern.compile("\\$\\{([\\w\\.]+)\\}", Pattern.MULTILINE + Pattern.DOTALL);
-
+  public static final String prefix = "\\$\\{";
+  public static final String REGEXP = prefix + "[\\w\\.]+\\}";
+  public static final Pattern pattern = Pattern.compile(prefix + "([\\w\\.]+)\\}", Pattern.MULTILINE + Pattern.DOTALL);
+  public static final String prefixDisabled = "!\\{";
+  
   private String name = null;
   private String renderedText;
   private boolean rendered;
