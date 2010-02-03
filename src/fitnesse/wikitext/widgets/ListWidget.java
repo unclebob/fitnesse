576239119281af00538d992964e0339bdc5aa202
schmax <maximilian.schachschal@stud.fh-regensburg.de>
2010-02-03 06:24:08 -0700

    Changed to generate a valid HTML

--- src/fitnesse/wikitext/widgets/ListWidget.java
+++ src/fitnesse/wikitext/widgets/ListWidget.java
@@ -45,8 +45,34 @@ public class ListWidget extends ParentWidget {
         // the trim is real important.  It removes the starting
         // spaces that could cause the item to be recognized
         // as another list.
-        new ListItemWidget(this, listItemContent, this.level + 1);
-        return buildList(text.substring(match.end()));
+        
+        // Find next tree item on same layer to get the child items.
+        Pattern nextPattern = Pattern.compile("([\\r\\n]+"+match.group(1)+"[\\*\\d])");
+        
+        Matcher nextMatch = nextPattern.matcher(text.substring(match.end()));
+        
+        String childItems;
+        if(nextMatch.find())
+        {
+          // If there is a item on same layer, get text until this position -> child items.
+          childItems = text.substring(match.end()).substring(0, nextMatch.start()).replaceAll("(^[\\r\\n]+)|([ \\r\\n]+$)", "");
+        }else
+        {
+          // If there is no item on same layer, the text to the end are child items.
+          childItems = text.substring(match.end()).replaceAll("(^[\\r\\n]+)|( [\\r\\n]+$)", "");
+        }
+        
+        ListItemWidget item = new ListItemWidget(this, listItemContent, this.level + 1);
+        
+        // Add the child items as a new list to the list item.
+        if(!childItems.isEmpty())
+        {
+          new ListWidget(item, childItems);
+        }
+        
+        // Go on with building list after all child items.
+        return buildList(text.substring(match.end()+childItems.length()));
+        
       }
     } else
       return null;
@@ -63,10 +89,10 @@ public class ListWidget extends ParentWidget {
   public String render() throws Exception {
     String tagValue = ordered ? "ol" : "ul";
     StringBuffer html = new StringBuffer();
-    appendTabs(html);
+    //appendTabs(html);
     html.append("<").append(tagValue).append(">").append("\n");
     html.append(childHtml());
-    appendTabs(html);
+    //appendTabs(html);
     html.append("</").append(tagValue).append(">").append("\n");
 
     return html.toString();
