ea12c90ed084874d62756daa8d93ff32665c9d93
Builder <konstantin.vlasenko@quest.com>
2010-12-17 17:42:38 +0300

    use free available port if SLIM_PORT (0)

--- src/fitnesse/FitNesseVersion.java
+++ src/fitnesse/FitNesseVersion.java
@@ -6,7 +6,7 @@ public class FitNesseVersion {
   private final String version;
 	
   public FitNesseVersion() {
-    this("v20101208");
+    this("v20101217");
   }
 
   public FitNesseVersion(String version) {
