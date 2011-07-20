1041f698cf4cace6d7bf6ffa205718f1e33370cf
Conrad Mueller <conrad.mueller@andrena.de>
2011-07-20 14:41:10 +0200

    Added ExcludeSuiteFilter annotation to FitNesseSuite

--- src/fitnesse/junit/FitNesseSuiteArgumentsTest.java
+++ src/fitnesse/junit/FitNesseSuiteArgumentsTest.java
@@ -15,7 +15,9 @@ public class FitNesseSuiteArgumentsTest {
     assertEquals("FitNesse.SuiteAcceptanceTests.SuiteSlimTests", FitNesseSuite.getSuiteName(FitNesseSuiteExampleTest.class));
     assertEquals(new File(System.getProperty("java.io.tmpdir"),"fitnesse").getAbsolutePath(),FitNesseSuite.getOutputDir(FitNesseSuiteExampleTest.class));
     assertNull("null filter allowed", FitNesseSuite.getSuiteFilter(FitNesseSuiteExampleTest.class));
+    assertNull("null exclude filter allowed", FitNesseSuite.getExcludeSuiteFilter(FitNesseSuiteExampleTest.class));
     assertEquals("testSuite", FitNesseSuite.getSuiteFilter(FitNesseSuiteWithFilterExampleTest.class));
+    assertEquals("excludedSuite", FitNesseSuite.getExcludeSuiteFilter(FitNesseSuiteWithFilterExampleTest.class));
     assertEquals(true, FitNesseSuite.useDebugMode(FitNesseSuiteExampleTest.class));
     assertEquals(true, FitNesseSuite.useDebugMode(FitNesseSuiteWithFilterExampleTest.class));
     assertEquals(false, FitNesseSuite.useDebugMode(FitNesseSuiteExampleTestNoDebug.class));
--- src/fitnesse/junit/JUnitHelper.java
+++ src/fitnesse/junit/JUnitHelper.java
@@ -2,6 +2,7 @@ package fitnesse.junit;
 
 
 import junit.framework.Assert;
+import fitnesse.junit.FitNesseSuite.ExcludeSuiteFilter;
 import fitnesse.responders.run.ResultsListener;
 import fitnesse.responders.run.TestSummary;
 
@@ -33,8 +34,14 @@ public class JUnitHelper {
   public void assertSuitePasses(String suiteName, String suiteFilter) throws Exception{
     assertPasses(suiteName, TestHelper.PAGE_TYPE_SUITE, suiteFilter);
   }
+  public void assertSuitePasses(String suiteName, String suiteFilter, String excludeSuiteFilter) throws Exception{
+    assertPasses(suiteName, TestHelper.PAGE_TYPE_SUITE, suiteFilter, excludeSuiteFilter);
+  }
   public void assertPasses(String pageName, String pageType, String suiteFilter) throws Exception{
-    TestSummary summary=helper.run(pageName, pageType, suiteFilter,port);
+    assertPasses(pageName, pageType, suiteFilter, null);
+  }
+  public void assertPasses(String pageName, String pageType, String suiteFilter, String excludeSuiteFilter) throws Exception{
+    TestSummary summary=helper.run(pageName, pageType, suiteFilter, excludeSuiteFilter, port);
     Assert.assertEquals("wrong", 0, summary.wrong);
     Assert.assertEquals("exceptions", 0, summary.exceptions);
     Assert.assertTrue("at least one test executed",summary.right>0);
