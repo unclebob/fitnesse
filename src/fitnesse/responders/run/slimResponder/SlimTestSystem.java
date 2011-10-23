600440be8447cf7712d5a89cf61346bb395a51a8
Markus Gärtner <markus.gaertner@it-agile.de>
2011-10-23 23:52:19 +0200

    killing slimRunner in case of a fastTest now to prevent hanging slim test threads when running unit tests

--- src/fitnesse/responders/run/slimResponder/SlimTestSystem.java
+++ src/fitnesse/responders/run/slimResponder/SlimTestSystem.java
@@ -115,7 +115,11 @@ public abstract class SlimTestSystem extends TestSystem implements SlimTestConte
     if (fastTest) {
       slimRunner = new MockCommandRunner();
       createSlimService(slimArguments);
-    } else {
+    } if (manualStart) {
+      slimSocket = getSlimPortBase();
+      slimRunner = new MockCommandRunner();
+    }
+    else {
       slimRunner = new CommandRunner(slimCommand, "", createClasspathEnvironment(classPath));
     }
     return new ExecutionLog(page, slimRunner);
@@ -180,8 +184,10 @@ public abstract class SlimTestSystem extends TestSystem implements SlimTestConte
 
   public void bye() throws Exception {
     slimClient.sendBye();
-    if (!fastTest)
+    if (!fastTest && !manualStart)
       slimRunner.join();
+    if (fastTest)
+      slimRunner.kill();
   }
 
   //For testing only.  Makes responder faster.
