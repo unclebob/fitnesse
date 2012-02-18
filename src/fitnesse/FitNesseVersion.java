6f7884c586979c7030d07ef865e595aab17b9c9b
unclebob <unclebob@cleancoder.com>
2012-02-18 00:49:31 -0600

    all tests (really) pass.

--- src/fitnesse/FitNesseVersion.java
+++ src/fitnesse/FitNesseVersion.java
@@ -10,7 +10,7 @@ public class FitNesseVersion {
   private final String version;
 	
   public FitNesseVersion() {
-    this("v20120217");
+    this("v20120218");
   }
 
   public FitNesseVersion(String version) {
