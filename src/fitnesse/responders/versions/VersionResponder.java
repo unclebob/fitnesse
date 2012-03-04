d58d1b4f23fcfd97c739ff3228523794a9bd6d63
Arjan Molenaar <gaphor@gmail.com>
2012-03-04 14:50:47 +0100

    css/template updates.

--- src/fitnesse/responders/versions/VersionResponder.java
+++ src/fitnesse/responders/versions/VersionResponder.java
@@ -50,7 +50,7 @@ public class VersionResponder implements SecureResponder {
     // TODO: subclass actions for specific rollback behaviour.
     html.actions = new WikiPageActions(page).withRollback();
     html.put("rollbackVersion", version);
-    html.setMainTemplate("versionPage");
+    html.setMainTemplate("wikiPage");
     html.put("content", new VersionRenderer(pageData));
     return html;
   }
