242e8c8a79703b100dc8be8188c0269721cfe521
unclebob <unclebob@cleancoder.com>
2012-04-01 08:31:44 -0500

    widened tolerance for MultipleDivisionTest since cloudbees seems to have some latency issues.

--- src/fitnesse/http/ResponseSender.java
+++ src/fitnesse/http/ResponseSender.java
@@ -2,7 +2,6 @@
 // Released under the terms of the CPL Common Public License version 1.0.
 package fitnesse.http;
 
-import java.io.IOException;
 import java.net.Socket;
 
 public interface ResponseSender {
