unclebob/fitnesse

commit 262f29f1145bf658136a07b70bddae4e28d6d55f

preparsed scenarios not using suite variable definitions

src/fitnesse/wiki/PageData.java
@@ -69,8 +69,7 @@ public class PageData implements ReadOnlyPageData, Serializable {
   public static final String TEST_RUNNER = "TEST_RUNNER";
   public static final String PATH_SEPARATOR = "PATH_SEPARATOR";
 
-  private Symbol contentSyntaxTree = null;
-  private ParsingPage parsingPage;
+  private transient ParsedPage parsedPage;
 
   public PageData(WikiPage page) {
     wikiPage = page;
@@ -87,8 +86,7 @@ public class PageData implements ReadOnlyPageData, Serializable {
     this(data.getWikiPage(), data.content);
     properties = new WikiPageProperties(data.properties);
     versions.addAll(data.versions);
-    contentSyntaxTree = data.contentSyntaxTree;
-    parsingPage = data.parsingPage;
+    parsedPage = data.parsedPage;
   }
 
   public void initializeAttributes() {
@@ -180,13 +178,11 @@ public class PageData implements ReadOnlyPageData, Serializable {
 
   public void setContent(String content) {
     this.content = content;
-    contentSyntaxTree = null;
-    parsingPage = null;
   }
 
   /* this is the public entry to page parse and translate */
   public String getHtml() {
-      return translateToHtml(getSyntaxTree());
+      return getParsedPage().toHtml();
   }
 
   public String getVariable(String name) {
@@ -195,33 +191,17 @@ public class PageData implements ReadOnlyPageData, Serializable {
       return getParsingPage().renderVariableValue(variable.getValue());
   }
 
-    public Symbol getSyntaxTree() {
-        parsePageContent();
-        return contentSyntaxTree;
+  public ParsedPage getParsedPage() {
+    if (parsedPage == null) parsedPage = new ParsedPage(new WikiSourcePage(wikiPage), content);
+    return parsedPage;
+  }
+
+    private Symbol getSyntaxTree() {
+        return getParsedPage().getSyntaxTree();
     }
 
-    public ParsingPage getParsingPage() {
-        parsePageContent();
-        return parsingPage;
-    }
-
-    private void parsePageContent() {
-        if (contentSyntaxTree == null) {
-            //long start = Clock.currentTimeInMillis();
-            parsingPage = new ParsingPage(new WikiSourcePage(wikiPage));
-            String content = getContent();
-            contentSyntaxTree = Parser.make(parsingPage, content).parse();
-            //long elapsed = Clock.currentTimeInMillis() - start;
-            //System.out.println((wikiPage != null && wikiPage.getName() != null ? wikiPage.getName() : "?") + " parse " + elapsed + " " + (content != null ? content.length() : 0));
-        }
-    }
-
-    public String translateToHtml(Symbol syntaxTree) {
-        //long start = Clock.currentTimeInMillis();
-        String result = new HtmlTranslator(new WikiSourcePage(wikiPage), parsingPage).translateTree(syntaxTree);
-        //long elapsed = Clock.currentTimeInMillis() - start;
-        //System.out.println((wikiPage != null ? wikiPage.getName() : "?") + " translate " + elapsed + " " + result.length());
-        return result;
+    private ParsingPage getParsingPage() {
+        return getParsedPage().getParsingPage();
     }
 
   public void setWikiPage(WikiPage page) {
@@ -234,7 +214,7 @@ public class PageData implements ReadOnlyPageData, Serializable {
 
   public List<String> getClasspaths() {
     Symbol tree = getSyntaxTree();
-    return new Paths(new HtmlTranslator(new WikiSourcePage(wikiPage), parsingPage)).getPaths(tree);
+    return new Paths(new HtmlTranslator(new WikiSourcePage(wikiPage), getParsingPage())).getPaths(tree);
   }
 
     public List<String> getXrefPages() {

src/fitnesse/components/ClassPathBuilderTest.java
@@ -37,10 +37,10 @@ public class ClassPathBuilderTest extends RegexTestCase {
 
   public void testPathSeparatorVariable() throws Exception {
     WikiPage page = crawler.addPage(root, PathParser.parse("TestPage"),
+      "!define PATH_SEPARATOR {|}\n" +
       "!path fitnesse.jar\n" +
         "!path my.jar");
     PageData data = page.getData();
-    data.getParsingPage().putVariable("PATH_SEPARATOR", "|");
     page.commit(data);
 
     String expected = "fitnesse.jar" + "|" + "my.jar";

src/fitnesse/responders/run/slimResponder/HtmlSlimTestSystem.java
@@ -9,8 +9,7 @@ import fitnesse.slimTables.Table;
 import fitnesse.slimTables.TableScanner;
 import fitnesse.wiki.ReadOnlyPageData;
 import fitnesse.wiki.WikiPage;
-import fitnesse.wikitext.parser.Collapsible;
-import fitnesse.wikitext.parser.Symbol;
+import fitnesse.wikitext.parser.ParsedPage;
 
 public class HtmlSlimTestSystem extends SlimTestSystem {
   public HtmlSlimTestSystem(WikiPage page, TestSystemListener listener) {
@@ -18,19 +17,10 @@ public class HtmlSlimTestSystem extends SlimTestSystem {
   }
 
   protected TableScanner scanTheTables(ReadOnlyPageData pageData) {
-    Symbol syntaxTree = pageData.getSyntaxTree();
-    Symbol preparsedScenarioLibrary = getPreparsedScenarioLibrary();
-    syntaxTree.addToFront(findCollapsibleSymbol(preparsedScenarioLibrary));
-    String html = pageData.translateToHtml(syntaxTree);
+    ParsedPage parsedPage = pageData.getParsedPage();
+    parsedPage.addToFront(getPreparsedScenarioLibrary());
+    String html = parsedPage.toHtml();
     return new HtmlTableScanner(html);
-  }
-
-  private Symbol findCollapsibleSymbol(Symbol syntaxTree) {
-    for (Symbol symbol : syntaxTree.getChildren()) {
-      if (symbol.getType() instanceof Collapsible)
-        return symbol;
-    }
-    throw new RuntimeException("There must be a collapsible widget in here.");
   }
 
   @Override

src/fitnesse/responders/run/slimResponder/SlimTestSystem.java
@@ -14,8 +14,7 @@ import fitnesse.slim.SlimService;
 import fitnesse.slimTables.*;
 import fitnesse.testutil.MockCommandRunner;
 import fitnesse.wiki.*;
-import fitnesse.wikitext.parser.Parser;
-import fitnesse.wikitext.parser.Symbol;
+import fitnesse.wikitext.parser.ParsedPage;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -58,7 +57,7 @@ public abstract class SlimTestSystem extends TestSystem implements SlimTestConte
   private Map<String, ScenarioTable> scenarios = new HashMap<String, ScenarioTable>();
   protected List<SlimTable.Expectation> expectations = new ArrayList<SlimTable.Expectation>();
   private SlimTableFactory slimTableFactory = new SlimTableFactory();
-  private Symbol preparsedScenarioLibrary;
+  private ParsedPage preparsedScenarioLibrary;
 
 
   public SlimTestSystem(WikiPage page, TestSystemListener listener) {
@@ -477,10 +476,9 @@ public abstract class SlimTestSystem extends TestSystem implements SlimTestConte
     return allExpectations;
   }
 
-
-  public Symbol getPreparsedScenarioLibrary() {
+  public ParsedPage getPreparsedScenarioLibrary() {
     if (preparsedScenarioLibrary == null) {
-      preparsedScenarioLibrary = Parser.make(page, getScenarioLibraryContent()).parse();
+      preparsedScenarioLibrary = new ParsedPage(page.readOnlyData().getParsedPage(), getScenarioLibraryContent());
     }
     return preparsedScenarioLibrary;
   }

src/fitnesse/responders/run/slimResponder/SlimTestSystemTest.java
@@ -16,6 +16,7 @@ import fitnesse.wiki.*;
 import fitnesse.wikitext.Utils;
 import fitnesse.wikitext.parser.Collapsible;
 import fitnesse.wikitext.parser.Include;
+import fitnesse.wikitext.parser.ParsedPage;
 import fitnesse.wikitext.parser.Symbol;
 import fitnesse.wikitext.test.ParserTestHelper;
 import org.junit.Before;
@@ -477,9 +478,9 @@ public class SlimTestSystemTest {
     crawler.addPage(suitePage, PathParser.parse("ScenarioLibrary"), "child library");
     SlimTestSystem sys = new HtmlSlimTestSystem(suitePage, dummyListener);
 
-    Symbol scenarios = sys.getPreparsedScenarioLibrary();
+    ParsedPage scenarios = sys.getPreparsedScenarioLibrary();
 
-    Symbol includeParent = getCollapsibleSymbol(scenarios);
+    Symbol includeParent = getCollapsibleSymbol(scenarios.getSyntaxTree());
     assertNotNull(includeParent);
     assertEquals("Precompiled Libraries", ParserTestHelper.serializeContent(includeParent.childAt(0)));
     Symbol childLibraryInclude = getIncludeSymbol(includeParent.childAt(1));
@@ -492,9 +493,9 @@ public class SlimTestSystemTest {
     crawler.addPage(root, PathParser.parse("ScenarioLibrary"), "uncle library");
     SlimTestSystem sys = new HtmlSlimTestSystem(suitePage, dummyListener);
 
-    Symbol scenarios = sys.getPreparsedScenarioLibrary();
+    ParsedPage scenarios = sys.getPreparsedScenarioLibrary();
 
-    Symbol includeParent = getCollapsibleSymbol(scenarios);
+    Symbol includeParent = getCollapsibleSymbol(scenarios.getSyntaxTree());
     assertNotNull(includeParent);
     assertEquals("Precompiled Libraries", ParserTestHelper.serializeContent(includeParent.childAt(0)));
     Symbol uncleLibraryInclude = getIncludeSymbol(includeParent.childAt(1));

src/fitnesse/wiki/ReadOnlyPageData.java
@@ -1,15 +1,12 @@
 package fitnesse.wiki;
 
-import fitnesse.wikitext.parser.ParsingPage;
-import fitnesse.wikitext.parser.Symbol;
+import fitnesse.wikitext.parser.ParsedPage;
 import java.util.List;
 
 public interface ReadOnlyPageData {
     String getHtml();
     String getVariable(String name);
-    Symbol getSyntaxTree();
-    ParsingPage getParsingPage();
-    String translateToHtml(Symbol syntaxTree);
+    ParsedPage getParsedPage();
     List<String> getClasspaths();
     List<String> getXrefPages();
     String getContent();

src/fitnesse/wikitext/parser/ParsedPage.java
@@ -1,0 +1,45 @@
+package fitnesse.wikitext.parser;
+
+public class ParsedPage {
+  public ParsedPage(SourcePage sourcePage, String content) {
+    parsingPage = new ParsingPage(sourcePage);
+    this.content = content;
+  }
+
+  public ParsedPage(ParsedPage sourceParsedPage, String content) {
+    parsingPage = sourceParsedPage.getParsingPage().copy();
+    this.content = content;
+  }
+
+  public Symbol getSyntaxTree() {
+    parseContent();
+    return syntaxTree;
+  }
+
+  public ParsingPage getParsingPage() {
+      parseContent();
+      return parsingPage;
+  }
+
+  public void addToFront(ParsedPage addFromPage) {
+    getSyntaxTree().addToFront(addFromPage.getSyntaxTree().childAt(0));
+  }
+
+  public String toHtml() {
+    return new HtmlTranslator(getParsingPage().getPage(), getParsingPage()).translateTree(getSyntaxTree());
+  }
+
+  private void parseContent() {
+    if (syntaxTree == null) {
+        //long start = Clock.currentTimeInMillis();
+        syntaxTree = Parser.make(parsingPage, content).parse();
+        //long elapsed = Clock.currentTimeInMillis() - start;
+        //System.out.println((wikiPage != null && wikiPage.getName() != null ? wikiPage.getName() : "?") + " parse " + elapsed + " " + (content != null ? content.length() : 0));
+    }
+  }
+
+  private final String content;
+  private final ParsingPage parsingPage;
+
+  private Symbol syntaxTree = null;
+}

src/fitnesse/wikitext/parser/ParsingPage.java
@@ -21,14 +21,18 @@ public class ParsingPage {
         this(page, page, new HashMap<String, HashMap<String, Maybe<String>>>());
     }
 
-    public ParsingPage copyForPage(SourcePage page) {
-        return new ParsingPage(page, page, this.cache);
-    }
+  public ParsingPage copy() {
+      return new ParsingPage(page, page, this.cache);
+  }
+
+  public ParsingPage copyForPage(SourcePage page) {
+      return new ParsingPage(page, page, this.cache);
+  }
 
     public ParsingPage copyForNamedPage(SourcePage namedPage) {
         return new ParsingPage(this.page, namedPage, this.cache);
     }
-    
+
     private ParsingPage(SourcePage page, SourcePage namedPage, HashMap<String, HashMap<String, Maybe<String>>> cache) {
         this.page = page;
         this.namedPage = namedPage;

commit 32275fc207186eff7b503408e6a917fe399be490

Make PageData accept page content with carriage returns again
This fixes a regression introduced in 262f29f1145bf658136a07b70bddae4e28d6d55f

The bug is subtle and blew up rather spectacularly, as the carriage returns
confused the parser and caused it to go into a deep recursion, for larger
tables to the point that fitnesse seemed to hang, saturating one CPU.

src/fitnesse/wiki/PageData.java
@@ -173,11 +173,11 @@ public class PageData implements ReadOnlyPageData, Serializable {
   }
 
   public String getContent() {
-    return StringUtil.stripCarriageReturns(content);
+    return content;
   }
 
   public void setContent(String content) {
-    this.content = content;
+    this.content = StringUtil.stripCarriageReturns(content);
   }
 
   /* this is the public entry to page parse and translate */

src/fitnesse/wiki/PageDataTest.java
@@ -196,4 +196,15 @@ public class PageDataTest extends RegexTestCase {
 
     assertEquals("Joe", data.getAttribute(LAST_MODIFYING_USER));
   }
+
+  public void testAllowsContentContainingCarriageReturns() throws Exception {
+    WikiPage root = InMemoryPage.makeRoot("RooT");
+    String content = "|a|\n|table|";
+    WikiPage pageWithUnixLineEndings = crawler.addPage(root, PathParser.parse("PageName"), content);
+    
+    String contentWithCarriageReturns = content.replaceAll("\n", "\r\n");
+    WikiPage pageWithDosLineEndings = crawler.addPage(root, PathParser.parse("PageName2"), contentWithCarriageReturns);
+    
+    assertEquals(pageWithUnixLineEndings.getData().getHtml(), pageWithDosLineEndings.getData().getHtml());
+  }
 }