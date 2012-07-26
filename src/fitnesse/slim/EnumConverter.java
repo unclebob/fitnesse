2bd04ed063f5aef362b0a75835ec2f03509ef84b
Arnout Engelen <arnouten@bzzt.net>
2012-07-26 18:04:39 +0200

    Handle null enum values

--- src/fitnesse/slim/EnumConverter.java
+++ src/fitnesse/slim/EnumConverter.java
@@ -1,22 +1,26 @@
 package fitnesse.slim;
 
+import org.apache.commons.lang.StringUtils;
+
 public class EnumConverter<T extends Enum<T>> implements Converter<T> {
 
-    private final Class<T> enumClass;
+  private final Class<T> enumClass;
 
-    public EnumConverter(Class<T> enumClass) {
-        this.enumClass = enumClass;
-    }
+  public EnumConverter(Class<T> enumClass) {
+    this.enumClass = enumClass;
+  }
 
-    @Override
-    public String toString(T o) {
-        return o.name();
-    }
+  @Override
+  public String toString(T o) {
+    return o.name();
+  }
 
-    @Override
-    public T fromString(String name) {
-        return Enum.valueOf(enumClass, name);
-    }
-
+  @Override
+  public T fromString(String name) {
+    if (StringUtils.isBlank(name))
+      return null;
+    else
+      return Enum.valueOf(enumClass, name);
+  }
 
 }
