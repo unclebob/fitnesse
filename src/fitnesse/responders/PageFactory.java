fb92ccc28d12e8bf13d62b468e5e3288f38e6b02
Arjan Molenaar <gaphor@gmail.com>
2012-09-17 21:57:24 +0200

    Allow per theme templates.

--- src/fitnesse/responders/PageFactory.java
+++ src/fitnesse/responders/PageFactory.java
@@ -2,9 +2,6 @@
 // Released under the terms of the CPL Common Public License version 1.0.
 package fitnesse.responders;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Properties;
@@ -12,14 +9,13 @@ import java.util.Properties;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
-import org.apache.velocity.runtime.RuntimeInstance;
 
 import fitnesse.FitNesseContext;
 import fitnesse.responders.templateUtilities.HtmlPage;
 import fitnesse.responders.templateUtilities.TraverseDirective;
 
 public class PageFactory {
-  
+
   private static VelocityEngine velocityEngine = null;
   private FitNesseContext context;
 
@@ -38,7 +34,7 @@ public class PageFactory {
     template.merge(context, writer);
     return writer.toString();
   }
-  
+
   public String toString() {
     return getClass().getName();
   }
@@ -47,28 +43,27 @@ public class PageFactory {
     if (velocityEngine == null) {
       Properties properties = new Properties();
 
-      String templatePath = getTemplatePath();
+      properties.setProperty(VelocityEngine.RESOURCE_LOADER, "file,themepath,classpath");
 
-      properties.setProperty(VelocityEngine.RESOURCE_LOADER, "file,classpath");
-      properties.setProperty(VelocityEngine.FILE_RESOURCE_LOADER_PATH, templatePath);
+      properties.setProperty(VelocityEngine.FILE_RESOURCE_LOADER_PATH,
+          String.format("%s/%s/files/fitnesse/templates", context.rootPath, context.rootDirectoryName));
+
+      properties.setProperty("themepath." + VelocityEngine.RESOURCE_LOADER + ".class",
+          fitnesse.responders.templateUtilities.ClasspathResourceLoader.class.getName());
+      properties.setProperty("themepath." + VelocityEngine.RESOURCE_LOADER + ".base",
+          String.format("/fitnesse/resources/%s/templates", context.pageTheme));
 
       properties.setProperty("classpath." + VelocityEngine.RESOURCE_LOADER + ".class",
           fitnesse.responders.templateUtilities.ClasspathResourceLoader.class.getName());
       properties.setProperty("classpath." + VelocityEngine.RESOURCE_LOADER + ".base",
           "/fitnesse/resources/templates");
-      
+
       velocityEngine = new VelocityEngine();
       velocityEngine.init(properties);
-      
-      // TODO: Add traverse directive
+
       velocityEngine.loadDirective(TraverseDirective.class.getName());
     }
     return velocityEngine;
   }
-
-  public String getTemplatePath() {
-    return String.format("%s/%s/files/fitnesse/templates", context.rootPath, context.rootDirectoryName);
-  }
-
 
 }
