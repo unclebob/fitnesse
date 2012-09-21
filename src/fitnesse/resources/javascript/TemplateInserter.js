d91d39ddb8a128a75a7db8880662a379dfd9ab9a
jpordon <josh.pordon@gmail.com>
2012-09-21 12:14:45 -0500

    Refactored AJAX into jQuery.

--- src/fitnesse/resources/javascript/TemplateInserter.js
+++ src/fitnesse/resources/javascript/TemplateInserter.js
@@ -2,34 +2,21 @@ function TemplateInserter()
 {
   this.insertInto = function(templateList, textArea) {
     
-    var xmlhttp;
-    if (window.XMLHttpRequest) {
-      xmlhttp=new XMLHttpRequest();
-    } else {
-      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    
-    xmlhttp.onreadystatechange=function()
-    {
-      if (xmlhttp.readyState === 4 && xmlhttp.status === 200)
-      {
-        $('#pageContent').replaceSelectedText(xmlhttp.responseText);
-      }
-      else if (xmlhttp.readyState === 4 && xmlhttp.status !== 200)
-      {
-        alert("Error Accessing Template");
-      }
-    };
-    
     var selectedValue = templateList.options[templateList.selectedIndex].value;
     
     if(selectedValue !== "")
     {
-      selectedValue = selectedValue.substr(1, selectedValue.length - 1);
-      xmlhttp.open("GET", selectedValue + "?pageData",true);
-      xmlhttp.send();
+      pageDataUrl = selectedValue.substr(1, selectedValue.length - 1) + "?pageData";
+      
+      $.ajax({
+        url: pageDataUrl,
+        success: function(result) {
+          $('#pageContent').replaceSelectedText(result);
+        },
+        error: function() {
+          alert("Error Accessing Template");
+        }
+      });
     }
   };
-  
-  
 }
