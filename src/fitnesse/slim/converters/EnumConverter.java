26b812e01ce44d774a477a9b5cc0a61b910887c7
Markus Gärtner <markus.gaertner@it-agile.de>
2012-12-02 20:08:55 +0100

    moved this file to the converters package)

---
+++ src/fitnesse/slim/converters/EnumConverter.java
@@ -0,0 +1,37 @@
+package fitnesse.slim.converters;
+
+import org.apache.commons.lang.StringUtils;
+
+import fitnesse.slim.Converter;
+
+public class EnumConverter<T extends Enum<T>> implements Converter<T> {
+
+	private final Class<T> enumClass;
+
+	public EnumConverter(Class<T> enumClass) {
+		this.enumClass = enumClass;
+	}
+
+	@Override
+	public String toString(T o) {
+		return o.name();
+	}
+
+	@Override
+	public T fromString(String name) {
+		if (StringUtils.isBlank(name))
+			return null;
+		else
+			try {
+				return Enum.valueOf(enumClass, name);
+			} catch (IllegalArgumentException e) {
+				for (T value : enumClass.getEnumConstants()) {
+					if (value.toString().equals(name)) {
+						return value;
+					}
+				}
+				throw e;
+			}
+	}
+
+}
