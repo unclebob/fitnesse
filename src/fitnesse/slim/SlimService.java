04208f84fb1aadffeff88adab2fcd182396f56b0
Markus Gärtner <markus.gaertner@it-agile.de>
2012-12-02 20:16:10 +0100

    some warnings during compile removed

--- src/fitnesse/slim/SlimService.java
+++ src/fitnesse/slim/SlimService.java
@@ -10,53 +10,59 @@ import util.CommandLine;
 import fitnesse.socketservice.SocketService;
 
 public class SlimService extends SocketService {
-  public static SlimService instance = null;
-  public static boolean verbose;
-  public static int port;
+	public static SlimService instance = null;
+	public static boolean verbose;
+	public static int port;
 
-  protected static String interactionClassName = null;
+	protected static String interactionClassName = null;
 
-  public static void main(String[] args) throws IOException {
-    if (parseCommandLine(args)) {
-      startWithFactory(args, new JavaSlimFactory());
-    } else {
-      parseCommandLineFailed(args);
-    }
-  }
+	public static void main(String[] args) throws IOException {
+		if (parseCommandLine(args)) {
+			startWithFactory(args, new JavaSlimFactory());
+		} else {
+			parseCommandLineFailed(args);
+		}
+	}
 
-  protected static void parseCommandLineFailed(String[] args) {
-    System.err.println("Invalid command line arguments:" + Arrays.asList(args));
-  }
+	protected static void parseCommandLineFailed(String[] args) {
+		System.err.println("Invalid command line arguments:"
+				+ Arrays.asList(args));
+	}
 
-  protected static void startWithFactory(String[] args, SlimFactory slimFactory) throws IOException {
-    new SlimService(port, slimFactory.getSlimServer(verbose));
-  }
+	protected static void startWithFactory(String[] args,
+			SlimFactory slimFactory) throws IOException {
+		new SlimService(port, slimFactory.getSlimServer(verbose));
+	}
 
-  protected static boolean parseCommandLine(String[] args) {
-    CommandLine commandLine = new CommandLine("[-v] [-i interactionClass] port ");
-    if (commandLine.parse(args)) {
-      verbose = commandLine.hasOption("v");
-      interactionClassName = commandLine.getOptionArgument("i", "interactionClass");
-      String portString = commandLine.getArgument("port");
-      port = (portString == null) ? 8099 :Integer.parseInt(portString);
-      return true;
-    }
-    return false;
-  }
+	protected static boolean parseCommandLine(String[] args) {
+		CommandLine commandLine = new CommandLine(
+				"[-v] [-i interactionClass] port ");
+		if (commandLine.parse(args)) {
+			verbose = commandLine.hasOption("v");
+			interactionClassName = commandLine.getOptionArgument("i",
+					"interactionClass");
+			String portString = commandLine.getArgument("port");
+			port = (portString == null) ? 8099 : Integer.parseInt(portString);
+			return true;
+		}
+		return false;
+	}
 
-  public SlimService(int port, SlimServer slimServer) throws IOException  {
-    super(port, slimServer);
-    instance = this;
-  }
+	public SlimService(int port, SlimServer slimServer) throws IOException {
+		super(port, slimServer);
+		instance = this;
+	}
 
-  public static Class<DefaultInteraction> getInteractionClass() {
-    if(interactionClassName==null){
-      return DefaultInteraction.class;
-    }
-    try {
-      return (Class<DefaultInteraction>) Class.forName(interactionClassName);
-    } catch (ClassNotFoundException e) {
-      throw new RuntimeException(e);
-    }
-  }
+	@SuppressWarnings("unchecked")
+	public static Class<DefaultInteraction> getInteractionClass() {
+		if (interactionClassName == null) {
+			return DefaultInteraction.class;
+		}
+		try {
+			return (Class<DefaultInteraction>) Class
+					.forName(interactionClassName);
+		} catch (ClassNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
