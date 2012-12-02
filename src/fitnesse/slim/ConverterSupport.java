7f0ed4b2cbf452b4926d6df5c20c00c577ae225e
Markus Gärtner <markus.gaertner@it-agile.de>
2012-12-02 19:57:46 +0100

    fixed a bug where an existing EnumEditor was not used

--- src/fitnesse/slim/ConverterSupport.java
+++ src/fitnesse/slim/ConverterSupport.java
@@ -8,37 +8,44 @@ import fitnesse.slim.converters.PropertyEditorConverter;
 
 class ConverterSupport {
 
-  public static <T> Converter<T> getConverter(Class<T> k) {
-    Converter<T> c = ConverterRegistry.getConverterForClass(k);
-    if (c != null)
-      return c;
-    PropertyEditor pe = PropertyEditorManager.findEditor(k);
-    if (pe != null) {
-      return new PropertyEditorConverter<T>(pe);
-    }
-    return null;
-  }
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static <T> Converter<T> getConverter(Class<? extends T> k) {
+		Converter<T> c = ConverterRegistry.getConverterForClass(k);
+		if (c != null)
+			return c;
+		PropertyEditor pe = PropertyEditorManager.findEditor(k);
+		// com.sun.beans.EnumEditor and sun.beans.EnumEditor seem to be used in different usages.
+	    if (Enum.class.isAssignableFrom(k) && "EnumEditor".equals(pe.getClass().getSimpleName()))
+	    	return new EnumConverter(k);
+	    if (pe != null) {
+	    	return new PropertyEditorConverter<T>(pe);
+	    }
+		return null;
+	}
 
-  public static Object[] convertArgs(Object[] args, Class<?>[] argumentTypes) {
-    Object[] convertedArgs = new Object[args.length];
-    for (int i = 0; i < argumentTypes.length; i++) {
-      convertedArgs[i] = convertArg(args[i], argumentTypes[i]);
-    }
-    return convertedArgs;
-  }
+	public static Object[] convertArgs(Object[] args, Class<?>[] argumentTypes) {
+		Object[] convertedArgs = new Object[args.length];
+		for (int i = 0; i < argumentTypes.length; i++) {
+			convertedArgs[i] = convertArg(args[i], argumentTypes[i]);
+		}
+		return convertedArgs;
+	}
 
-  @SuppressWarnings("unchecked")
-  private static <T> T convertArg(Object arg, Class<T> argumentType) throws SlimError {
-    if (arg == null || argumentType.isInstance(arg)) {
-      // arg may be a List or an instance that comes from the variable store
-      return (T) arg;
-    }
-    Converter<T> converter = getConverter(argumentType);
-    if (converter != null) {
-      return converter.fromString(arg.toString());
-    }
-    throw new SlimError(String.format("message:<<NO_CONVERTER_FOR_ARGUMENT_NUMBER %s.>>",
-        argumentType.getName()));
-  }
+	@SuppressWarnings("unchecked")
+	private static <T> T convertArg(Object arg, Class<T> argumentType)
+			throws SlimError {
+		if (arg == null || argumentType.isInstance(arg)) {
+			// arg may be a List or an instance that comes from the variable
+			// store
+			return (T) arg;
+		}
+		Converter<T> converter = getConverter(argumentType);
+		if (converter != null) {
+			return converter.fromString(arg.toString());
+		}
+		throw new SlimError(String.format(
+				"message:<<NO_CONVERTER_FOR_ARGUMENT_NUMBER %s.>>",
+				argumentType.getName()));
+	}
 
 }
--- src/fitnesse/slim/converters/ConverterRegistry.java
+++ src/fitnesse/slim/converters/ConverterRegistry.java
@@ -6,57 +6,49 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import fitnesse.slim.EnumConverter;
-
 import fitnesse.slim.Converter;
 
 public class ConverterRegistry {
 
-  static Map<Class<?>, Converter<?>> converters = new HashMap<Class<?>, Converter<?>>();
+	static Map<Class<?>, Converter<?>> converters = new HashMap<Class<?>, Converter<?>>();
 
-  static {
-    addStandardConverters();
-  }
+	static {
+		addStandardConverters();
+	}
 
-  @SuppressWarnings("unchecked")
-  protected static void addStandardConverters() {
-    addConverter(void.class, new VoidConverter());
-    addConverter(String.class, new StringConverter());
-    addConverter(int.class, new IntConverter());
-    addConverter(double.class, new DoubleConverter());
-    addConverter(Integer.class, new IntConverter());
-    addConverter(Double.class, new DoubleConverter());
-    addConverter(char.class, new CharConverter());
-    addConverter(boolean.class, new BooleanConverter());
-    addConverter(Boolean.class, new BooleanConverter());
-    addConverter(Date.class, new DateConverter());
-    addConverter(List.class, new ListConverter());
-    addConverter(Integer[].class, new IntegerArrayConverter());
-    addConverter(int[].class, new IntegerArrayConverter());
-    addConverter(String[].class, new StringArrayConverter());
-    addConverter(boolean[].class, new BooleanArrayConverter());
-    addConverter(Boolean[].class, new BooleanArrayConverter());
-    addConverter(double[].class, new DoubleArrayConverter());
-    addConverter(Double[].class, new DoubleArrayConverter());
-  }
+	@SuppressWarnings("unchecked")
+	protected static void addStandardConverters() {
+		addConverter(void.class, new VoidConverter());
+		addConverter(String.class, new StringConverter());
+		addConverter(int.class, new IntConverter());
+		addConverter(double.class, new DoubleConverter());
+		addConverter(Integer.class, new IntConverter());
+		addConverter(Double.class, new DoubleConverter());
+		addConverter(char.class, new CharConverter());
+		addConverter(boolean.class, new BooleanConverter());
+		addConverter(Boolean.class, new BooleanConverter());
+		addConverter(Date.class, new DateConverter());
+		addConverter(List.class, new ListConverter());
+		addConverter(Integer[].class, new IntegerArrayConverter());
+		addConverter(int[].class, new IntegerArrayConverter());
+		addConverter(String[].class, new StringArrayConverter());
+		addConverter(boolean[].class, new BooleanArrayConverter());
+		addConverter(Boolean[].class, new BooleanArrayConverter());
+		addConverter(double[].class, new DoubleArrayConverter());
+		addConverter(Double[].class, new DoubleArrayConverter());
+	}
 
-  @SuppressWarnings({"rawtypes", "unchecked"})
-  public static <T> Converter<T> getConverterForClass(Class<? extends T> clazz) {
-    Converter converter = converters.get(clazz);
-    
-    if (converter != null)
-      return converter;
-    else if (Enum.class.isAssignableFrom(clazz))
-      return new EnumConverter(clazz);
-    else
-      return null;
-  }
+	@SuppressWarnings("unchecked")
+	public static <T> Converter<T> getConverterForClass(Class<? extends T> clazz) {
+		return (Converter<T>) converters.get(clazz);
+	}
 
-  public static <T> void addConverter(Class<? extends T> clazz, Converter<T> converter) {
-    converters.put(clazz, converter);
-  }
+	public static <T> void addConverter(Class<? extends T> clazz,
+			Converter<T> converter) {
+		converters.put(clazz, converter);
+	}
 
-  public static Map<Class<?>, Converter<?>> getConverters() {
-    return Collections.unmodifiableMap(converters);
-  }
+	public static Map<Class<?>, Converter<?>> getConverters() {
+		return Collections.unmodifiableMap(converters);
+	}
 }
