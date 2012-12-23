7123c941e65480261b5f54dc159e1944ed29a73c
Arjan Wulder <awulder@xebia.com>
2012-12-23 12:05:46 +0100

    2-space indent fix
    Now using 2-space indent instead of 4-spaces

--- src/fitnesse/responders/run/JavaFormatter.java
+++ src/fitnesse/responders/run/JavaFormatter.java
@@ -1,5 +1,10 @@
 package fitnesse.responders.run;
 
+import fitnesse.responders.run.formatters.BaseFormatter;
+import fitnesse.wiki.WikiPage;
+import fitnesse.wiki.WikiPagePath;
+import util.TimeMeasurement;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,11 +17,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import util.TimeMeasurement;
-import fitnesse.responders.run.formatters.BaseFormatter;
-import fitnesse.wiki.WikiPage;
-import fitnesse.wiki.WikiPagePath;
-
 /**
  * Used to run tests from a JUnit test suite.
  *
@@ -24,210 +24,212 @@ import fitnesse.wiki.WikiPagePath;
  */
 public class JavaFormatter extends BaseFormatter {
 
-    private String mainPageName;
-    private boolean isSuite = true;
-    public static final String SUMMARY_FOOTER = "</table>";
-    public static final String SUMMARY_HEADER = "<table><tr><td>Name</td><td>Right</td><td>Wrong</td><td>Exceptions</td></tr>";
+  private String mainPageName;
+  private boolean isSuite = true;
+  public static final String SUMMARY_FOOTER = "</table>";
+  public static final String SUMMARY_HEADER = "<table><tr><td>Name</td><td>Right</td><td>Wrong</td><td>Exceptions</td></tr>";
 
-    public interface ResultsRepository {
-        void open(String string) throws IOException;
+  public interface ResultsRepository {
+    void open(String string) throws IOException;
 
-        void close() throws IOException;
+    void close() throws IOException;
 
-        void write(String content) throws IOException;
+    void write(String content) throws IOException;
+  }
+
+  public static class FolderResultsRepository implements ResultsRepository {
+    private String outputPath;
+    private Writer currentWriter;
+
+    public FolderResultsRepository(String outputPath, String fitNesseRoot) throws IOException {
+      this.outputPath = outputPath;
+      initFolder(fitNesseRoot);
     }
 
-    public static class FolderResultsRepository implements ResultsRepository {
-        private String outputPath;
-        private Writer currentWriter;
+    public void open(String testName) throws IOException {
+      File outputFile = new File(outputPath, testName + ".html");
+      currentWriter = new OutputStreamWriter(new FileOutputStream(outputFile), "UTF-8");
 
-        public FolderResultsRepository(String outputPath, String fitNesseRoot) throws IOException {
-            this.outputPath = outputPath;
-            initFolder(fitNesseRoot);
-        }
-
-        public void open(String testName) throws IOException {
-            File outputFile = new File(outputPath, testName + ".html");
-            currentWriter = new OutputStreamWriter(new FileOutputStream(outputFile), "UTF-8");
-
-            currentWriter.write("<html><head><title>");
-            currentWriter.write(testName);
-            currentWriter
-                    .write("</title><meta http-equiv='Content-Type' content='text/html;charset=utf-8'/>"
-                            + "<link rel='stylesheet' type='text/css' href='css/fitnesse.css'/>"
-                            + "<script src='javascript/jquery-1.7.2.min.js' type='text/javascript'></script>"
-                            + "<script src='javascript/fitnesse.js' type='text/javascript'></script>" + "</head><body><header><h2>");
-            currentWriter.write(testName);
-            currentWriter.write("</h2></header><article>");
-
-        }
-
-        public void close() throws IOException {
-            if (currentWriter != null) {
-                currentWriter.write("</article></body></html>");
-                currentWriter.close();
-            }
-        }
-
-        public void write(String content) throws IOException {
-            currentWriter.write(content.replace("src=\"/files/images/", "src=\"images/"));
-        }
-
-        public void addFile(String resource, String relativeFilePath) throws IOException {
-            File dst = new File(outputPath, relativeFilePath);
-            dst.getParentFile().mkdirs();
-            copy(resource, dst);
-        }
-
-        private void copy(String source, File destination) throws IOException {
-            InputStream in = getClass().getResourceAsStream(source);
-            OutputStream out = new FileOutputStream(destination);
-            // Transfer bytes from in to out
-            byte[] buf = new byte[1024];
-            int len;
-            while ((len = in.read(buf)) > 0) {
-                out.write(buf, 0, len);
-            }
-            in.close();
-            out.close();
-        }
-
-        private void initFolder(String fitnesseRoot) throws IOException {
-            String base = "/fitnesse/resources/";
-            String cssDir = base + "css/";
-            addFile(cssDir + "fitnesse_wiki.css", "css/fitnesse.css");
-            String javascriptDir = base + "javascript/";
-            addFile(javascriptDir + "jquery-1.7.2.min.js", "javascript/jquery-1.7.2.min.js");
-            addFile(javascriptDir + "fitnesse.js", "javascript/fitnesse.js");
-            String imagesDir = base + "images/";
-            addFile(imagesDir + "collapsibleOpen.png", "images/collapsibleOpen.png");
-            addFile(imagesDir + "collapsibleClosed.png", "images/collapsibleClosed.png");
-        }
-    }
-
-    private TestSummary totalSummary = new TestSummary();
-
-    public String getFullPath(final WikiPage wikiPage) {
-        return new WikiPagePath(wikiPage).toString();
-    }
-
-    private List<String> visitedTestPages = new ArrayList<String>();
-    private Map<String, TestSummary> testSummaries = new HashMap<String, TestSummary>();
-
-    @Override
-    public void newTestStarted(TestPage test, TimeMeasurement timeMeasurement) throws IOException {
-        resultsRepository.open(getFullPath(test.getSourcePage()));
-        if (listener != null)
-            listener.newTestStarted(test, timeMeasurement);
-    }
-
-    @Override
-    public void setExecutionLogAndTrackingId(String stopResponderId, CompositeExecutionLog log) {
-    }
-
-    public void testComplete(TestPage test, TestSummary testSummary, TimeMeasurement timeMeasurement) throws IOException {
-        String fullPath = getFullPath(test.getSourcePage());
-        visitedTestPages.add(fullPath);
-        totalSummary.add(testSummary);
-        testSummaries.put(fullPath, new TestSummary(testSummary));
-        resultsRepository.close();
-        isSuite = isSuite && (!mainPageName.equals(fullPath));
-        if (listener != null)
-            listener.testComplete(test, testSummary, timeMeasurement);
-    }
-
-    TestSummary getTestSummary(String testPath) {
-        return testSummaries.get(testPath);
-    }
-
-    @Override
-    public void testOutputChunk(String output) throws IOException {
-        resultsRepository.write(output);
-    }
-
-    @Override
-    public void testSystemStarted(TestSystem testSystem, String testSystemName, String testRunner) {
-    }
-
-    private ResultsRepository resultsRepository;
-
-    public TestSummary getTotalSummary() {
-        return totalSummary;
-    }
-
-    public void setTotalSummary(TestSummary testSummary) {
-        totalSummary = testSummary;
-    }
-
-    public void setResultsRepository(ResultsRepository mockResultsRepository) {
-        this.resultsRepository = mockResultsRepository;
+      currentWriter.write("<html><head><title>");
+      currentWriter.write(testName);
+      currentWriter
+        .write("</title><meta http-equiv='Content-Type' content='text/html;charset=utf-8'/>"
+          + "<link rel='stylesheet' type='text/css' href='css/fitnesse.css'/>"
+          + "<script src='javascript/jquery-1.7.2.min.js' type='text/javascript'></script>"
+          + "<script src='javascript/fitnesse.js' type='text/javascript'></script>" + "</head><body><header><h2>");
+      currentWriter.write(testName);
+      currentWriter.write("</h2></header><article>");
 
     }
 
-    /** package-private to prevent instantiation apart from getInstance and tests */
-    JavaFormatter(String suiteName) {
-        this.mainPageName = suiteName;
+    public void close() throws IOException {
+      if (currentWriter != null) {
+        currentWriter.write("</article></body></html>");
+        currentWriter.close();
+      }
     }
 
-    private static Map<String, JavaFormatter> allocatedInstances = new HashMap<String, JavaFormatter>();
-    private ResultsListener listener;
-
-    public synchronized static JavaFormatter getInstance(String testName) {
-        JavaFormatter existing = allocatedInstances.get(testName);
-        if (existing != null)
-            return existing;
-        existing = new JavaFormatter(testName);
-        allocatedInstances.put(testName, existing);
-        return existing;
+    public void write(String content) throws IOException {
+      currentWriter.write(content.replace("src=\"/files/images/", "src=\"images/"));
     }
 
-    @Override
-    public void allTestingComplete(TimeMeasurement totalTimeMeasurement) throws IOException {
-        if (isSuite)
-            writeSummary(mainPageName);
-        if (listener != null)
-            listener.allTestingComplete(totalTimeMeasurement);
+    public void addFile(String resource, String relativeFilePath) throws IOException {
+      File dst = new File(outputPath, relativeFilePath);
+      dst.getParentFile().mkdirs();
+      copy(resource, dst);
     }
 
-    public void writeSummary(String suiteName) throws IOException {
-        resultsRepository.open(suiteName);
-        resultsRepository.write(SUMMARY_HEADER);
-        for (String s : visitedTestPages) {
-            resultsRepository.write(summaryRow(s, testSummaries.get(s)));
-        }
-        resultsRepository.write(SUMMARY_FOOTER);
-        resultsRepository.close();
+    private void copy(String source, File destination) throws IOException {
+      InputStream in = getClass().getResourceAsStream(source);
+      OutputStream out = new FileOutputStream(destination);
+      // Transfer bytes from in to out
+      byte[] buf = new byte[1024];
+      int len;
+      while ((len = in.read(buf)) > 0) {
+        out.write(buf, 0, len);
+      }
+      in.close();
+      out.close();
     }
 
-    public String summaryRow(String testName, TestSummary testSummary) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("<tr class=\"").append(getCssClass(testSummary)).append("\"><td>").append(
-                "<a href=\"").append(testName).append(".html\">").append(testName).append("</a>").append(
-                "</td><td>").append(testSummary.right).append("</td><td>").append(testSummary.wrong)
-                .append("</td><td>").append(testSummary.exceptions).append("</td></tr>");
-        return sb.toString();
+    private void initFolder(String fitnesseRoot) throws IOException {
+      String base = "/fitnesse/resources/";
+      String cssDir = base + "css/";
+      addFile(cssDir + "fitnesse_wiki.css", "css/fitnesse.css");
+      String javascriptDir = base + "javascript/";
+      addFile(javascriptDir + "jquery-1.7.2.min.js", "javascript/jquery-1.7.2.min.js");
+      addFile(javascriptDir + "fitnesse.js", "javascript/fitnesse.js");
+      String imagesDir = base + "images/";
+      addFile(imagesDir + "collapsibleOpen.png", "images/collapsibleOpen.png");
+      addFile(imagesDir + "collapsibleClosed.png", "images/collapsibleClosed.png");
     }
+  }
 
-    private String getCssClass(TestSummary ts) {
-        if (ts.exceptions > 0)
-            return "error";
-        if (ts.wrong > 0)
-            return "fail";
-        if (ts.right > 0)
-            return "pass";
-        return "plain";
-    }
+  private TestSummary totalSummary = new TestSummary();
 
-    public void setListener(ResultsListener listener) {
-        this.listener = listener;
-    }
+  public String getFullPath(final WikiPage wikiPage) {
+    return new WikiPagePath(wikiPage).toString();
+  }
 
-    public List<String> getTestsExecuted() {
-        return visitedTestPages;
-    }
+  private List<String> visitedTestPages = new ArrayList<String>();
+  private Map<String, TestSummary> testSummaries = new HashMap<String, TestSummary>();
 
-    public static void dropInstance(String testName) {
-        allocatedInstances.remove(testName);
+  @Override
+  public void newTestStarted(TestPage test, TimeMeasurement timeMeasurement) throws IOException {
+    resultsRepository.open(getFullPath(test.getSourcePage()));
+    if (listener != null)
+      listener.newTestStarted(test, timeMeasurement);
+  }
+
+  @Override
+  public void setExecutionLogAndTrackingId(String stopResponderId, CompositeExecutionLog log) {
+  }
+
+  public void testComplete(TestPage test, TestSummary testSummary, TimeMeasurement timeMeasurement) throws IOException {
+    String fullPath = getFullPath(test.getSourcePage());
+    visitedTestPages.add(fullPath);
+    totalSummary.add(testSummary);
+    testSummaries.put(fullPath, new TestSummary(testSummary));
+    resultsRepository.close();
+    isSuite = isSuite && (!mainPageName.equals(fullPath));
+    if (listener != null)
+      listener.testComplete(test, testSummary, timeMeasurement);
+  }
+
+  TestSummary getTestSummary(String testPath) {
+    return testSummaries.get(testPath);
+  }
+
+  @Override
+  public void testOutputChunk(String output) throws IOException {
+    resultsRepository.write(output);
+  }
+
+  @Override
+  public void testSystemStarted(TestSystem testSystem, String testSystemName, String testRunner) {
+  }
+
+  private ResultsRepository resultsRepository;
+
+  public TestSummary getTotalSummary() {
+    return totalSummary;
+  }
+
+  public void setTotalSummary(TestSummary testSummary) {
+    totalSummary = testSummary;
+  }
+
+  public void setResultsRepository(ResultsRepository mockResultsRepository) {
+    this.resultsRepository = mockResultsRepository;
+
+  }
+
+  /**
+   * package-private to prevent instantiation apart from getInstance and tests
+   */
+  JavaFormatter(String suiteName) {
+    this.mainPageName = suiteName;
+  }
+
+  private static Map<String, JavaFormatter> allocatedInstances = new HashMap<String, JavaFormatter>();
+  private ResultsListener listener;
+
+  public synchronized static JavaFormatter getInstance(String testName) {
+    JavaFormatter existing = allocatedInstances.get(testName);
+    if (existing != null)
+      return existing;
+    existing = new JavaFormatter(testName);
+    allocatedInstances.put(testName, existing);
+    return existing;
+  }
+
+  @Override
+  public void allTestingComplete(TimeMeasurement totalTimeMeasurement) throws IOException {
+    if (isSuite)
+      writeSummary(mainPageName);
+    if (listener != null)
+      listener.allTestingComplete(totalTimeMeasurement);
+  }
+
+  public void writeSummary(String suiteName) throws IOException {
+    resultsRepository.open(suiteName);
+    resultsRepository.write(SUMMARY_HEADER);
+    for (String s : visitedTestPages) {
+      resultsRepository.write(summaryRow(s, testSummaries.get(s)));
     }
+    resultsRepository.write(SUMMARY_FOOTER);
+    resultsRepository.close();
+  }
+
+  public String summaryRow(String testName, TestSummary testSummary) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("<tr class=\"").append(getCssClass(testSummary)).append("\"><td>").append(
+      "<a href=\"").append(testName).append(".html\">").append(testName).append("</a>").append(
+      "</td><td>").append(testSummary.right).append("</td><td>").append(testSummary.wrong)
+      .append("</td><td>").append(testSummary.exceptions).append("</td></tr>");
+    return sb.toString();
+  }
+
+  private String getCssClass(TestSummary ts) {
+    if (ts.exceptions > 0)
+      return "error";
+    if (ts.wrong > 0)
+      return "fail";
+    if (ts.right > 0)
+      return "pass";
+    return "plain";
+  }
+
+  public void setListener(ResultsListener listener) {
+    this.listener = listener;
+  }
+
+  public List<String> getTestsExecuted() {
+    return visitedTestPages;
+  }
+
+  public static void dropInstance(String testName) {
+    allocatedInstances.remove(testName);
+  }
 
 }
