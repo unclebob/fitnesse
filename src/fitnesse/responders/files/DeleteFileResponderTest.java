5a87d9ba1798fe28488da4c0b719e8a86c244b9a
Arjan Molenaar <gaphor@gmail.com>
2012-12-28 15:06:38 +0100

    Fixed DeleteFileResponderTest. Fricken Mac HFS type preserving stuff makes tests fail on a real FS.

--- src/fitnesse/responders/files/DeleteFileResponderTest.java
+++ src/fitnesse/responders/files/DeleteFileResponderTest.java
@@ -16,17 +16,17 @@ public class DeleteFileResponderTest extends RegexTestCase {
   private FitNesseContext context;
 
   public void setUp() {
-    FileUtil.makeDir("testdir");
+    FileUtil.makeDir(FitNesseUtil.base);
     request = new MockRequest();
-    context = FitNesseUtil.makeTestContext(null);
+    context = FitNesseUtil.makeTestContext();
   }
 
   public void tearDown() throws Exception {
-    FileUtil.deleteFileSystemDirectory("testdir");
+    FileUtil.deleteFileSystemDirectory(FitNesseUtil.base);
   }
 
   public void testDelete() throws Exception {
-    File file = new File("testdir/testfile");
+    File file = new File(FitNesseUtil.base + "/testfile");
     assertTrue(file.createNewFile());
     DeleteFileResponder responder = new DeleteFileResponder();
     request.addInput("filename", "testfile");
@@ -38,7 +38,7 @@ public class DeleteFileResponderTest extends RegexTestCase {
   }
 
   public void testDeleteDirectory() throws Exception {
-    File dir = new File("testdir/dir");
+    File dir = new File(FitNesseUtil.base + "/dir");
     assertTrue(dir.mkdir());
     File file = new File(dir, "testChildFile");
     assertTrue(file.createNewFile());
