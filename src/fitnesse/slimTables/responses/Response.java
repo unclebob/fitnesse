98a3bfbe0e925ce5dafe527a70b778eac116481d
Arjan Molenaar <gaphor@gmail.com>
2013-01-10 09:36:28 +0100

    Response.toHtml is deprecated by design

--- src/fitnesse/slimTables/responses/Response.java
+++ src/fitnesse/slimTables/responses/Response.java
@@ -4,6 +4,7 @@ public interface Response {
 
   /**
    * @return String representation of the response.
+   * @deprecated Formatting should move to the frontend, based on reponse content.
    */
   String toHtml();
 }
