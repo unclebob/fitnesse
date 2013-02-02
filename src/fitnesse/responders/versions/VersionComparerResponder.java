32480a305a9b46e88524afbf43fd96201b5ad5ea
Mark Hopkins <markhop@sky.com>
2013-02-02 17:26:16 +0000

    Beginnings of version compare

---
+++ src/fitnesse/responders/versions/VersionComparerResponder.java
@@ -0,0 +1,17 @@
+package fitnesse.responders.versions;
+
+import fitnesse.FitNesseContext;
+import fitnesse.Responder;
+import fitnesse.http.Request;
+import fitnesse.http.Response;
+
+public class VersionComparerResponder implements Responder {
+
+  @Override
+  public Response makeResponse(FitNesseContext context, Request request)
+      throws Exception {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
---
+++ src/fitnesse/responders/versions/VersionComparerResponderTest.java
@@ -0,0 +1,11 @@
+package fitnesse.responders.versions;
+
+import org.junit.Test;
+
+public class VersionComparerResponderTest {
+
+  @Test
+  public void shouldBeAbleToMakeASimpleResponse() throws Exception {
+    
+  }
+}
