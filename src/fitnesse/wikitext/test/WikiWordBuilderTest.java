ba522e19ed920d19f9b120ff6fce01a15eb80a82
Arjan Molenaar <gaphor@gmail.com>
2013-02-04 18:19:38 +0100

    Missed one test where & was not replaced by &amp;

--- src/fitnesse/wikitext/test/WikiWordBuilderTest.java
+++ src/fitnesse/wikitext/test/WikiWordBuilderTest.java
@@ -8,6 +8,6 @@ public class WikiWordBuilderTest {
   @Test public void buildsLinkToNonExistent() {
     WikiWordBuilder builder = new WikiWordBuilder(new TestSourcePage().withUrl("NonExistentPage"), "", "");
     String result = builder.buildLink("", "<i>name</i>");
-    Assert.assertEquals("<i>name</i><a title=\"create page\" href=\"NonExistentPage?edit&nonExistent=true\">[?]</a>", result);
+    Assert.assertEquals("<i>name</i><a title=\"create page\" href=\"NonExistentPage?edit&amp;nonExistent=true\">[?]</a>", result);
   }
 }
