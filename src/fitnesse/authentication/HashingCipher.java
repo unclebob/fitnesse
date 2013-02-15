ee2be6d96ce174e621e35c782bde4616bece2cb1
Arjan Molenaar <gaphor@gmail.com>
2013-02-15 11:25:47 +0100

    Move Base64.java to fitnesse.util package

--- src/fitnesse/authentication/HashingCipher.java
+++ src/fitnesse/authentication/HashingCipher.java
@@ -2,7 +2,7 @@
 // Released under the terms of the CPL Common Public License version 1.0.
 package fitnesse.authentication;
 
-import fitnesse.components.Base64;
+import fitnesse.util.Base64;
 
 public class HashingCipher implements PasswordCipher {
   private static final int repetitions = 3;
