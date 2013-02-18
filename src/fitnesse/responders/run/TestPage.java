7103606c9beb8f4ef9798dacb6bb5aba70f51874
Arjan Molenaar <gaphor@gmail.com>
2013-02-18 07:43:52 +0100

    reformat TestPage

--- src/fitnesse/responders/run/TestPage.java
+++ src/fitnesse/responders/run/TestPage.java
@@ -5,34 +5,48 @@ import fitnesse.wiki.ReadOnlyPageData;
 import fitnesse.wiki.WikiPage;
 
 public class TestPage {
-    public TestPage(WikiPage sourcePage) {
-        this.sourcePage = sourcePage;
-    }
+  private WikiPage sourcePage;
+  private PageData data;
+  private PageData decoratedData;
 
-    public TestPage(PageData data) {
-        this.data = data;
-        this.sourcePage = data.getWikiPage();
-    }
+  public TestPage(WikiPage sourcePage) {
+    this.sourcePage = sourcePage;
+  }
 
-    public WikiPage getSourcePage() { return sourcePage; }
-    public PageData getData() { return data == null ? sourcePage.getData() : data; }
-    public ReadOnlyPageData parsedData() { return sourcePage.readOnlyData(); }
-    public PageData getDecoratedData() { return decoratedData != null ? decoratedData : getData(); }
-    public String getName() { return sourcePage.getName(); }
+  public TestPage(PageData data) {
+    this.data = data;
+    this.sourcePage = data.getWikiPage();
+  }
 
-    public void decorate(String decoratedContent) {
-        decoratedData = new PageData(sourcePage, decoratedContent);
-   }
+  public WikiPage getSourcePage() {
+    return sourcePage;
+  }
 
-    public boolean isSlim() {
-        return "slim".equalsIgnoreCase(parsedData().getVariable("TEST_SYSTEM"));
-    }
+  public PageData getData() {
+    return data == null ? sourcePage.getData() : data;
+  }
 
-    public boolean isTestPage() {
-      return parsedData().hasAttribute("Test");
-    }
+  public ReadOnlyPageData parsedData() {
+    return sourcePage.readOnlyData();
+  }
 
-    private WikiPage sourcePage;
-    private PageData data;
-    private PageData decoratedData;
+  public PageData getDecoratedData() {
+    return decoratedData != null ? decoratedData : getData();
+  }
+
+  public String getName() {
+    return sourcePage.getName();
+  }
+
+  public void decorate(String decoratedContent) {
+    decoratedData = new PageData(sourcePage, decoratedContent);
+  }
+
+  public boolean isSlim() {
+    return "slim".equalsIgnoreCase(parsedData().getVariable("TEST_SYSTEM"));
+  }
+
+  public boolean isTestPage() {
+    return parsedData().hasAttribute("Test");
+  }
 }
