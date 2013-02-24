033aab811244546aa6d2495b7cf799174c129227
Arjan Molenaar <gaphor@gmail.com>
2013-02-24 21:26:15 +0100

    Remove preparsed scenario stuff, since it does not improve performance.

--- src/fitnesse/testsystems/slim/HtmlSlimTestSystem.java
+++ src/fitnesse/testsystems/slim/HtmlSlimTestSystem.java
@@ -2,24 +2,20 @@
 // Released under the terms of the CPL Common Public License version 1.0.
 package fitnesse.testsystems.slim;
 
+import java.util.List;
+
 import fitnesse.testsystems.TestPage;
 import fitnesse.testsystems.TestSystemListener;
 import fitnesse.testsystems.slim.tables.SlimTable;
-import fitnesse.wiki.PageCrawlerImpl;
 import fitnesse.wiki.ReadOnlyPageData;
 import fitnesse.wiki.WikiPage;
-import fitnesse.wiki.WikiPagePath;
 import fitnesse.wikitext.parser.ParsedPage;
-
-import java.util.Collections;
-import java.util.List;
 
 /*
 TODO: scan for scenario's on each parent level. Create a TestContext for each page level (linking the parent)
 TODO: Ease page loading by parsing the scenario tables only once. figure out what output should look like.
  */
 public class HtmlSlimTestSystem extends SlimTestSystem {
-  private ParsedPage preparsedScenarioLibrary;
   private HtmlTableScanner tableScanner;
 
   public HtmlSlimTestSystem(WikiPage page, Descriptor descriptor, TestSystemListener listener) {
@@ -34,37 +30,8 @@ public class HtmlSlimTestSystem extends SlimTestSystem {
 
   protected HtmlTableScanner scanTheTables(ReadOnlyPageData pageData) {
     ParsedPage parsedPage = pageData.getParsedPage();
-    parsedPage.addToFront(getPreparsedScenarioLibrary());
     String html = parsedPage.toHtml();
     return new HtmlTableScanner(html);
-  }
-
-  public ParsedPage getPreparsedScenarioLibrary() {
-    if (preparsedScenarioLibrary == null) {
-      preparsedScenarioLibrary = new ParsedPage(page.readOnlyData().getParsedPage(), getScenarioLibraryContent());
-    }
-    return preparsedScenarioLibrary;
-  }
-
-
-  private String getScenarioLibraryContent() {
-    StringBuilder content = new StringBuilder("!*> Precompiled Libraries\n\n");
-    content.append(includeUncleLibraries());
-    content.append("*!\n");
-    return content.toString();
-  }
-
-  private String includeUncleLibraries() {
-    String content = "";
-    List<WikiPage> uncles = PageCrawlerImpl.getAllUncles("ScenarioLibrary", page);
-    Collections.reverse(uncles);
-    for (WikiPage uncle : uncles)
-      content += include(page.getPageCrawler().getFullPath(uncle));
-    return content;
-  }
-
-  private String include(WikiPagePath path) {
-    return "!include -c ." + path + "\n";
   }
 
   @Override
