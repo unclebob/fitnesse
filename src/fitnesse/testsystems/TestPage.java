unclebob/fitnesse

commit 7496cecd0d8547d3b14f553f40101ac8be661d47

Simplified collapsible section layout.

FitNesseRoot/FitNesse/SuiteAcceptanceTests/SuiteResponderTests/SuiteTestResponders/SuiteSetUpAndTearDown/TestSetUpAndTearDownAreCollapsable/properties.xml
@@ -2,7 +2,7 @@
 <properties>
 	<Edit>true</Edit>
 	<Files>true</Files>
-	<LastModified>20090318113214</LastModified>
+	<Help></Help>
 	<Properties>true</Properties>
 	<RecentChanges>true</RecentChanges>
 	<Refactor>true</Refactor>

FitNesseRoot/FitNesse/SuiteAcceptanceTests/SuiteResponderTests/SuiteTestResponders/SuiteSetUpAndTearDown/TestSetUpAndTearDownAreCollapsableWithVariable/properties.xml
@@ -2,7 +2,7 @@
 <properties>
 	<Edit/>
 	<Files/>
-	<Help/>
+	<Help></Help>
 	<Properties/>
 	<RecentChanges/>
 	<Refactor/>

FitNesseRoot/FitNesse/SuiteAcceptanceTests/SuiteResponderTests/SuiteTestResponders/SuiteSetUpAndTearDown/TestSetUpAndTearDownAreHidden/properties.xml
@@ -1,7 +1,7 @@
 <?xml version="1.0"?>
 <properties>
 	<Files/>
-	<LastModified>20090314114812</LastModified>
+	<Help></Help>
 	<RecentChanges/>
 	<Test/>
 	<WhereUsed/>

FitNesseRoot/FitNesse/SuiteAcceptanceTests/SuiteResponderTests/SuiteTestResponders/SuiteSetUpAndTearDown/TestSetUpAndTearDownAreUncollapsedWithVariable/properties.xml
@@ -2,6 +2,7 @@
 <properties>
 	<Edit>true</Edit>
 	<Files>true</Files>
+	<Help></Help>
 	<Properties>true</Properties>
 	<RecentChanges>true</RecentChanges>
 	<Refactor>true</Refactor>

FitNesseRoot/FitNesse/SuiteAcceptanceTests/SuiteResponderTests/SuiteTestResponders/SuiteSetUpAndTearDown/TestSuiteSetUpAndTearDownAreHidden/properties.xml
@@ -2,6 +2,7 @@
 <properties>
 	<Edit>true</Edit>
 	<Files>true</Files>
+	<Help></Help>
 	<Properties>true</Properties>
 	<RecentChanges>true</RecentChanges>
 	<Refactor>true</Refactor>

FitNesseRoot/FitNesse/SuiteAcceptanceTests/SuiteWidgetTests/TestCollapsableSections/properties.xml
@@ -1,7 +1,7 @@
 <?xml version="1.0"?>
 <properties>
 	<Files/>
-	<LastModified>20090204172330</LastModified>
+	<Help></Help>
 	<RecentChanges/>
 	<Test/>
 	<WhereUsed/>

FitNesseRoot/FitNesse/SuiteAcceptanceTests/SuiteWidgetTests/TestIncludeCollapsed/properties.xml
@@ -1,11 +1,11 @@
 <?xml version="1.0"?>
 <properties>
-  <Files/>
-  <LastModified>20081025203911</LastModified>
-  <RecentChanges/>
-  <Suites/>
-  <Test/>
-  <WhereUsed/>
-  <saveId>1224985151218</saveId>
-  <ticketId>1162019334740908922</ticketId>
+	<Files/>
+	<Help></Help>
+	<RecentChanges/>
+	<Suites/>
+	<Test/>
+	<WhereUsed/>
+	<saveId>1224985151218</saveId>
+	<ticketId>1162019334740908922</ticketId>
 </properties>

FitNesseRoot/FitNesse/SuiteAcceptanceTests/SuiteWidgetTests/TestIncludeIsCollapsable/properties.xml
@@ -1,10 +1,10 @@
 <?xml version="1.0"?>
 <properties>
-  <Files/>
-  <LastModified>20081025204002</LastModified>
-  <RecentChanges/>
-  <Test/>
-  <WhereUsed/>
-  <saveId>1224985202193</saveId>
-  <ticketId>-5717088232592493606</ticketId>
+	<Files/>
+	<Help></Help>
+	<RecentChanges/>
+	<Test/>
+	<WhereUsed/>
+	<saveId>1224985202193</saveId>
+	<ticketId>-5717088232592493606</ticketId>
 </properties>

commit 9ec27a60771922bc6f1303255a09e22b0bb5be14

Include content directly in page, instead of using !include Fix issue with expanding tear down page introduced in 7496cecd.

FitNesseRoot/FitNesse/SuiteAcceptanceTests/SuiteResponderTests/SuiteTestResponders/SuiteSetUpAndTearDown/TestSetUpAndTearDownAreCollapsable/properties.xml
@@ -2,7 +2,6 @@
 <properties>
 	<Edit>true</Edit>
 	<Files>true</Files>
-	<Help></Help>
 	<Properties>true</Properties>
 	<RecentChanges>true</RecentChanges>
 	<Refactor>true</Refactor>

FitNesseRoot/FitNesse/SuiteAcceptanceTests/SuiteResponderTests/SuiteTestResponders/SuiteSetUpAndTearDown/TestSetUpAndTearDownAreCollapsableWithVariable/properties.xml
@@ -2,11 +2,9 @@
 <properties>
 	<Edit/>
 	<Files/>
-	<Help></Help>
 	<Properties/>
 	<RecentChanges/>
 	<Refactor/>
-	<Suites/>
 	<Test/>
 	<Versions/>
 	<WhereUsed/>

FitNesseRoot/FitNesse/SuiteAcceptanceTests/SuiteResponderTests/SuiteTestResponders/SuiteSetUpAndTearDown/TestSetUpAndTearDownAreHidden/properties.xml
@@ -1,7 +1,6 @@
 <?xml version="1.0"?>
 <properties>
 	<Files/>
-	<Help></Help>
 	<RecentChanges/>
 	<Test/>
 	<WhereUsed/>

FitNesseRoot/FitNesse/SuiteAcceptanceTests/SuiteResponderTests/SuiteTestResponders/SuiteSetUpAndTearDown/TestSetUpAndTearDownAreUncollapsedWithVariable/properties.xml
@@ -2,7 +2,6 @@
 <properties>
 	<Edit>true</Edit>
 	<Files>true</Files>
-	<Help></Help>
 	<Properties>true</Properties>
 	<RecentChanges>true</RecentChanges>
 	<Refactor>true</Refactor>

FitNesseRoot/FitNesse/SuiteAcceptanceTests/SuiteResponderTests/SuiteTestResponders/SuiteSetUpAndTearDown/TestSuiteSetUpAndTearDownAreHidden/properties.xml
@@ -2,7 +2,6 @@
 <properties>
 	<Edit>true</Edit>
 	<Files>true</Files>
-	<Help></Help>
 	<Properties>true</Properties>
 	<RecentChanges>true</RecentChanges>
 	<Refactor>true</Refactor>

src/fitnesse/testsystems/TestPage.java
@@ -10,6 +10,7 @@ import fitnesse.wiki.PathParser;
 import fitnesse.wiki.ReadOnlyPageData;
 import fitnesse.wiki.WikiPage;
 import fitnesse.wiki.WikiPagePath;
+import util.GracefulNamer;
 
 public class TestPage {
   public static final String TEAR_DOWN = "TearDown";
@@ -72,43 +73,47 @@ public class TestPage {
 
   protected void decorate(WikiPage wikiPage, StringBuilder decoratedContent) {
     if (wikiPage == getSetUp()) {
-      includePage(wikiPage, "-setup", decoratedContent);
+      includePage(wikiPage, collapse("SETUP"), decoratedContent);
     } else if (wikiPage == getTearDown()) {
-      includePage(wikiPage, "-teardown", decoratedContent);
+      includePage(wikiPage, collapse("TEARDOWN"), decoratedContent);
     } else if (getScenarioLibraries().contains(wikiPage)) {
-      includeScenarioLibrary(wikiPage, decoratedContent);
+      includePage(wikiPage, true, decoratedContent);
     } else {
       decoratedContent.append(wikiPage.readOnlyData().getContent());
     }
   }
 
+  protected boolean collapse(String page) {
+    String collapse = sourcePage.getData().getVariable("COLLAPSE_" + page);
+    return collapse == null || "true".equals(collapse);
+  }
 
   protected void includeScenarioLibraries(StringBuilder decoratedContent) {
     if (!getScenarioLibraries().isEmpty()) {
       decoratedContent.append("!*> Scenario Libraries\n");
       for (WikiPage scenarioLibrary : getScenarioLibraries()) {
-        includeScenarioLibrary(scenarioLibrary, decoratedContent);
+        includePage(scenarioLibrary, true, decoratedContent);
       }
       decoratedContent.append("*!\n");
     }
   }
 
-  protected void includeScenarioLibrary(WikiPage scenarioLibrary, StringBuilder newPageContent) {
-    newPageContent.append("!include -c .");
-    newPageContent.append(getPathNameForPage(scenarioLibrary));
-    newPageContent.append("\n");
-  }
-
-  protected void includePage(WikiPage wikiPage, String arg, StringBuilder newPageContent) {
+  protected void includePage(WikiPage wikiPage, boolean collapse, StringBuilder newPageContent) {
     if (wikiPage == null)
       return;
     String pagePathName = getPathNameForPage(wikiPage);
+    String content = wikiPage.readOnlyData().getContent();
     newPageContent
-            .append("\n!include ")
-            .append(arg)
-            .append(" .")
+            .append("\n!*")
+            .append(collapse ? ">" : "")
+            .append(" !-")
+            .append(GracefulNamer.regrace(wikiPage.getName()))
+            .append("-! page: .")
             .append(pagePathName)
-            .append("\n");
+            .append("\n")
+            .append(content)
+            .append(content.endsWith("\n") ? "" : "\n")
+            .append("*!\n");
   }
 
   private String getPathNameForPage(WikiPage page) {

src/fitnesse/testsystems/TestPageWithSuiteSetUpAndTearDown.java
@@ -16,15 +16,25 @@ public class TestPageWithSuiteSetUpAndTearDown extends TestPage {
     StringBuilder decoratedContent = new StringBuilder(1024);
     includeScenarioLibraries(decoratedContent);
 
-    includePage(getSuiteSetUp(), "-setup", decoratedContent);
-    includePage(getSetUp(), "-setup", decoratedContent);
+    decorate(getSuiteSetUp(), decoratedContent);
+
+    decorate(getSetUp(), decoratedContent);
 
     decoratedContent.append(parsedData().getContent());
 
-    includePage(getTearDown(), "-teardown", decoratedContent);
-    includePage(getSuiteTearDown(), "-teardown", decoratedContent);
+    decorate(getTearDown(), decoratedContent);
+
+    decorate(getSuiteTearDown(), decoratedContent);
 
     return new PageData(getSourcePage(), decoratedContent.toString());
+  }
+
+  protected void decorate(WikiPage wikiPage, StringBuilder decoratedContent) {
+    if (wikiPage == getSuiteSetUp() || wikiPage == getSuiteTearDown()) {
+      includePage(wikiPage, true, decoratedContent);
+    } else {
+      super.decorate(wikiPage, decoratedContent);
+    }
   }
 
   public WikiPage getSuiteSetUp() {