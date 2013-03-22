unclebob/fitnesse

commit cfb535f5cbf64c2dd11c88533a62d5ea918ab280

Removed noChunk property from FitNesseContext

src/fitnesse/http/MockRequestBuilder.java
@@ -5,30 +5,34 @@ import fitnesse.http.Request;
 
 public class MockRequestBuilder {
   protected String specification;
-  
+  private boolean noChunk = false;
+
   public MockRequestBuilder(String specification) {
     this.specification = specification;
     validate();
   }
 
   public Request build() {
-    Request request = new MockRequest();
+    MockRequest request = new MockRequest();
     request.parseRequestUri(getCommand());
     if (hasCredentials()) {
       request.setCredentials(getUsername(), getPassword());
     }
+    if (noChunk) {
+      request.addInput("noChunk", true);
+    }
     return request;
   }
-  
+
   private String getCommand() {
     String actualCommand = null;
-    
+
     if (hasCredentials())
       actualCommand = commandParts()[2];
     else
       actualCommand = specification;
-      
-    
+
+
     if (actualCommand.startsWith("/"))
       return actualCommand;
     else
@@ -42,12 +46,12 @@ public class MockRequestBuilder {
   private boolean hasNoCredentials() {
     return (commandParts().length == 1);
   }
-  
+
   private void validate() {
     if (!hasCredentials() && !hasNoCredentials())
       throw new IllegalArgumentException("Command specification [" + specification + "] invalid. Format shold be /cmd or user:pass:/cmd");
   }
-  
+
   private String[] commandParts() {
     return specification.split(":");
   }
@@ -58,5 +62,10 @@ public class MockRequestBuilder {
 
   private String getPassword() {
     return commandParts()[1];
+  }
+
+  public MockRequestBuilder noChunk() {
+    this.noChunk = true;
+    return this;
   }
 }

src/fitnesse/responders/ChunkingResponder.java
@@ -30,8 +30,8 @@ public abstract class ChunkingResponder implements Responder, ChunkedDataProvide
     this.root = context.root;
     String format = (String) request.getInput("format");
     response = new ChunkedResponse(format, this);
-    
-    if (dontChunk || context.doNotChunk || request.hasInput("nochunk"))
+
+    if (dontChunk || request.hasInput("nochunk"))
       response.turnOffChunking();
     getRequestedPage(request);
     if (page == null && shouldRespondWith404())

src/fitnesse/FitNesse.java
@@ -104,7 +104,7 @@ public class FitNesse {
   }
 
   public void executeSingleCommand(String command, OutputStream out) throws Exception {
-    Request request = new MockRequestBuilder(command).build();
+    Request request = new MockRequestBuilder(command).noChunk().build();
     FitNesseExpediter expediter = new FitNesseExpediter(new MockSocket(), context);
     Response response = expediter.createGoodResponse(request);
     MockResponseSender sender = new MockResponseSender.OutputStreamSender(out);

src/fitnesse/FitNesseContext.java
@@ -31,7 +31,6 @@ public class FitNesseContext {
   public String defaultNewPageContent = "!contents -R2 -g -p -f -h";
   public Logger logger;
   public Authenticator authenticator = new PromiscuousAuthenticator();
-  public boolean doNotChunk;
   public String pageTheme = "fitnesse_straight";
 
 

src/fitnesseMain/FitNesseMain.java
@@ -66,7 +66,6 @@ public class FitNesseMain {
   }
 
   private static void executeSingleCommand(Arguments arguments, FitNesse fitnesse, FitNesseContext context) throws Exception {
-    context.doNotChunk = true;
     TestTextFormatter.finalErrorCount = 0;
     System.out.println("Executing command: " + arguments.getCommand());
     System.out.println("-----Command Output-----");

commit 2b6cf41a0eac75c7bc3d8d5036fea8f9aec5e34b

Consistently spell 'nochunk' request input key
cfb535f5cbf64c2dd11c88533a62d5ea918ab280 introduced a bug: noChunk was not
consistently spelled. This caused running FitNesse with '-c' to produce
chunked output, which is not desirable.

src/fitnesse/http/MockRequestBuilder.java
@@ -5,7 +5,7 @@ import fitnesse.http.Request;
 
 public class MockRequestBuilder {
   protected String specification;
-  private boolean noChunk = false;
+  private boolean chunk = true;
 
   public MockRequestBuilder(String specification) {
     this.specification = specification;
@@ -18,8 +18,8 @@ public class MockRequestBuilder {
     if (hasCredentials()) {
       request.setCredentials(getUsername(), getPassword());
     }
-    if (noChunk) {
-      request.addInput("noChunk", true);
+    if (!chunk) {
+      request.addInput(Request.NOCHUNK, true);
     }
     return request;
   }
@@ -65,7 +65,7 @@ public class MockRequestBuilder {
   }
 
   public MockRequestBuilder noChunk() {
-    this.noChunk = true;
+    this.chunk = false;
     return this;
   }
 }

src/fitnesse/http/Request.java
@@ -27,6 +27,9 @@ public class Request {
 
   private static Collection<String> allowedMethods = buildAllowedMethodList();
 
+  /** input key to suppress chunking. */
+  public static final String NOCHUNK = "nochunk";
+
   protected StreamReader input;
   protected String requestURI;
   private String resource;

src/fitnesse/responders/ChunkingResponder.java
@@ -31,7 +31,7 @@ public abstract class ChunkingResponder implements Responder, ChunkedDataProvide
     String format = (String) request.getInput("format");
     response = new ChunkedResponse(format, this);
 
-    if (dontChunk || request.hasInput("nochunk"))
+    if (dontChunk || request.hasInput(Request.NOCHUNK))
       response.turnOffChunking();
     getRequestedPage(request);
     if (page == null && shouldRespondWith404())