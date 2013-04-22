60c91740a0ea6a1b1df333652ae7bc308b123352
Arjan Molenaar <gaphor@gmail.com>
2013-04-22 07:13:03 +0200

    Remove CachingPage.

--- src/fitnesse/wiki/ProxyPageTest.java
+++ src/fitnesse/wiki/ProxyPageTest.java
@@ -2,15 +2,20 @@
 // Released under the terms of the CPL Common Public License version 1.0.
 package fitnesse.wiki;
 
+import static junit.framework.Assert.*;
+import static org.junit.Assert.assertEquals;
+
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
 import fitnesse.testutil.FitNesseUtil;
 import fitnesse.wiki.mem.InMemoryPage;
-import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-public class ProxyPageTest extends TestCase {
+public class ProxyPageTest {
   private WikiPage root;
   private WikiPage original;
   private ProxyPage proxy;
@@ -18,8 +23,9 @@ public class ProxyPageTest extends TestCase {
   private PageCrawler crawler;
   private WikiPagePath page1Path;
 
+  @Before
   public void setUp() throws Exception {
-    CachingPage.cacheTime = 0;
+    ProxyPage.cacheTime = 0;
     root = InMemoryPage.makeRoot("RooT");
     crawler = root.getPageCrawler();
     page1Path = PathParser.parse("PageOne");
@@ -36,22 +42,26 @@ public class ProxyPageTest extends TestCase {
     proxy.setHostPort("localhost", FitNesseUtil.PORT);
   }
 
+  @After
   public void tearDown() throws Exception {
     FitNesseUtil.stopFitnesse();
   }
 
+  @Test
   public void testConstructor() throws Exception {
     assertEquals("page one content", proxy.getData().getContent());
     assertEquals("PageOne", proxy.getName());
     assertEquals(true, proxy.getData().hasAttribute("Attr1"));
   }
 
+  @Test
   public void testHasChildren() throws Exception {
     assertEquals(false, proxy.hasChildPage("BlaH"));
     assertEquals(true, proxy.hasChildPage("ChildOne"));
     assertEquals(true, proxy.hasChildPage("ChildTwo"));
   }
 
+  @Test
   public void testGetChildrenOneAtATime() throws Exception {
     WikiPage child1 = proxy.getChildPage("ChildOne");
     assertEquals("child one", child1.getData().getContent());
@@ -59,6 +69,7 @@ public class ProxyPageTest extends TestCase {
     assertEquals("child two", child2.getData().getContent());
   }
 
+  @Test
   public void testGetAllChildren() throws Exception {
     List<?> children = proxy.getChildren();
     assertEquals(2, children.size());
@@ -68,6 +79,7 @@ public class ProxyPageTest extends TestCase {
     assertEquals(true, "ChildOne".equals(child.getName()) || "ChildTwo".equals(child.getName()));
   }
 
+  @Test
   public void testSetHostAndPort() throws Exception {
     List<?> children = proxy.getChildren();
     proxy.setHostPort("a.new.host", 123);
@@ -82,6 +94,7 @@ public class ProxyPageTest extends TestCase {
     }
   }
 
+  @Test
   public void testCanFindNewChildOfAProxy() throws Exception {
     ProxyPage child1Proxy = (ProxyPage) proxy.getChildPage("ChildOne");
     assertNull(child1Proxy.getChildPage("ChildOneChild"));
@@ -91,6 +104,7 @@ public class ProxyPageTest extends TestCase {
     assertNotNull(child1Proxy.getChildPage("ChildOneChild"));
   }
 
+  @Test
   public void testHasSubpageCallsLoadChildrenNoMoreThanNeeded() throws Exception {
     proxy.getNormalChildren();
     ProxyPage.retrievalCount = 0;
@@ -100,9 +114,22 @@ public class ProxyPageTest extends TestCase {
     assertEquals(1, ProxyPage.retrievalCount);
   }
 
+  @Test
   public void testGetVersions() {
     Collection<VersionInfo> versions = proxy.getVersions();
     assertNotNull(versions);
     assertEquals(3, versions.size());
   }
+
+
+  @Test
+  public void testPageDataIsCached() throws Exception {
+    ProxyPage.cacheTime = 100;
+
+    PageData data1 = proxy.getCachedData();
+    PageData data2 = proxy.getCachedData();
+
+    assertSame(data1, data2);
+  }
+
 }
