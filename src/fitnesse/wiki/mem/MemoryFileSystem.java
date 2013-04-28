455c3166797de4c88b0f7337af44399ffe4c6153
Arjan Molenaar <gaphor@gmail.com>
2013-04-28 22:07:23 +0200

    Store last modified date on Memory filesystem

--- src/fitnesse/wiki/mem/MemoryFileSystem.java
+++ src/fitnesse/wiki/mem/MemoryFileSystem.java
@@ -1,6 +1,7 @@
 package fitnesse.wiki.mem;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Hashtable;
 import java.util.Iterator;
 
@@ -8,14 +9,14 @@ import fitnesse.wiki.fs.FileSystem;
 import util.Clock;
 
 public class MemoryFileSystem implements FileSystem {
-    private final Hashtable<String, String> files = new Hashtable<String, String>();
+    private final Hashtable<String, Payload> files = new Hashtable<String, Payload>();
 
     public void makeFile(String path, String content) {
-        files.put(path, content);
+        files.put(path, payload(content));
     }
 
     public void makeDirectory(String path) {
-        files.put(path, "");
+        files.put(path, payload(""));
     }
 
     public boolean exists(String path) {
@@ -40,7 +41,7 @@ public class MemoryFileSystem implements FileSystem {
     }
 
     public String getContent(String path) {
-        return files.get(path);
+        return files.get(path).payload;
     }
 
   @Override
@@ -54,6 +55,20 @@ public class MemoryFileSystem implements FileSystem {
 
   @Override
   public long lastModified(String path) {
-    return Clock.currentTimeInMillis();
+    return files.get(path).lastModified;
+  }
+
+  private Payload payload(String payload) {
+    return new Payload(payload);
+  }
+
+  private static class Payload {
+    private final String payload;
+    private final long lastModified;
+
+    private Payload(String payload) {
+      this.payload = payload;
+      this.lastModified = new Date().getTime();
+    }
   }
 }
