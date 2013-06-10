98da6b042042341686166984976c882da76d122a
Arjan Molenaar <gaphor@gmail.com>
2013-06-10 15:49:16 +0200

    organise imports.

--- src/fitnesse/responders/SerializedPageResponderTest.java
+++ src/fitnesse/responders/SerializedPageResponderTest.java
@@ -11,7 +11,6 @@ import fitnesse.Responder;
 import fitnesse.http.MockRequest;
 import fitnesse.http.SimpleResponse;
 import fitnesse.testutil.FitNesseUtil;
-import fitnesse.wiki.fs.FileSystemPage;
 import fitnesse.wiki.mem.InMemoryPage;
 import fitnesse.wiki.PageCrawler;
 import fitnesse.wiki.PageData;
