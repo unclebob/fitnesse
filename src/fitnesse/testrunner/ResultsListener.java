ca15f6a289af054ec126e91370bc52aaa21dc6c8
Arjan Molenaar <gaphor@gmail.com>
2013-09-01 21:13:00 +0200

    Let ResultsListener inherit from TestSystemListener.

--- src/fitnesse/testrunner/ResultsListener.java
+++ src/fitnesse/testrunner/ResultsListener.java
@@ -2,40 +2,19 @@
 // Released under the terms of the CPL Common Public License version 1.0.
 package fitnesse.testrunner;
 
-import fitnesse.testsystems.Assertion;
-import fitnesse.testsystems.ExceptionResult;
-import fitnesse.testsystems.ExecutionLog;
-import fitnesse.testsystems.TestResult;
-import fitnesse.testsystems.TestSummary;
-import fitnesse.testsystems.TestSystem;
-import util.TimeMeasurement;
-
 import java.io.IOException;
 
-public interface ResultsListener {
+import fitnesse.testsystems.TestSystemListener;
+
+public interface ResultsListener extends TestSystemListener<WikiTestPage> {
 
   // runner specific
 
+  // TODO: replace by Closeable.close().
   public void allTestingComplete() throws IOException;
 
   public void setExecutionLogAndTrackingId(String stopResponderId, CompositeExecutionLog log);
 
   // Only really used in SuiteHtmlFormatter
   public void announceNumberTestsToRun(int testsToRun);
-
-  // The remaining methods are redundant with TestSystemListener:
-
-  public void testSystemStarted(TestSystem testSystem);
-
-  public void testStarted(WikiTestPage test) throws IOException;
-
-  public void testOutputChunk(String output) throws IOException;
-
-  public void testAssertionVerified(Assertion assertion, TestResult testResult);
-
-  public void testExceptionOccurred(Assertion assertion, ExceptionResult exceptionResult);
-
-  public void testComplete(WikiTestPage test, TestSummary testSummary) throws IOException;
-
-  public void testSystemStopped(TestSystem testSystem, ExecutionLog executionLog, Throwable cause);
 }
