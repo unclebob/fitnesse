497b7f54b39d82783d6119409be982018ad05ab1
Arjan Molenaar <gaphor@gmail.com>
2013-10-16 17:02:18 +0200

    Add documentation to TestSystem interface.

--- src/fitnesse/testsystems/TestSystem.java
+++ src/fitnesse/testsystems/TestSystem.java
@@ -6,17 +6,49 @@ import java.io.IOException;
 
 public interface TestSystem {
 
+  /**
+   * Get the test system name identifier, as it was provided by the user.
+   * @return
+   */
   String getName();
 
+  /**
+   * Start the test system.
+   * @throws IOException
+   */
   void start() throws IOException;
 
+  /**
+   * Close the test system. This is typically performed from the test execution thread.
+   * @throws IOException
+   * @throws InterruptedException
+   */
   void bye() throws IOException, InterruptedException;
 
+  /**
+   * Kill the test system. This is typically invoked asynchronously.
+   * @throws IOException
+   */
   void kill() throws IOException;
 
+  /**
+   * Run a collection of tests.
+   * @param pageToTest
+   * @throws IOException
+   * @throws InterruptedException
+   */
   void runTests(TestPage pageToTest) throws IOException, InterruptedException;
 
+  /**
+   * System is up and running.
+   * @return
+   */
   boolean isSuccessfullyStarted();
 
+  /**
+   * Add a listener. During test execution the listeners will be kept informed about the
+   * status of the test execution.
+   * @param listener
+   */
   void addTestSystemListener(TestSystemListener listener);
 }
