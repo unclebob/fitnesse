4c9f2861fb9e126ea31f5846c255af5312cc279f
Arjan Molenaar <gaphor@gmail.com>
2013-11-17 23:31:53 +0100

    Remove package fitnesse.schedule.

--- src/fitnesse/schedule/ScheduleItem.java
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2003-2009 by Object Mentor, Inc. All rights reserved.
-// Released under the terms of the CPL Common Public License version 1.0.
-package fitnesse.schedule;
-
-public interface ScheduleItem {
-  public boolean shouldRun(long time) throws Exception;
-
-  public void run(long time) throws Exception;
-}
