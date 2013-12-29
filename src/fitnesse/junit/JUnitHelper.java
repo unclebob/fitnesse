074a0df3a4ea4ff0f0e5d3f8e65db7db8b905ead
Arjan Molenaar <gaphor@gmail.com>
2013-12-29 13:29:39 +0100

    Remove TestHelper, move remaining bits to JUnitHelper.

--- src/fitnesse/junit/JUnitHelper.java
+++ src/fitnesse/junit/JUnitHelper.java
@@ -1,8 +1,11 @@
 package fitnesse.junit;
 
 
+import fitnesse.reporting.JavaFormatter;
 import fitnesse.testsystems.TestSummary;
 import fitnesse.testsystems.TestSystemListener;
+import fitnesseMain.Arguments;
+import fitnesseMain.FitNesseMain;
 
 import java.text.MessageFormat;
 
@@ -11,40 +14,47 @@ import static org.junit.Assert.assertTrue;
 
 public class JUnitHelper {
 
-  private final TestHelper helper;
+  public static final String PAGE_TYPE_SUITE="suite";
+  public static final String PAGE_TYPE_TEST="test";
+  private final String fitNesseRootPath;
+  private final String outputPath;
+  private final TestSystemListener resultListener;
+  private boolean debug = true;
   private int port = 0;
-
-  public void setPort(int port) {
-    this.port = port;
-  }
 
   public JUnitHelper(String fitNesseRootPath, String outputPath) {
     this(fitNesseRootPath, outputPath, new PrintTestListener());
   }
 
-  public JUnitHelper(String fitNesseDir, String outputDir,
-                     TestSystemListener resultsListener) {
-    helper = new TestHelper(fitNesseDir, outputDir, resultsListener);
+  public JUnitHelper(String fitNesseRootPath, String outputPath,
+                     TestSystemListener listener) {
+    this.fitNesseRootPath = fitNesseRootPath;
+    this.outputPath = outputPath;
+    this.resultListener = listener;
+  }
+
+  public void setPort(int port) {
+    this.port = port;
   }
 
   public void setDebugMode(boolean enabled) {
-    helper.setDebugMode(enabled);
+    debug = enabled;
   }
 
   public void assertTestPasses(String testName) throws Exception {
-    assertPasses(testName, TestHelper.PAGE_TYPE_TEST, null);
+    assertPasses(testName, PAGE_TYPE_TEST, null);
   }
 
   public void assertSuitePasses(String suiteName) throws Exception {
-    assertPasses(suiteName, TestHelper.PAGE_TYPE_SUITE, null);
+    assertPasses(suiteName, PAGE_TYPE_SUITE, null);
   }
 
   public void assertSuitePasses(String suiteName, String suiteFilter) throws Exception {
-    assertPasses(suiteName, TestHelper.PAGE_TYPE_SUITE, suiteFilter);
+    assertPasses(suiteName, PAGE_TYPE_SUITE, suiteFilter);
   }
 
   public void assertSuitePasses(String suiteName, String suiteFilter, String excludeSuiteFilter) throws Exception {
-    assertPasses(suiteName, TestHelper.PAGE_TYPE_SUITE, suiteFilter, excludeSuiteFilter);
+    assertPasses(suiteName, PAGE_TYPE_SUITE, suiteFilter, excludeSuiteFilter);
   }
 
   public void assertPasses(String pageName, String pageType, String suiteFilter) throws Exception {
@@ -52,15 +62,27 @@ public class JUnitHelper {
   }
 
   public void assertPasses(String pageName, String pageType, String suiteFilter, String excludeSuiteFilter) throws Exception {
-    TestSummary summary = helper.run(pageName, pageType, suiteFilter, excludeSuiteFilter, port);
+    TestSummary summary = run(pageName, pageType, suiteFilter, excludeSuiteFilter);
     assertEquals("wrong", 0, summary.wrong);
     assertEquals("exceptions", 0, summary.exceptions);
     assertTrue(msgAtLeastOneTest(pageName, summary), summary.right > 0);
   }
 
   private String msgAtLeastOneTest(String pageName, TestSummary summary) {
-    return
-      MessageFormat.format("at least one test executed in {0}\n{1}",
-        pageName, summary.toString());
+    return MessageFormat.format("at least one test executed in {0}\n{1}",
+              pageName, summary.toString());
+  }
+
+  public  TestSummary run(String pageName, String pageType, String suiteFilter, String excludeSuiteFilter) throws Exception{
+    JavaFormatter testFormatter=JavaFormatter.getInstance(pageName);
+    testFormatter.setResultsRepository(new JavaFormatter.FolderResultsRepository(outputPath));
+    testFormatter.setListener(resultListener);
+    Arguments arguments=new Arguments("-e", "0",
+            "-o",
+            "-p", String.valueOf(port),
+            "-d", fitNesseRootPath,
+            "-c", new CommandBuilder(pageName, pageType).withSuiteFilter(suiteFilter).withExcludeSuiteFilter(excludeSuiteFilter).withDebug(debug).build());
+    new FitNesseMain().launchFitNesse(arguments);
+    return testFormatter.getTotalSummary();
   }
 }
