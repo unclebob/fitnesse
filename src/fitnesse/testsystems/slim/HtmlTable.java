8d89d94181935eeec891f3178ddf0b05430a6e32
Arjan Molenaar <gaphor@gmail.com>
2014-04-07 22:04:12 +0200

    Fix issues with ignored fields in SliM TableTable

--- src/fitnesse/testsystems/slim/HtmlTable.java
+++ src/fitnesse/testsystems/slim/HtmlTable.java
@@ -186,7 +186,7 @@ public class HtmlTable implements Table {
     }
   }
 
-  private Tag newTag(Class<? extends Tag> klass) {
+  private static Tag newTag(Class<? extends Tag> klass) {
     Tag tag = null;
     try {
       tag = klass.newInstance();
@@ -267,7 +267,7 @@ public class HtmlTable implements Table {
     }
   }
 
-  class Cell {
+  static class Cell {
     private final TableColumn columnNode;
     private final String originalContent;
     private SlimTestResult testResult;
@@ -347,7 +347,9 @@ public class HtmlTable implements Table {
           }
           return String.format("<span class=\"fail\">%s</span>", message);
         case IGNORE:
-          return String.format("%s <span class=\"ignore\">%s</span>", originalContent, message);
+          return testResult.hasMessage()
+                  ? String.format("%s <span class=\"ignore\">%s</span>", originalContent, message)
+                  : String.format("<span class=\"ignore\">%s</span>", originalContent);
         case ERROR:
           return String.format("%s <span class=\"error\">%s</span>", originalContent, message);
       }
@@ -355,7 +357,7 @@ public class HtmlTable implements Table {
     }
   }
 
-  private String asHtml(String text) {
+  private static String asHtml(String text) {
     return qualifiesAsHtml(text) ? text : Utils.escapeHTML(text);
   }
 
