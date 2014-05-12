0344a0d7121ccdbb0faf32ab68c5f43127001250
Antoine Aumjaud <antoine_dev@aumjaud.com>
2014-05-12 17:33:03 +0200

    Change logging handler to be compliant with behavior before java_logging feature commit (91e93be).
    Before, standard and error output was used and give information of
    Fitnesse server status.
    Now, with Java logging, the output used is always System.err.
    
    Other applications as "Jenkins Fitnesse plugin" wait for something in
    the standard output.
    
    I've created a new JUL Handler which writes in Sytem.out all logs with
    level < LEVEL.WARNING, others goes to System.err.
    
    (Test with "Jenkins Fitnesse plugin":
    https://github.com/jenkinsci/fitnesse-plugin/blob/master/src/main/java/hudson/plugins/fitnesse/StdConsole.java)

---
+++ src/fitnesse/logging/ConsoleHandler.java
@@ -0,0 +1,61 @@
+package fitnesse.logging;
+
+import java.util.logging.ErrorManager;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+/**
+ * This <tt>Handler</tt> publishes log records on <tt>System.err</tt> or
+ * <tt>Sytem.out</tt>
+ * <p>
+ * If level <tt>WARNING</tt> or <tt>SEVERE</tt>, log message is sent to
+ * <tt>System.err</tt> otherwise to <tt>System.out</tt>.
+ */
+
+public class ConsoleHandler extends Handler {
+    /**
+     * Initialize the console Handler level and formatter
+     */
+    public ConsoleHandler() {
+	setLevel(Level.INFO);
+	setFormatter(new LogFormatter());
+    }
+
+    @Override
+    public void publish(LogRecord record) {
+	if (!isLoggable(record)) {
+	    return;
+	}
+
+	String msg;
+	try {
+	    msg = getFormatter().format(record);
+	} catch (Exception ex) {
+	    // We don't want to throw an exception here, but we report the
+	    // exception to any registered ErrorManager.
+	    reportError(null, ex, ErrorManager.FORMAT_FAILURE);
+	    return;
+	}
+
+	if (record.getLevel().intValue() >= Level.WARNING.intValue()) {
+	    System.err.println(msg);
+	} else {
+	    System.out.println(msg);
+	}
+    }
+
+    /**
+     * Flush but not to close the output streams.
+     */
+    @Override
+    public void close() {
+	flush();
+    }
+
+    @Override
+    public void flush() {
+	System.out.flush();
+	System.err.flush();
+    }
+}
