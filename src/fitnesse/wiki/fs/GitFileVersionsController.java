064242017da3898a60f9991c0347d18d4dcafcdf
lldias <lldias@github.com>
2014-06-03 19:36:05 -0300

    [git] fix for IndexOutOfBoundsException when viewing RecentChanges

--- src/fitnesse/wiki/fs/GitFileVersionsController.java
+++ src/fitnesse/wiki/fs/GitFileVersionsController.java
@@ -185,10 +185,15 @@ public class GitFileVersionsController implements VersionsController, RecentChan
 
     assert pagePath.startsWith(workTreePath);
 
-    // Add 1 for trailing '/' (not included in abs. path)
-    pagePath = pagePath.substring(workTreePath.length() + 1);
+    pagePath = pagePath.substring(workTreePath.length());
+    
     // git stores paths unix-style
     pagePath = pagePath.replace(File.separatorChar, '/');
+    
+    // Skip starting '/'
+    if (pagePath.startsWith("/"))
+        pagePath = pagePath.substring(1);
+
     return pagePath;
   }
 
