0b2dceccbf6141144eec004394b860c32d7983b6
antoine-aumjaud <antoine_dev@aumjaud.com>
2014-06-05 15:49:42 +0200

    Add acceptance test

---
+++ src/fitnesse/slim/test/TableTableIncFirstCol.java
@@ -0,0 +1,21 @@
+package fitnesse.slim.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TableTableIncFirstCol {
+
+  public List<List<String>> doTable(List<List<?>> table) {
+    List<List<String>> ret = new ArrayList<List<String>>();
+
+    for (List<?> line : table) {
+      List<String> retLine = new ArrayList<String>();
+      ret.add(retLine);
+
+      retLine.add("no change");
+      retLine.add("pass:" + (Integer.parseInt(line.get(0).toString()) + 1));
+    }
+
+    return ret;
+  }
+}
