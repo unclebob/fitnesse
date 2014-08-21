02ca9c557eaaa60418a3cc5267107bfd264d6cce
unknown <szukov@stivan-859.StemCell.com>
2014-08-21 16:04:40 -0700

    Restored to match original version from upstrea/master

--- src/fitnesse/testsystems/CommandRunner.java
+++ src/fitnesse/testsystems/CommandRunner.java
@@ -9,7 +9,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -55,8 +54,9 @@ public class CommandRunner {
   }
 
   public void asynchronousStart() throws IOException {
-    Runtime rt = Runtime.getRuntime();
-    process = rt.exec(StringUtil.join(Arrays.asList(command), " "), determineEnvironment());
+    ProcessBuilder processBuilder = new ProcessBuilder(command);
+    processBuilder.environment().putAll(determineEnvironment());
+    process = processBuilder.start();
 
     OutputStream stdin = process.getOutputStream();
     InputStream stdout = process.getInputStream();
@@ -95,17 +95,13 @@ public class CommandRunner {
     });
   }
 
-  private String[] determineEnvironment() {
+  private Map<String, String> determineEnvironment() {
     if (environmentVariables == null) {
-      return null;
+      return Collections.emptyMap();
     }
     Map<String, String> systemVariables = new HashMap<String, String>(System.getenv());
     systemVariables.putAll(environmentVariables);
-    List<String> systemVariableAssignments = new ArrayList<String>();
-    for (Map.Entry<String, String> entry : systemVariables.entrySet()) {
-      systemVariableAssignments.add(entry.getKey() + "=" + entry.getValue());
-    }
-    return systemVariableAssignments.toArray(new String[systemVariableAssignments.size()]);    
+    return systemVariables;
   }
 
   public void join() {
