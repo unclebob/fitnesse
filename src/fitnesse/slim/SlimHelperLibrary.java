013c43646ad491a3a77eac17619cbe4c759876e4
Arjan Molenaar <gaphor@gmail.com>
2014-10-26 11:40:08 +0100

    Move getValuefrom{Query|Table}ResultSymbol() to a separate helper class.

--- src/fitnesse/slim/SlimHelperLibrary.java
+++ src/fitnesse/slim/SlimHelperLibrary.java
@@ -1,6 +1,5 @@
 package fitnesse.slim;
 
-import java.util.List;
 import java.util.Stack;
 
 public class SlimHelperLibrary implements StatementExecutorConsumer {
@@ -33,21 +32,8 @@ public class SlimHelperLibrary implements StatementExecutorConsumer {
   public Object cloneSymbol(Object master){
 	  return master;
   }
+
   public Object freeSymbol(){
 	  return null;
-  }
-  
-  public Object getValuefromQueryResultSymbol(List<List<List<Object>>> queryResult, int rowNo, String columnName ){
-	  List<List<Object>> row =  queryResult.get(rowNo);
-	  for(int i=0; i< row.size(); i++){
-		  if(columnName.compareTo( (String) (row.get(i).get(0))) == 0){
-			  return row.get(i).get(1);
-		  }
-	  }
-	  throw new RuntimeException("No column with name '" + columnName + "' found in row " + rowNo); 
-	  
-  }
-  public Object getValuefromTableResultSymbol(List<List<Object>> tableResult, int rowNo, int columnNo ){
-	  return  tableResult.get(rowNo).get(columnNo);
   }
 }
