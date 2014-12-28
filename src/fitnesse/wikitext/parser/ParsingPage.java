2d5c5cf5a1021c2b786c4788fd1934448cbff9bd
Arjan Molenaar <gaphor@gmail.com>
2014-12-28 21:11:45 +0100

    Split Page- and NamedPageVariableSource. Construct composite in constructor.

--- src/fitnesse/wikitext/parser/ParsingPage.java
+++ src/fitnesse/wikitext/parser/ParsingPage.java
@@ -11,31 +11,44 @@ import fitnesse.wiki.UrlPathVariableSource;
 public class ParsingPage implements VariableSource {
 
   private final SourcePage page;
-  private final SourcePage namedPage;
+  private final SourcePage namedPage; // included page
   private final VariableSource variableSource;
   private final Cache cache;
-  private final ParentPageVariableSource parentPageVariableSource;
 
   public ParsingPage(SourcePage page) {
     this(page, null);
   }
 
   public ParsingPage(SourcePage page, VariableSource variableSource) {
-    this(page, page, variableSource, new Cache());
+    this(page, variableSource, new Cache());
+  }
+
+  public ParsingPage(SourcePage page, VariableSource variableSource, Cache cache) {
+    this(page, page, new CompositeVariableSource(
+                      new ApplicationVariableSource(variableSource),
+                      new PageVariableSource(page),
+                      new UserVariableSource(variableSource),
+                      cache,
+                      new ParentPageVariableSource(page, variableSource),
+                      variableSource),
+            cache);
   }
 
   private ParsingPage(SourcePage page, SourcePage namedPage, VariableSource variableSource, Cache cache) {
     this.page = page;
     this.namedPage = namedPage;
-    this.variableSource = variableSource != null ? variableSource : new NothingVariableSource();
+    this.variableSource = new CompositeVariableSource(
+            new NamedPageVariableSource(namedPage),
+            variableSource);
     this.cache = cache;
-    
-    SourcePage parentPage = page.getParent();
-    this.parentPageVariableSource = parentPage != null ? new ParentPageVariableSource(parentPage, variableSource) : null;
   }
 
   public ParsingPage copyForNamedPage(SourcePage namedPage) {
-    return new ParsingPage(this.page, namedPage, variableSource, cache);
+    return new ParsingPage(
+            this.page,
+            namedPage,
+            this.variableSource,
+            this.cache);
   }
 
   public SourcePage getPage() {
@@ -52,23 +65,6 @@ public class ParsingPage implements VariableSource {
 
   @Override
   public Maybe<String> findVariable(String name) {
-    Maybe<String> result = new PageVariableSource(page, namedPage).findVariable(name);
-    if (!result.isNothing()) return result;
-
-    result = new ApplicationVariableSource(variableSource).findVariable(name);
-    if (!result.isNothing()) return result;
-
-    result = new UserVariableSource(variableSource).findVariable(name);
-    if (!result.isNothing()) return result;
-
-    result = cache.findVariable(name);
-    if (!result.isNothing()) return result;
-
-    if (parentPageVariableSource != null) {
-      result = parentPageVariableSource.findVariable(name);
-      if (!result.isNothing()) return result;
-    }
-
     return variableSource.findVariable(name);
   }
 
@@ -91,8 +87,7 @@ public class ParsingPage implements VariableSource {
     }
   }
 
-
-  public static class Cache implements VariableSource {
+  private static class Cache implements VariableSource {
 
     private final Map<String, Maybe<String>> cache;
 
@@ -119,11 +114,9 @@ public class ParsingPage implements VariableSource {
   public static class PageVariableSource implements VariableSource {
 
     private final SourcePage page;
-    private final SourcePage namedPage;
 
-    public PageVariableSource(SourcePage page, SourcePage namedPage) {
+    public PageVariableSource(SourcePage page) {
       this.page = page;
-      this.namedPage = namedPage;
     }
 
     public Maybe<String> findVariable(String key) {
@@ -132,7 +125,24 @@ public class ParsingPage implements VariableSource {
         value = page.getName();
       else if (key.equals("RUNNING_PAGE_PATH"))
         value = page.getPath();
-      else if (key.equals("PAGE_NAME"))
+      else
+        return Maybe.noString;
+
+      return new Maybe<String>(value);
+    }
+  }
+
+  public static class NamedPageVariableSource implements VariableSource {
+
+    private final SourcePage namedPage;
+
+    public NamedPageVariableSource(SourcePage namedPage) {
+      this.namedPage = namedPage;
+    }
+
+    public Maybe<String> findVariable(String key) {
+      String value;
+      if (key.equals("PAGE_NAME"))
         value = namedPage.getName();
       else if (key.equals("PAGE_PATH"))
         value = namedPage.getPath();
@@ -143,23 +153,27 @@ public class ParsingPage implements VariableSource {
     }
   }
 
-
   public static class ParentPageVariableSource implements VariableSource {
 
     private final ParsingPage page;
     private boolean parsed = false;
 
     public ParentPageVariableSource(SourcePage page, VariableSource variableSource) {
-      this.page = new ParsingPage(page, variableSource);
+      SourcePage parentPage = page.getParent();
+      this.page = parentPage != null ? new ParsingPage(parentPage, variableSource) : null;
     }
 
     @Override
     public Maybe<String> findVariable(String name) {
+      if (page == null) {
+        return Maybe.noString;
+      }
       if (!parsed) {
         // The cache is passed along... page is rendered as a normal page.
         parsed = true;
         Parser.make(page, page.page.getContent()).parse();
       }
+//      System.out.println("Looked for " + name + " in " + page.page.getFullName() + " => " + page.findVariable(name));
       return page.findVariable(name);
     }
 
@@ -176,19 +190,22 @@ public class ParsingPage implements VariableSource {
     @Override
     public Maybe<String> findVariable(String name) {
       String value;
-      if (name.equals("FITNESSE_PORT")) {
-        Maybe<String> port = variableSource.findVariable("FITNESSE_PORT");
-        value = port.isNothing() ? "-1" : port.getValue();
-      } else if (name.equals("FITNESSE_ROOTPATH")) {
-        Maybe<String> path = variableSource.findVariable("FITNESSE_ROOTPATH");
-        value = path.isNothing() ? "" : path.getValue();
-      } else if (name.equals("FITNESSE_VERSION")) {
-        Maybe<String> version = variableSource.findVariable("FITNESSE_VERSION");
-        value = version.isNothing() ? "" : version.getValue();
-      } else {
-        return Maybe.noString;
+      if (variableSource != null) {
+        if (name.equals("FITNESSE_PORT")) {
+          Maybe<String> port = variableSource.findVariable("FITNESSE_PORT");
+          value = port.isNothing() ? "-1" : port.getValue();
+        } else if (name.equals("FITNESSE_ROOTPATH")) {
+          Maybe<String> path = variableSource.findVariable("FITNESSE_ROOTPATH");
+          value = path.isNothing() ? "" : path.getValue();
+        } else if (name.equals("FITNESSE_VERSION")) {
+          Maybe<String> version = variableSource.findVariable("FITNESSE_VERSION");
+          value = version.isNothing() ? "" : version.getValue();
+        } else {
+          return Maybe.noString;
+        }
+        return new Maybe<String>(value);
       }
-      return new Maybe<String>(value);
+      return Maybe.noString;
     }
   }
 
@@ -204,8 +221,10 @@ public class ParsingPage implements VariableSource {
     @Override
     public Maybe<String> findVariable(String name) {
       for (VariableSource variableSource : variableSources) {
-        Maybe<String> result = variableSource.findVariable(name);
-        if (!result.isNothing()) return result;
+        if (variableSource != null) {
+          Maybe<String> result = variableSource.findVariable(name);
+          if (!result.isNothing()) return result;
+        }
       }
       return Maybe.noString;
     }
