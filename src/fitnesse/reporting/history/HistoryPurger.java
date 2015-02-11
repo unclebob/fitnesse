90e13263ff244096eb6bbfc7e425020608c56f7c
Arjan Molenaar <gaphor@gmail.com>
2015-02-11 21:59:38 +0100

    Extracted history purger logic from PurgeHistoryResponder.

---
+++ src/fitnesse/reporting/history/HistoryPurger.java
@@ -0,0 +1,78 @@
+package fitnesse.reporting.history;
+
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import fitnesse.responders.run.SuiteResponder;
+import fitnesse.util.Clock;
+import util.FileUtil;
+
+import static java.lang.String.format;
+
+public class HistoryPurger {
+  private static final Logger LOG = Logger.getLogger(HistoryPurger.class.getName());
+
+  private final File resultsDirectory;
+
+  public HistoryPurger(File resultsDirectory) {
+    this.resultsDirectory = resultsDirectory;
+  }
+
+  public void deleteTestHistoryOlderThanDays(int days) {
+    Date expirationDate = getDateDaysAgo(days);
+    File[] files = FileUtil.getDirectoryListing(resultsDirectory);
+    deleteExpiredFiles(files, expirationDate);
+  }
+
+  private void deleteExpiredFiles(File[] files, Date expirationDate) {
+    for (File file : files)
+      deleteIfExpired(file, expirationDate);
+  }
+
+  public Date getDateDaysAgo(int days) {
+    long now = Clock.currentTimeInMillis();
+    long millisecondsPerDay = 1000L * 60L * 60L * 24L;
+    Date daysEarlier = new Date(now - (millisecondsPerDay * days));
+    return daysEarlier;
+  }
+
+  private void deleteIfExpired(File file, Date expirationDate) {
+    if (file.isDirectory()) {
+      deleteDirectoryIfExpired(file, expirationDate);
+    } else
+      deleteFileIfExpired(file, expirationDate);
+  }
+
+  private void deleteDirectoryIfExpired(File file, Date expirationDate) {
+    File[] files = FileUtil.getDirectoryListing(file);
+    deleteExpiredFiles(files, expirationDate);
+    if (file.list().length == 0)
+      FileUtil.deleteFileSystemDirectory(file);
+  }
+
+  private void deleteFileIfExpired(File file, Date purgeOlder) {
+    String name = file.getName();
+    Date date = getDateFromPageHistoryFileName(name);
+    if (date.getTime() < purgeOlder.getTime())
+      FileUtil.deleteFile(file);
+  }
+
+  private Date getDateFromPageHistoryFileName(String name) {
+    try {
+      return tryExtractDateFromTestHistoryName(name);
+    } catch (ParseException e) {
+      LOG.log(Level.SEVERE, format("Can not determine date from test history file %s", name));
+      return new Date();
+    }
+  }
+
+  private Date tryExtractDateFromTestHistoryName(String testHistoryName) throws ParseException {
+    SimpleDateFormat dateFormat = new SimpleDateFormat(SuiteResponder.TEST_RESULT_FILE_DATE_PATTERN);
+    String dateString = testHistoryName.split("_")[0];
+    return dateFormat.parse(dateString);
+  }
+}
---
+++ test/fitnesse/reporting/history/HistoryPurgerTest.java
@@ -0,0 +1,111 @@
+package fitnesse.reporting.history;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import fitnesse.responders.run.SuiteResponder;
+import fitnesse.util.Clock;
+import fitnesse.util.DateAlteringClock;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import util.FileUtil;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+public class HistoryPurgerTest {
+
+
+  private File resultsDirectory;
+  private HistoryPurger historyPurger;
+
+  @Before
+  public void setUp() {
+    resultsDirectory = new File("testHistoryDirectory");
+    removeResultsDirectory();
+    resultsDirectory.mkdir();
+    historyPurger = new HistoryPurger(resultsDirectory);
+  }
+
+  @After
+  public void resetClock() {
+    Clock.restoreDefaultClock();
+  }
+
+  @Test
+  public void shouldBeAbleToSubtractDaysFromDates() throws Exception {
+    Date date = makeDate("20090616171615");
+    new DateAlteringClock(date).freeze();
+    Date resultDate = historyPurger.getDateDaysAgo(10);
+    Date tenDaysEarlier = makeDate("20090606171615");
+    assertEquals(tenDaysEarlier, resultDate);
+  }
+
+  @Test
+  public void shouldBeAbleToDeleteSomeTestHistory() throws Exception {
+    new DateAlteringClock(makeDate("20090616000000")).freeze();
+    File pageDirectory = addPageDirectory("SomePage");
+    addTestResult(pageDirectory, "20090614000000_1_0_0_0");
+    addTestResult(pageDirectory, "20090615000000_1_0_0_0");
+
+    TestHistory history = new TestHistory();
+    history.readHistoryDirectory(resultsDirectory);
+    PageHistory pageHistory = history.getPageHistory("SomePage");
+    assertEquals(2, pageHistory.size());
+    historyPurger.deleteTestHistoryOlderThanDays(1);
+    history.readHistoryDirectory(resultsDirectory);
+    pageHistory = history.getPageHistory("SomePage");
+    assertEquals(1, pageHistory.size());
+    assertNotNull(pageHistory.get(makeDate("20090615000000")));
+    assertNull(pageHistory.get(makeDate("20090614000000")));
+  }
+
+  @Test
+  public void shouldDeletePageHistoryDirectoryIfEmptiedByPurge() throws Exception {
+    new DateAlteringClock(makeDate("20090616000000")).freeze();
+    File pageDirectory = addPageDirectory("SomePage");
+    addTestResult(pageDirectory, "20090614000000_1_0_0_0");
+    historyPurger.deleteTestHistoryOlderThanDays(1);
+    String[] files = resultsDirectory.list();
+    assertEquals(0, files.length);
+  }
+
+  @Test
+  public void fileWithInvalidDateWillNotBeRemoved() throws Exception {
+    new DateAlteringClock(makeDate("20090616000000")).freeze();
+    File pageDirectory = addPageDirectory("SomePage");
+    addTestResult(pageDirectory, "someFile");
+    historyPurger.deleteTestHistoryOlderThanDays(1);
+    String[] files = new File(resultsDirectory, "SomePage").list();
+    assertEquals(1, files.length);
+    assertEquals("someFile.xml", files[0]);
+  }
+
+  private void removeResultsDirectory() {
+    if (resultsDirectory.exists())
+      FileUtil.deleteFileSystemDirectory(resultsDirectory);
+  }
+
+  private File addTestResult(File pageDirectory, String testResultFileName) throws IOException {
+    File testResultFile = new File(pageDirectory, testResultFileName + ".xml");
+    testResultFile.createNewFile();
+    return testResultFile;
+  }
+
+  private File addPageDirectory(String pageName) {
+    File pageDirectory = new File(resultsDirectory, pageName);
+    pageDirectory.mkdir();
+    return pageDirectory;
+  }
+
+  private Date makeDate(String dateString) throws ParseException {
+    SimpleDateFormat format = new SimpleDateFormat(SuiteResponder.TEST_RESULT_FILE_DATE_PATTERN);
+    Date date = format.parse(dateString);
+    return date;
+  }
+
+}
