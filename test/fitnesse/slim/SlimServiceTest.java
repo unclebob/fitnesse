d8ca0c4902b89256073655eb60191078932cba95
Hans Joachim Desserud <github@desserud.org>
2015-02-12 20:19:52 +0100

    Switch from deprecated Junit assertions

--- test/fitnesse/slim/SlimServiceTest.java
+++ test/fitnesse/slim/SlimServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 
-import static junit.framework.Assert.assertNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
