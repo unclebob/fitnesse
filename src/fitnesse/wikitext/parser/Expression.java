5db17332e4f82d798407cf4515a54c40a691bb63
Arjan Molenaar <gaphor@gmail.com>
2015-02-22 21:15:29 +0100

    Kill cyclic dependencies by moving Expression from fitnesse.util to fitnesse.wikitext.parser.

--- src/fitnesse/wikitext/parser/Expression.java
+++ src/fitnesse/wikitext/parser/Expression.java
@@ -1,8 +1,6 @@
 // Copyright (C) 2003-2009 by Object Mentor, Inc. All rights reserved.
 // Released under the terms of the CPL Common Public License version 1.0.
-package fitnesse.util;
-
-import fitnesse.wikitext.parser.Maybe;
+package fitnesse.wikitext.parser;
 
 public class Expression {
   /*
