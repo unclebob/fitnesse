e02dc1a597d190a14a69eab910d51c91014c8a09
Hans Joachim Desserud <github@desserud.org>
2015-04-04 12:01:03 +0200

    Removed unused imports

--- src/fitnesse/slim/converters/StringConverter.java
+++ src/fitnesse/slim/converters/StringConverter.java
@@ -2,8 +2,6 @@
 // Released under the terms of the CPL Common Public License version 1.0.
 package fitnesse.slim.converters;
 
-import fitnesse.util.StringUtils;
-
 import fitnesse.slim.Converter;
 
 public class StringConverter implements Converter<String> {
--- test/fitnesse/reporting/history/TestXmlFormatterTest.java
+++ test/fitnesse/reporting/history/TestXmlFormatterTest.java
@@ -4,10 +4,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 import java.io.StringWriter;
