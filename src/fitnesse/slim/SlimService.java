8a398b011d1948db7ebd963373ed815e68a3d426
Frank Scheffler <frank.scheffler@novatec-gmbh.de>
2015-04-21 15:00:39 +0200

    Fixes #719 by adding System.exit(0) call to SlimService to ensure main() method properly exits, even if non-daemon threads have been spawned by test fixture.

--- src/fitnesse/slim/SlimService.java
+++ src/fitnesse/slim/SlimService.java
@@ -55,6 +55,7 @@ public class SlimService {
     if (options != null) {
     	try{
     		startWithFactory(createJavaSlimFactory(options), options);
+		System.exit(0);
     	}catch (Exception e){
     		e.printStackTrace();
     		System.out.println("Exiting as exception occured: " + e.getMessage());
