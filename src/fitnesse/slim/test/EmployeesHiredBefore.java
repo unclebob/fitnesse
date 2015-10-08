6aad77cc8fc29d9084d28d75659958e1fe57d8af
Arjan Molenaar <gaphor@gmail.com>
2015-10-08 17:05:21 +0200

    Fix Slim QueryTable example.

--- src/fitnesse/slim/test/EmployeesHiredBefore.java
+++ src/fitnesse/slim/test/EmployeesHiredBefore.java
@@ -1,8 +1,9 @@
 package fitnesse.slim.test;
 
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+
+import static java.util.Arrays.asList;
 
 /**
  * Used by FitNesse.UserGuide.SliM.QueryTable.
@@ -18,6 +19,22 @@ public class EmployeesHiredBefore {
 
   public List<List<List<String>>> query() {
 
-    return Arrays.asList(Arrays.asList(Arrays.asList("company number", "4808147"), Arrays.asList("employee number", "1429"), Arrays.asList("first name", "Bob"), Arrays.asList("last name", "Martin"), Arrays.asList("hire date", "10-Oct-1974")), Arrays.asList(Arrays.asList("company number", "5123122"), Arrays.asList("employee number", "8832"), Arrays.asList("first name", "James"), Arrays.asList("last name", "Grenning"), Arrays.asList("hire date", "15-Dec-1979")));
+    return
+      asList( // table level
+        asList( // row level
+          asList("company number", "4808147"), // cell column name, value
+          asList("employee number", "1429"),
+          asList("first name", "Bob"),
+          asList("last name", "Martin"),
+          asList("hire date", "10-Oct-1974")
+        ),
+        asList(
+          asList("company number", "5123122"),
+          asList("employee number", "8832"),
+          asList("first name", "James"),
+          asList("last name", "Grenning"),
+          asList("hire date", "15-Dec-1979")
+        )
+      );
   }
 }
