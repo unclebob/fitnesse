b98c970e4e7f4413ad03cf3ceba0f1766117d77b
Arjan Molenaar <gaphor@gmail.com>
2016-01-31 21:38:38 +0100

    Decode URL path
    This makes spaces in path names work again.

--- src/fitnesse/testsystems/ClassPath.java
+++ src/fitnesse/testsystems/ClassPath.java
@@ -2,9 +2,9 @@ package fitnesse.testsystems;
 
 import java.io.File;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -70,12 +70,22 @@ public class ClassPath {
     String mainClassFile = mainClass.replaceAll("\\.", "/") + ".class";
     URL url = getClass().getClassLoader().getResource(mainClassFile);
     if (url == null) return null;
-    String path = url.getPath();
+
     if ("file".equals(url.getProtocol())) {
+      String path = uri(url).getPath();
       return new File(path.substring(0, path.length() - mainClassFile.length())).getAbsolutePath();
     } else if ("jar".equals(url.getProtocol())) {
+      String path = url.getPath();
       return new File(URI.create(path.substring(0, path.indexOf("!/")))).getAbsolutePath();
     }
     return null;
+  }
+
+  private URI uri(URL url) {
+    try {
+      return url.toURI();
+    } catch (URISyntaxException e) {
+      throw new RuntimeException("Could not convert URL " + url + " to URI", e);
+    }
   }
 }
