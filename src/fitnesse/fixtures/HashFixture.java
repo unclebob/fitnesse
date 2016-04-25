a7cf17d1c20bbc178da67cea8a6c18ba43f2cdaf
Arjan Molenaar <gaphor@gmail.com>
2016-04-25 22:34:50 +0200

    Hash table is a Map<String, String>.

--- src/fitnesse/fixtures/HashFixture.java
+++ src/fitnesse/fixtures/HashFixture.java
@@ -5,13 +5,13 @@ import java.util.TreeMap;
 
 public class HashFixture {
 
-  private Map<String, Object> hash;
+  private Map<String, String> hash;
 
-  public void sendAsHash(Map<String, Object> hash) {
+  public void sendAsHash(Map<String, String> hash) {
     this.hash = hash;
   }
 
-  public Map<String, Object> hash() {
+  public Map<String, String> hash() {
     // Make result predictable (ordered)
     return new TreeMap<>(hash);
   }
