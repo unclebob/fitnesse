76b71f8393de296629858cae4528a1f8adaf0eda
Fried Hoeben <github@hsac.nl>
2016-09-14 15:16:27 +0200

    Add test to show CachedInteraction cannot be created

---
+++ test/fitnesse/slim/fixtureInteraction/CachedInteractionTest.java
@@ -0,0 +1,11 @@
+package fitnesse.slim.fixtureInteraction;
+
+import org.junit.Test;
+
+public class CachedInteractionTest {
+
+  @Test
+  public void canBeCreated() {
+    CachedInteraction interaction = new CachedInteraction();
+  }
+}
