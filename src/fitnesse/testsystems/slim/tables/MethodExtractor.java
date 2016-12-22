649b25e3dddf722fea9776e8552d027c8b91c5d8
Hans Joachim Desserud <github@desserud.org>
2016-12-22 13:23:03 +0100

    Replace tabs with spaces for indentation

--- src/fitnesse/testsystems/slim/tables/MethodExtractor.java
+++ src/fitnesse/testsystems/slim/tables/MethodExtractor.java
@@ -6,71 +6,73 @@ import java.util.regex.Matcher;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-public class MethodExtractor{
-	private final String version = "1.0";
+public class MethodExtractor {
+  private final String version = "1.0";
 
-	private final ArrayList<MethodExtractorRule> configurations;
+  private final ArrayList<MethodExtractorRule> configurations;
 
-	public MethodExtractor() {
-		super();
-		this.configurations = new ArrayList<>();
-	}
+  public MethodExtractor() {
+    super();
+    this.configurations = new ArrayList<>();
+  }
 
-	public MethodExtractor(String configString) {
-		super();
-		this.configurations = new ArrayList<>();
+  public MethodExtractor(String configString) {
+    super();
+    this.configurations = new ArrayList<>();
 
-			JSONObject jo = new JSONObject(configString);
+    JSONObject jo = new JSONObject(configString);
 
-		String fV= jo.getString("FormatVersion");
-		if (!version.equals(fV)) throw new IllegalArgumentException("JSON Mesage has version '" + fV + "'. This class expects version '"+ version +"'." );
-		JSONArray configList = jo.getJSONArray("MethodExtractorRules");
-		for (int i=0;i< configList.length(); i++){
-			JSONObject config = configList.getJSONObject(i);
-			String scopePattern = config.getString("Scope");
-			String methodName = config.getString("TargetName");
-			String parameterNames = config.getString("Parameters");
-			add(new MethodExtractorRule(scopePattern, methodName, parameterNames));
-		}
-	}
+    String fV = jo.getString("FormatVersion");
+    if (!version.equals(fV))
+      throw new IllegalArgumentException("JSON Mesage has version '" + fV + "'. This class expects version '" + version + "'.");
+    JSONArray configList = jo.getJSONArray("MethodExtractorRules");
+    for (int i = 0; i < configList.length(); i++) {
+      JSONObject config = configList.getJSONObject(i);
+      String scopePattern = config.getString("Scope");
+      String methodName = config.getString("TargetName");
+      String parameterNames = config.getString("Parameters");
+      add(new MethodExtractorRule(scopePattern, methodName, parameterNames));
+    }
+  }
 
-	public boolean add(String scope, String targetName, String parameters){
-		return add(new MethodExtractorRule(scope, targetName, parameters));
-	}
+  public boolean add(String scope, String targetName, String parameters) {
+    return add(new MethodExtractorRule(scope, targetName, parameters));
+  }
 
-	public boolean add(MethodExtractorRule config){
-		return configurations.add(config);
-	}
+  public boolean add(MethodExtractorRule config) {
+    return configurations.add(config);
+  }
 
 
-	public MethodExtractorResult findRule(String methodName){
-		for(int i=0; i< configurations.size(); i++){
-			  Matcher m = configurations.get(i).matcher(methodName);
-			  if(m.matches()){
-				  // The order of the next two lines is important. Don't change it
-				  ArrayList<String>  parameterObjects = configurations.get(i).getParameterList(m);
-				  methodName = configurations.get(i).getMethodName(m);
-				  return new MethodExtractorResult(methodName, parameterObjects);
-			  }
-		}
-		return null;
-	}
-	public String toString(){
-		StringBuilder sb = new StringBuilder("ME:[");
-		for(int i=0; i< configurations.size(); i++){
-			sb.append( configurations.get(i).toString());
-		}
-		sb.append("]");
-		return sb.toString();
-	}
+  public MethodExtractorResult findRule(String methodName) {
+    for (int i = 0; i < configurations.size(); i++) {
+      Matcher m = configurations.get(i).matcher(methodName);
+      if (m.matches()) {
+        // The order of the next two lines is important. Don't change it
+        ArrayList<String> parameterObjects = configurations.get(i).getParameterList(m);
+        methodName = configurations.get(i).getMethodName(m);
+        return new MethodExtractorResult(methodName, parameterObjects);
+      }
+    }
+    return null;
+  }
 
-	public String toJson(){
-		StringBuilder sb = new StringBuilder("{\n\"FormatVersion\":\""+version+"\",\n\"MethodExtractorRules\":[\n");
-		for(int i=0; i< configurations.size(); i++){
-			if(i>0)sb.append(", ");
-			sb.append( configurations.get(i).toJson());
-		}
-		sb.append("]\n}\n");
-		return sb.toString();
-	}
+  public String toString() {
+    StringBuilder sb = new StringBuilder("ME:[");
+    for (int i = 0; i < configurations.size(); i++) {
+      sb.append(configurations.get(i).toString());
+    }
+    sb.append("]");
+    return sb.toString();
+  }
+
+  public String toJson() {
+    StringBuilder sb = new StringBuilder("{\n\"FormatVersion\":\"" + version + "\",\n\"MethodExtractorRules\":[\n");
+    for (int i = 0; i < configurations.size(); i++) {
+      if (i > 0) sb.append(", ");
+      sb.append(configurations.get(i).toJson());
+    }
+    sb.append("]\n}\n");
+    return sb.toString();
+  }
 }
