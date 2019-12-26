cfd6b34230cc3dbe2621805270606a1fd3649561
Fried Hoeben <github@hsac.nl>
2019-12-26 13:11:53 +0100

    format code

--- src/fitnesse/slim/SlimError.java
+++ src/fitnesse/slim/SlimError.java
@@ -8,29 +8,29 @@ public class SlimError extends RuntimeException {
 
   public SlimError(String s) {
     super(s);
-    this.prettyPrint=false;
+    this.prettyPrint = false;
   }
-  
+
   public SlimError(String s, String tag, boolean prettyPrint) {
     super(prettyPrint ? makeSlimMessage(s, tag) : s);
     this.prettyPrint = prettyPrint;
   }
-  
+
   public SlimError(String s, Throwable throwable) {
     super(s, throwable);
-    this.prettyPrint=false;
+    this.prettyPrint = false;
   }
 
   public SlimError(String s, Throwable throwable, String tag, boolean prettyPrint) {
-      super(prettyPrint ? makeSlimMessage(s, tag) : s, throwable);
-      this.prettyPrint=prettyPrint;
-    }
+    super(prettyPrint ? makeSlimMessage(s, tag) : s, throwable);
+    this.prettyPrint = prettyPrint;
+  }
 
   public SlimError(Throwable e) {
     this(e.getClass().getName() + " " + e.getMessage());
   }
 
-  public static String makeSlimMessage(String msg, String tag){
+  public static String makeSlimMessage(String msg, String tag) {
     StringBuilder sb = new StringBuilder();
     sb.append(SlimVersion.PRETTY_PRINT_TAG_START);
     if (tag != null && !tag.isEmpty()) {
@@ -44,13 +44,15 @@ public class SlimError extends RuntimeException {
     sb.append(SlimVersion.PRETTY_PRINT_TAG_END);
     return sb.toString();
   }
-  public static String extractSlimMessage(String msg){
+
+  public static String extractSlimMessage(String msg) {
     msg = msg.replaceFirst(".*" + SlimVersion.PRETTY_PRINT_TAG_START, "");
     msg = msg.replaceFirst(SlimVersion.PRETTY_PRINT_TAG_END, "");
     return msg;
   }
-  public static boolean hasSlimMessage(String msg){
-    return msg.contains(SlimVersion.PRETTY_PRINT_TAG_START) && 
+
+  public static boolean hasSlimMessage(String msg) {
+    return msg.contains(SlimVersion.PRETTY_PRINT_TAG_START) &&
       msg.contains(SlimVersion.PRETTY_PRINT_TAG_END);
   }
 }
