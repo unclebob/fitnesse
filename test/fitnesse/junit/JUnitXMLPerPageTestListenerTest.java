fc69a1a8f92faf8c3c1bcd0c0d52b9d53dfc7d91
Oliver Kuntze <oliver.kuntze@deutschebahn.com>
2020-09-14 15:32:38 +0200

    adds empty unit tests

---
+++ test/fitnesse/junit/JUnitXMLPerPageTestListenerTest.java
@@ -0,0 +1,24 @@
+package fitnesse.junit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JUnitXMLPerPageTestListenerTest {
+
+    @Test
+    public void beforeSuite() {
+    }
+
+    @Test
+    public void afterSuite() {
+    }
+
+    @Test
+    public void beforeTest() {
+    }
+
+    @Test
+    public void afterTest() {
+    }
+}
---
+++ test/fitnesse/junit/JUnitXMLTestResultRecorderTest.java
@@ -0,0 +1,12 @@
+package fitnesse.junit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JUnitXMLTestResultRecorderTest {
+
+    @Test
+    public void recordTestResult() {
+    }
+}
