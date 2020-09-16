bb1377e5357f7950b991d755c953db0bf560a3e0
Oliver Kuntze <oliver.kuntze@deutschebahn.com>
2020-09-16 13:33:33 +0200

    refactors using JUnitXMLReportHelper

--- test/fitnesse/junit/JUnitXMLTestResultRecorderTest.java
+++ test/fitnesse/junit/JUnitXMLTestResultRecorderTest.java
@@ -1,15 +1,11 @@
 package fitnesse.junit;
 
-import org.apache.commons.text.StringEscapeUtils;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -19,12 +15,15 @@ public class JUnitXMLTestResultRecorderTest {
   @Rule
   public TemporaryFolder reportDir = new TemporaryFolder();
 
+  private JUnitXMLReportHelper jUnitXMLReportHelper;
+
   // SUT
-  JUnitXMLTestResultRecorder jUnitXMLTestResultRecorder;
+  private JUnitXMLTestResultRecorder jUnitXMLTestResultRecorder;
 
   @Before
   public void setUp() {
     jUnitXMLTestResultRecorder = new JUnitXMLTestResultRecorder(reportDir.getRoot());
+    jUnitXMLReportHelper = new JUnitXMLReportHelper(reportDir.getRoot());
   }
 
   @Test
@@ -33,13 +32,13 @@ public class JUnitXMLTestResultRecorderTest {
       // given a test name and a test success
       String testName = "myTestName";
       long executionTimeMillis = 1275;
-      String xmlResultOnSuccess = getXmlResultOnSuccess(testName, executionTimeMillis);
+      String xmlResultOnSuccess = jUnitXMLReportHelper.getXmlResultOnSuccess(testName, executionTimeMillis);
 
       // when the test result recorder is called
       jUnitXMLTestResultRecorder.recordTestResult(testName, 0, 0, 0, null, executionTimeMillis);
 
       // then the correct report is written to disk
-      assertEquals(xmlResultOnSuccess, readReportFile("TEST-" + testName + ".xml"));
+      assertEquals(xmlResultOnSuccess, jUnitXMLReportHelper.readReportFile("TEST-" + testName + ".xml"));
     } catch (IOException e) {
       // and no exception is thrown
       fail("IOException was caught but should have never been thrown.");
@@ -53,13 +52,13 @@ public class JUnitXMLTestResultRecorderTest {
       String testName = "myTestName";
       long executionTimeMillis = 1275;
       Throwable throwable = new AssertionError("Gallier approved detail message");
-      String xmlResultOnSkipped = getXmlResultOnSkipped(testName, executionTimeMillis, throwable);
+      String xmlResultOnSkipped = jUnitXMLReportHelper.getXmlResultOnSkipped(testName, executionTimeMillis, throwable);
 
       // when the test result recorder is called
       jUnitXMLTestResultRecorder.recordTestResult(testName, 1, 0, 0, throwable, executionTimeMillis);
 
       // then the correct report is written to disk
-      assertEquals(xmlResultOnSkipped, readReportFile("TEST-" + testName + ".xml"));
+      assertEquals(xmlResultOnSkipped, jUnitXMLReportHelper.readReportFile("TEST-" + testName + ".xml"));
     } catch (IOException e) {
       // and no exception is thrown
       fail("IOException was caught but should have never been thrown.");
@@ -73,13 +72,13 @@ public class JUnitXMLTestResultRecorderTest {
       String testName = "myTestName";
       long executionTimeMillis = 1275;
       Throwable throwable = new AssertionError("Gallier approved detail message");
-      String xmlResultOnFailure = getXmlResultOnFailure(testName, executionTimeMillis, throwable);
+      String xmlResultOnFailure = jUnitXMLReportHelper.getXmlResultOnFailure(testName, executionTimeMillis, throwable);
 
       // when the test result recorder is called
       jUnitXMLTestResultRecorder.recordTestResult(testName, 0, 1, 0, throwable, executionTimeMillis);
 
       // then the correct report is written to disk
-      assertEquals(xmlResultOnFailure, readReportFile("TEST-" + testName + ".xml"));
+      assertEquals(xmlResultOnFailure, jUnitXMLReportHelper.readReportFile("TEST-" + testName + ".xml"));
     } catch (IOException e) {
       // and no exception is thrown
       fail("IOException was caught but should have never been thrown.");
@@ -93,73 +92,17 @@ public class JUnitXMLTestResultRecorderTest {
       String testName = "myTestName";
       long executionTimeMillis = 1275;
       Throwable throwable = new RuntimeException("Gallier approved detail message");
-      String xmlResultOnError = getXmlResultOnError(testName, executionTimeMillis, throwable);
+      String xmlResultOnError = jUnitXMLReportHelper.getXmlResultOnError(testName, executionTimeMillis, throwable);
 
       // when the test result recorder is called
       jUnitXMLTestResultRecorder.recordTestResult(testName, 0, 0, 1, throwable, executionTimeMillis);
 
       // then the correct report is written to disk
-      assertEquals(xmlResultOnError, readReportFile("TEST-" + testName + ".xml"));
+      assertEquals(xmlResultOnError, jUnitXMLReportHelper.readReportFile("TEST-" + testName + ".xml"));
     } catch (IOException e) {
       // and no exception is thrown
       fail("IOException was caught but should have never been thrown.");
     }
-  }
-
-  private String readReportFile(String reportName) throws IOException {
-    return new String(Files.readAllBytes(new File(reportDir.getRoot(), reportName).toPath()), StandardCharsets.UTF_8);
-  }
-
-  private String getXmlResultOnSuccess(String testName, long executionTimeMillis) {
-    return getXmlResult(testName, executionTimeMillis, 0, 0, 0, null);
-  }
-
-  private String getXmlResultOnSkipped(String testName, long executionTimeMillis, Throwable throwable) {
-    return getXmlResult(testName, executionTimeMillis, 1, 0, 0, throwable);
-  }
-
-  private String getXmlResultOnFailure(String testName, long executionTimeMillis, Throwable throwable) {
-    return getXmlResult(testName, executionTimeMillis, 0, 1, 0, throwable);
-  }
-
-  private String getXmlResultOnError(String testName, long executionTimeMillis, Throwable throwable) {
-    return getXmlResult(testName, executionTimeMillis, 0, 0, 1, throwable);
-  }
-
-  private String getXmlResult(String testName, long executionTimeMillis, int skipped, int failures, int errors, Throwable throwable) {
-    String failureXml = "";
-    if (throwable != null) {
-      failureXml = "<failure type=\"" + throwable.getClass().getName()
-        + "\" message=\"" + getMessage(throwable)
-        + "\"></failure>";
-    }
-    double executionTime = executionTimeMillis / 1000d;
-    return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
-      + "<testsuite errors=\""
-      + String.valueOf(errors)
-      + "\" skipped=\""
-      + String.valueOf(skipped)
-      + "\" tests=\"1\" time=\""
-      + String.valueOf(executionTime)
-      + "\" failures=\""
-      + String.valueOf(failures)
-      + "\" name=\""
-      + testName
-      + "\"><properties></properties>"
-      + "<testcase classname=\""
-      + testName
-      + "\" time=\""
-      + String.valueOf(executionTime)
-      + "\" name=\""
-      + testName
-      + "\">"
-      + failureXml
-      + "</testcase></testsuite>";
-  }
-
-  private String getMessage(Throwable throwable) {
-    String errorMessage = throwable.getMessage();
-    return StringEscapeUtils.escapeXml10(errorMessage);
   }
 }
 
