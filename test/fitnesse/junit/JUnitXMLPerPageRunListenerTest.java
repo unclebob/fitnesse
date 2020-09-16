02557821fce28edbfd9525fe62468529cb89faed
Oliver Kuntze <oliver.kuntze@deutschebahn.com>
2020-09-16 16:11:28 +0200

    adds unit tests for JUnitXMLPerPageRunListener

--- test/fitnesse/junit/JUnitXMLPerPageRunListenerTest.java
+++ test/fitnesse/junit/JUnitXMLPerPageRunListenerTest.java
@@ -1,8 +1,14 @@
 package fitnesse.junit;
 
 import org.junit.Test;
+import org.junit.runner.Description;
+import org.junit.runner.notification.Failure;
 
+import java.io.IOException;
+
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class JUnitXMLPerPageRunListenerTest {
 
@@ -12,14 +18,72 @@ public class JUnitXMLPerPageRunListenerTest {
   private JUnitXMLPerPageRunListener jUnitXMLPerPageRunListener = new JUnitXMLPerPageRunListener(testResultRecorderMock);
 
   @Test
-  public void testStarted() {
-  }
+  public void testSuccess() {
+    // given some test
+    Description description = Description.createTestDescription(JUnitXMLPerPageRunListenerTest.class, "myTestName");
 
-  @Test
-  public void testFinished() {
+    // when the test is started and finished
+    try {
+      jUnitXMLPerPageRunListener.testStarted(description);
+      jUnitXMLPerPageRunListener.testFinished(description);
+    } catch (Exception e) {
+      fail("Exception was caught but should have never been thrown.");
+    }
+
+    // then the test result recorder is called with the correct parameters and no exception IOException is thrown
+    try {
+      verify(testResultRecorderMock)
+        .recordTestResult(description.getMethodName(), 0, 0, 0, null, jUnitXMLPerPageRunListener.getExecutionTime());
+    } catch (IOException e) {
+      fail("IOException was caught but should have never been thrown.");
+    }
   }
 
   @Test
   public void testFailure() {
+    // given some test and an associated failure
+    Description description = Description.createTestDescription(JUnitXMLPerPageRunListenerTest.class, "myTestName");
+    Throwable throwable = new AssertionError("Gallier approved detail messsage");
+    Failure failure = new Failure(description, throwable);
+
+    // when the test is started and fails
+    try {
+      jUnitXMLPerPageRunListener.testStarted(description);
+      jUnitXMLPerPageRunListener.testFailure(failure);
+    } catch (Exception e) {
+      fail("Exception was caught but should have never been thrown.");
+    }
+
+    // then the test result recorder is called with the correct parameters and no exception IOException is thrown
+    try {
+      verify(testResultRecorderMock)
+        .recordTestResult(description.getMethodName(), 0, 1, 0, throwable, jUnitXMLPerPageRunListener.getExecutionTime());
+    } catch (IOException e) {
+      fail("IOException was caught but should have never been thrown.");
+    }
+  }
+
+  @Test
+  public void testFailureWithError() {
+    // given some test and an associated failure
+    Description description = Description.createTestDescription(JUnitXMLPerPageRunListenerTest.class, "myTestName");
+    Throwable throwable = new RuntimeException("Gallier approved detail messsage");
+    Failure failure = new Failure(description, throwable);
+
+    // when the test is started and fails
+    try {
+      jUnitXMLPerPageRunListener.testStarted(description);
+      jUnitXMLPerPageRunListener.testFailure(failure);
+    } catch (Exception e) {
+      fail("Exception was caught but should have never been thrown.");
+    }
+
+    // then the test result recorder is called with the correct parameters and no exception IOException is thrown
+    try {
+      verify(testResultRecorderMock)
+        .recordTestResult(description.getMethodName(), 0, 0, 1, throwable, jUnitXMLPerPageRunListener.getExecutionTime());
+    } catch (IOException e) {
+      fail("IOException was caught but should have never been thrown.");
+    }
   }
 }
