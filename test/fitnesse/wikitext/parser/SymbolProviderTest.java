cc5f2e5010af8acd17bfb0cb06123862d6aa2e64
Tcnh <tom.heintzberger@gmail.com>
2020-11-19 09:12:14 +0100

    Remove unused import

--- test/fitnesse/wikitext/parser/SymbolProviderTest.java
+++ test/fitnesse/wikitext/parser/SymbolProviderTest.java
@@ -2,7 +2,6 @@ package fitnesse.wikitext.parser;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
 public class SymbolProviderTest {
