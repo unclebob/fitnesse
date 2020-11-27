1e6de2d927eea7d2999e3b29aae4e6f985759c12
Tcnh <tom.heintzberger@gmail.com>
2020-11-27 13:22:20 +0100

    Limit not-escaping to tables in lists, added unittests

--- src/fitnesse/testsystems/slim/HtmlTable.java
+++ src/fitnesse/testsystems/slim/HtmlTable.java
@@ -37,8 +37,8 @@ public class HtmlTable implements Table {
                                                                 HtmlUtil.HTML_CELL_CONTENT_PATTERN_TEXT +
                                                                 SYMBOL_ASSIGNMENT_SUFFIX + "?$",
                                                           Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-  private static final Pattern CONTAINS_HTML_PATTERN = Pattern.compile(".*" + HtmlUtil.HTML_CELL_CONTENT_PATTERN_TEXT + ".*",
-    Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+  private static final Pattern CONTAINS_TABLE_HTML_PATTERN = Pattern.compile(".*(<table.*?>.*</table>).*",
+                                                                          Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
   private static final Pattern SYMBOL_REPLACEMENT_PATTERN = Pattern.compile("^(" + SYMBOL_ASSIGNMENT + ")(.*)(" +
           SYMBOL_ASSIGNMENT_SUFFIX + ")$", Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 
@@ -455,7 +455,7 @@ public class HtmlTable implements Table {
       }
       return text;
     }
-    if (containsHtml(text) && qualifiesAsConvertedList(text)) {
+    if (containsHtmlTable(text) && qualifiesAsConvertedList(text)) {
       return text;
     }
     return HtmlUtil.escapeHTML(text);
@@ -482,8 +482,8 @@ public class HtmlTable implements Table {
     return text.startsWith("[") && text.endsWith("]");
   }
 
-  static boolean containsHtml(String text) {
-    return text.contains("<") && CONTAINS_HTML_PATTERN.matcher(text).matches();
+  static boolean containsHtmlTable(String text) {
+    return text.contains("<") && CONTAINS_TABLE_HTML_PATTERN.matcher(text).matches();
   }
 
   private static String[] parseSymbol(String text) {
--- test/fitnesse/testsystems/slim/HtmlTableTest.java
+++ test/fitnesse/testsystems/slim/HtmlTableTest.java
@@ -1,8 +1,15 @@
 package fitnesse.testsystems.slim;
 
 import fitnesse.testsystems.slim.results.SlimTestResult;
+import org.htmlparser.tags.TableColumn;
+import org.htmlparser.tags.TableRow;
+import org.htmlparser.tags.TableTag;
+import org.htmlparser.util.NodeList;
 import org.junit.Test;
 
+import java.util.Collections;
+
+import static fitnesse.testsystems.slim.HtmlTable.qualifiesAsConvertedList;
 import static fitnesse.testsystems.slim.HtmlTable.qualifiesAsHtml;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertFalse;
@@ -10,6 +17,19 @@ import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class HtmlTableTest {
+
+  private final String listContainingHashTable = "$list<-[[a, b, <table class=\"hash_table\">\n" +
+    "<tr class=\"hash_row\">\n" +
+    "<td class=\"hash_key\">first</td>\n" +
+    "<td class=\"hash_value\">1</td>\n" +
+    "</tr>\n" +
+    "<tr class=\"hash_row\">\n" +
+    "<td class=\"hash_key\">second</td>\n" +
+    "<td class=\"hash_value\">2</td>\n" +
+    "</tr>\n" +
+    "</table>]]";
+
+  private final String listContainingImage = "$list<-[[a, b, <img src='http://fitnesse.org'/>]]";
 
   @Test
   public void normalTextDoesNotQualifyAsHtml() {
@@ -71,6 +91,34 @@ public class HtmlTableTest {
   }
 
   @Test
+  public void convertedListQualifiesAsConvertedList() {
+    assertTrue(qualifiesAsConvertedList("[a, b, c]"));
+    assertTrue(qualifiesAsConvertedList(listContainingHashTable));
+  }
+
+  @Test
+  public void symbolAssignmentOfConvertedListQualifiesAsConvertedList() {
+    assertTrue(qualifiesAsConvertedList("$list<-[[a, b]]"));
+    assertTrue(qualifiesAsConvertedList(listContainingHashTable));
+  }
+
+  @Test
+  public void convertedListContainingHashTableIsNotEscaped() {
+    HtmlTable t = getDummyTable();
+    t.addRow(Collections.singletonList(listContainingHashTable));
+    assertFalse(t.toHtml().contains("&lt;"));
+    assertTrue(t.toHtml().contains(listContainingHashTable));
+  }
+
+  @Test
+  public void convertedListContainingOtherHtmlIsEscaped() {
+    HtmlTable t = getDummyTable();
+    t.addRow(Collections.singletonList(listContainingImage));
+    assertTrue(t.toHtml().contains("&lt;"));
+    assertFalse(t.toHtml().contains(listContainingHashTable));
+  }
+
+  @Test
   public void ignoredCellWithoutMessageShouldRenderOriginalContentAsIgnored() {
     HtmlTable.Cell cell = new HtmlTable.Cell("original content");
 
@@ -86,6 +134,14 @@ public class HtmlTableTest {
     cell.setTestResult(SlimTestResult.ignore("a message"));
 
     assertThat(cell.formatTestResult(), is("<span class=\"ignore\">a message</span>"));
+  }
+
+  private HtmlTable getDummyTable() {
+    TableTag tbl = new TableTag();
+    TableRow row = new TableRow();
+    row.setChildren(new NodeList(new TableColumn()));
+    tbl.setChildren(new NodeList(row));
+    return new HtmlTable(tbl);
   }
 
 }
