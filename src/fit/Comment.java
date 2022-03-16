c43d17913b820e696890dd26ea4110198aabaa17
jakcomp <jakobvalen9@gmail.com>
2022-03-16 16:46:01 -0700

    Safe delete of unused and empty Comment class

--- src/fit/Comment.java
+++
@@ -1,7 +0,0 @@
-// Modified or written by Object Mentor, Inc. for inclusion with FitNesse.
-// Copyright (c) 2002 Cunningham & Cunningham, Inc.
-// Released under the terms of the GNU General Public License version 2 or later.
-package fit;
-
-public class Comment extends Fixture {
-}
