c814c6e8db86ceb4cc074d379e9521d9422c1ddc
Daniel Brown <djbrown@users.noreply.github.com>
2022-04-30 20:53:43 +0200

    allow base64 data url images (#1367)
    fixes unclebob/fitnesse#1135
    see https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
    and https://en.wikipedia.org/wiki/Base64

--- src/fitnesse/wikitext/parser/Image.java
+++ src/fitnesse/wikitext/parser/Image.java
@@ -1,5 +1,6 @@
 package fitnesse.wikitext.parser;
 
+import java.util.List;
 import java.util.TreeMap;
 import java.util.Map;
 
@@ -55,12 +56,56 @@ public class Image extends SymbolType implements Rule, Translation {
             return makeImageLink(current, link.getValue(), imageProperty);
         }
         else if (name.isType(SymbolType.Text) || name.isType(WikiWord.symbolType)) {
-            Symbol list = new Symbol(SymbolType.SymbolList).add(new Symbol(SymbolType.Text, name.getContent()));
+            String imageDataUrl = tryParseBase64DataUrl(parser);
+            String imageUrl = (imageDataUrl == null) ? name.getContent() : imageDataUrl;
+            Symbol list = new Symbol(SymbolType.SymbolList).add(new Symbol(SymbolType.Text, imageUrl));
             Symbol link = new Symbol(Link.symbolType).add(list);
             addOptions(link, options);
             return makeImageLink(current, link, imageProperty);
         }
         else return Symbol.nothing;
+    }
+
+    /**
+     * @return null if the base64 data url could not be parsed
+     * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
+     * @see https://datatracker.ietf.org/doc/html/rfc2397
+     */
+    private String tryParseBase64DataUrl(Parser parser) {
+        List<Symbol> nextSymbols = parser.peek(new SymbolType[]{SymbolType.Colon, SymbolType.Text, SymbolType.Comma});
+        if (nextSymbols.size() == 0) return null;
+        
+        Symbol prefix = parser.getCurrent();
+        if (!prefix.isType(SymbolType.Text) || !prefix.getContent().equals("data")) return null;
+        StringBuilder imageUrl = new StringBuilder(prefix.getContent());
+
+        Symbol colon = nextSymbols.get(0);
+        imageUrl.append(colon.getContent());
+
+        Symbol mediaType = nextSymbols.get(1);
+        if (!mediaType.getContent().endsWith(";base64")) return null;
+        imageUrl.append(mediaType.getContent());
+
+        Symbol comma = nextSymbols.get(2);
+        imageUrl.append(comma.getContent());
+
+        Symbol dataSymbol = parser.peek(4).get(3);
+        if(!isBase64Symbol(dataSymbol)) return null;
+        parser.moveNext(4);
+        imageUrl.append(dataSymbol.getContent());
+
+        while(isBase64Symbol(parser.peek())) {
+            Symbol symbol = parser.moveNext(1);
+            imageUrl.append(symbol.getContent());
+        }
+        return imageUrl.toString();
+    }
+
+    private boolean isBase64Symbol(Symbol symbol) {
+        if (symbol.isType(SymbolType.Whitespace)) return false;
+        return symbol.isType(SymbolType.Text)
+                || symbol.isType(WikiWord.symbolType)
+                || symbol.isType(SymbolType.Delta);
     }
 
     private void addOptions(Symbol link, Map<String, String> options) {
