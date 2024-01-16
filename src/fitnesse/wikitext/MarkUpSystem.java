bd43869bbd2d5bcdce58ed8b69ab664854e61d56
AlexNetcare <135694182+AlexNetcare@users.noreply.github.com>
2024-01-16 18:42:39 +0100

    Add Variable values to ScopeVariablesResponder (#1472)

--- src/fitnesse/wikitext/MarkUpSystem.java
+++ src/fitnesse/wikitext/MarkUpSystem.java
@@ -4,7 +4,9 @@ import fitnesse.wiki.WikiPage;
 import fitnesse.wiki.WikiSourcePage;
 import fitnesse.wikitext.parser.MarkUpSystemV2;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -18,10 +20,14 @@ public interface MarkUpSystem {
   static MarkUpSystem make() { return new MarkUpSystemV2(); }
   static MarkUpSystem make(String content) { return MarkUpSystems.STORE.make(content); }
 
-  static List<String> listVariables(WikiPage page) {
+  static Map<String, String> listVariables(WikiPage page) {
+    Map<String, String> listVariables = new HashMap<>();
     ParsingPage parsingPage = new ParsingPage(new WikiSourcePage(page));
     String content = page.getData().getContent();
     MarkUpSystem.make(content).parse(parsingPage, content);
-    return parsingPage.listVariables();
+    for (String listVariable : parsingPage.listVariables()) {
+      listVariables.put(listVariable, parsingPage.findVariable(listVariable).orElse(""));
+    }
+    return listVariables;
   }
 }
