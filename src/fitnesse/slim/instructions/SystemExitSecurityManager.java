16bde2b60b7d2c415632e21e0bb734dabed5d58c
Vincent Boulaye <vincent.boulaye@orange.com>
2024-06-10 20:53:14 +0200

    ignore UnsupportedOperationException thrown by jdk>18 zhen trying to setup the SystemExitSecurityManager (fixes #1455)

--- src/fitnesse/slim/instructions/SystemExitSecurityManager.java
+++ src/fitnesse/slim/instructions/SystemExitSecurityManager.java
@@ -35,6 +35,10 @@ public class SystemExitSecurityManager extends SecurityManager {
       System.setSecurityManager(securityManager);
     } catch (SecurityException e) {
       System.err.println("Security manager could not be updated");
+    } catch (UnsupportedOperationException e) {
+      System.err.println("Security manager could not be updated. If you are usin a JDK version >=18, you need to set " +
+        "-Djava.security.manager=allow to allow this. Or use -Dprevent.system.exit=false to disable this feature inside " +
+        "fitnesse. Exception was :" + e.getMessage());
     }
   }
 
